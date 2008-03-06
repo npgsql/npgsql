696055f85d62c406c3bd81ef08b6c04bd975c085
jbcooley <>
2008-03-06 04:48:07 +0000

    More select statement support plus simple dml support

--- src/Npgsql/SqlGenerators/SqlUpdateGenerator.cs
+++ src/Npgsql/SqlGenerators/SqlUpdateGenerator.cs
@@ -2,14 +2,54 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Data.Common.CommandTrees;
 
 namespace Npgsql.SqlGenerators
 {
     class SqlUpdateGenerator : SqlBaseGenerator
-	{
+    {
+        private DbUpdateCommandTree _commandTree;
+
+        public SqlUpdateGenerator(DbUpdateCommandTree commandTree)
+        {
+            commandTree.Validate();
+
+            _commandTree = commandTree;
+        }
+
+        public override VisitedExpression Visit(DbPropertyExpression expression)
+        {
+            DbVariableReferenceExpression variable = expression.Instance as DbVariableReferenceExpression;
+            if (variable == null || variable.VariableName != _projectVarName.Peek())
+                throw new NotSupportedException();
+            return new LiteralExpression(expression.Property.Name);
+        }
+
         public override void BuildCommand(DbCommand command)
         {
-            throw new NotImplementedException();
+            // TODO: handle _commandTree.Returning and _commandTree.Parameters
+            UpdateExpression update = new UpdateExpression();
+            _projectVarName.Push(_commandTree.Target.VariableName);
+            update.Append(_commandTree.Target.Expression.Accept(this));
+            update.Append(" SET ");
+            bool first = true;
+            foreach (DbSetClause clause in _commandTree.SetClauses)
+            {
+                if (!first)
+                    update.Append(",");
+                VisitedExpression assignment = clause.Property.Accept(this);
+                assignment.Append("=");
+                assignment.Append(clause.Value.Accept(this));
+                update.Append(assignment);
+                first = false;
+            }
+            if (_commandTree.Predicate != null)
+            {
+                update.Append(" WHERE ");
+                update.Append(_commandTree.Predicate.Accept(this));
+            }
+            _projectVarName.Pop();
+            command.CommandText = update.ToString();
         }
 	}
 }
