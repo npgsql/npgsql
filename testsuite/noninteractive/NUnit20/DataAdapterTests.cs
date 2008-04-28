c2780e9abb507527f6527acd4b12c8e85715440e
jbcooley <>
2008-04-28 01:50:30 +0000

    Make Fill test work with version 2 of the protocol

--- testsuite/noninteractive/NUnit20/DataAdapterTests.cs
+++ testsuite/noninteractive/NUnit20/DataAdapterTests.cs
@@ -138,7 +138,11 @@ namespace NpgsqlTests
             Assert.AreEqual("field_serial", field_serial.ColumnName);
             Assert.AreEqual(typeof(int), field_serial.DataType);
             Assert.AreEqual(0, field_serial.Ordinal);
-            Assert.IsTrue(field_serial.Unique);
+            // version 2 of the protocol doesn't know how to populate the unique field
+            if (TheConnection.BackendProtocolVersion != ProtocolVersion.Version2)
+            {
+                Assert.IsTrue(field_serial.Unique);
+            }
 
             Assert.IsTrue(field_int2.AllowDBNull);
             Assert.IsFalse(field_int2.AutoIncrement);
