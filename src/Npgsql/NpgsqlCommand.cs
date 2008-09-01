554a89048688bb96611efca274b69a5559e014cc
fxjr <>
2008-09-01 20:58:25 +0000

    [npgsql-open-discussion][1003946] RE: Connection leak in ExecuteReader/CloseConnection. Fixed a problem where Npgsql wasn't closing the connection if commandbehavior was said to close it and an error occured while executing ExecuteReader(CommandBehavior) method. Thanks Andrus Moor and Alaric Dailey for heads up and tests.

--- src/Npgsql/NpgsqlCommand.cs
+++ src/Npgsql/NpgsqlCommand.cs
@@ -525,16 +525,29 @@ namespace Npgsql
 		public new NpgsqlDataReader ExecuteReader(CommandBehavior cb)
 		{
 			NpgsqlEventLog.LogMethodEnter(LogLevel.Debug, CLASSNAME, "ExecuteReader", cb);
-			if (connection != null)
-			{
-				if (connection.PreloadReader)
-				{
-					//Adjust behaviour so source reader is sequential access - for speed - and doesn't close the connection - or it'll do so at the wrong time.
-					CommandBehavior adjusted = (cb | CommandBehavior.SequentialAccess) & ~CommandBehavior.CloseConnection;
-					return new CachingDataReader(GetReader(adjusted), cb);
-				}
-			}
-			return GetReader(cb);
+			
+			// Close connection if requested even when there is an error.
+			try
+            {
+                if (connection != null)
+                {
+                    if (connection.PreloadReader)
+                    {
+                        //Adjust behaviour so source reader is sequential access - for speed - and doesn't close the connection - or it'll do so at the wrong time.
+                        CommandBehavior adjusted = (cb | CommandBehavior.SequentialAccess) & ~CommandBehavior.CloseConnection;
+                        return new CachingDataReader(GetReader(adjusted), cb);
+        				}
+                }
+                return GetReader(cb);
+            }
+            finally
+            {
+                if ((cb & CommandBehavior.CloseConnection) == CommandBehavior.CloseConnection)
+			    {
+				    connection.Close();
+			    }
+            }
+        		
 		}
 
 		internal ForwardsOnlyDataReader GetReader(CommandBehavior cb)
