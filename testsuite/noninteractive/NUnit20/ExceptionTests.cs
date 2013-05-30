e6bb5723167b02c46e89510e14bca58de186087c
davidrowley <dgrowley@gmail.com>
2013-05-30 13:40:22 +1200

    Renamed newly added file and added copyright
    Renamed Exception.cs to ExceptionTests.cs and added copyright notice to
    the top of the file

--- testsuite/noninteractive/NUnit20/ExceptionTests.cs
+++ testsuite/noninteractive/NUnit20/ExceptionTests.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿// project created on 30/11/2002 at 22:00
+//
+// Author:
+//  David Rowley <dgrowley@gmail.com>
+// 	Francisco Figueiredo Jr. <fxjrlists@yahoo.com>
+//
+//	Copyright (C) 2002 The Npgsql Development Team
+//	npgsql-general@gborg.postgresql.org
+//	http://gborg.postgresql.org/project/npgsql/projdisplay.php
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+using System;
 using System.Data;
 using System.Web.UI.WebControls;
 
@@ -70,11 +93,9 @@ namespace NpgsqlTests
             }
             catch (NpgsqlException ex)
             {
-                Assert.AreEqual("id", ex.ColumnName);
                 Assert.AreEqual("uniqueviolation", ex.TableName);
                 Assert.AreEqual("public", ex.SchemaName);
                 Assert.AreEqual("uniqueviolation_pkey", ex.ConstraintName);
-                Assert.AreEqual("integer", ex.DataTypeName);
             }
         }
 
