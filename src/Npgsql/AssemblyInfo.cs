3ba521307c9e08da0686728a7edd6f95a6f22210
Francisco Figueiredo Jr <francisco@npgsql.org>
2013-06-28 09:35:37 -0300

    Fixed assembly version to the correct value of 2.0.13 beta 1.
    Internally the version numbers of the beta are coded with a vlue
    starting with 1x.9x where 12.91 is beta1, 12.92 is beta2 to indicate it is close
    to the final release 13.0. Incorrectly I used a wrong value.

--- src/Npgsql/AssemblyInfo.cs
+++ src/Npgsql/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Resources;
 [assembly: AssemblyProductAttribute("Npgsql")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2002 - 2013 Npgsql Development Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("2.0.13.91")]
-[assembly: AssemblyFileVersionAttribute("2.0.13.91")]
+[assembly: AssemblyVersionAttribute("2.0.12.91")]
+[assembly: AssemblyFileVersionAttribute("2.0.12.91")]
 [assembly: NeutralResourcesLanguageAttribute("en", UltimateResourceFallbackLocation.MainAssembly)]
 
