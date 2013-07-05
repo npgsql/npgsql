0a63b88ccfd6f21db5119e1c9664a109985395e6
Francisco Figueiredo Jr <francisco@npgsql.org>
2013-07-05 17:28:36 -0300

    Fixed strange + sign which appeared in last commit.

--- src/Npgsql/SqlGenerators/VisitedExpression.cs
+++ src/Npgsql/SqlGenerators/VisitedExpression.cs
@@ -137,7 +137,7 @@ namespace Npgsql.SqlGenerators
                     sqlText.AppendFormat(ni, "cast({0} as float4)", _value);
                     break;
                 case PrimitiveTypeKind.Boolean:
-+                    sqlText.AppendFormat(ni, "cast({0} as boolean)", ((bool)_value)?"TRUE":"FALSE");
+                     sqlText.AppendFormat(ni, "cast({0} as boolean)", ((bool)_value)?"TRUE":"FALSE");
                      break;
                 case PrimitiveTypeKind.Guid:
                 case PrimitiveTypeKind.String:
