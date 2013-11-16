2eb7f5570141881932b1937fbebe5fa335859d23
Brian Crowell <bcrowell@barbnet.com>
2013-11-15 22:42:33 -0600

    IMPORTANT: Freeze user name in connection string when using integrated security
    When connection pooling is turned on, connections are cached by connection
    string. But when integrated security is turned on, the user's identity
    isn't part of the connection string, so two users can get each other's
    connections back from the connection pool.
    
    This patch forces the username to appear in the connection string if
    integrated security is on, before any pooling takes place.

--- Npgsql/Npgsql/NpgsqlConnection.cs
+++ Npgsql/Npgsql/NpgsqlConnection.cs
@@ -1058,15 +1058,14 @@ namespace Npgsql
         /// <param name="connectionString">The connection string to load the builder from</param>
         private void LoadConnectionStringBuilder(string connectionString)
         {
-            settings = cache[connectionString];
-            if (settings == null)
+            NpgsqlConnectionStringBuilder newSettings = cache[connectionString];
+            if (newSettings == null)
             {
-                settings = new NpgsqlConnectionStringBuilder(connectionString);
-                cache[connectionString] = settings;
+                newSettings = new NpgsqlConnectionStringBuilder(connectionString);
+                cache[connectionString] = newSettings;
             }
 
-            RefreshConnectionString();
-            LogConnectionString();
+            LoadConnectionStringBuilder(newSettings);
         }
 
         /// <summary>
@@ -1075,7 +1074,13 @@ namespace Npgsql
         /// <param name="connectionString">The connection string to load the builder from</param>
         private void LoadConnectionStringBuilder(NpgsqlConnectionStringBuilder connectionString)
         {
-            settings = connectionString;
+            // Clone the settings, because if Integrated Security is enabled, user ID can be different
+            settings = connectionString.Clone();
+
+            // Set the UserName explicitly to freeze any Integrated Security-determined names
+            if (settings.IntegratedSecurity)
+               settings.UserName = settings.UserName;
+
             RefreshConnectionString();
             LogConnectionString();
         }
