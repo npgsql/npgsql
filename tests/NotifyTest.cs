6f7eb2053bf742dae25e9e2945b098dd80a15adf
Udo Liess <udo.liess@gmx.net>
2013-12-05 10:22:42 +0100

    add constructor for NotifyTest

--- tests/NotifyTest.cs
+++ tests/NotifyTest.cs
@@ -6,7 +6,25 @@
 // Send multiple NOTIFY events and wait for all of them.
 // See bug [#1011374] http://pgfoundry.org/tracker/index.php?func=detail&aid=1011374&group_id=1000140&atid=590
 
+// Permission to use, copy, modify, and distribute this software and its
+// documentation for any purpose, without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph and the following two paragraphs appear in all copies.
+//
+// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
+// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
+// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
 // 2013-11-26 initial version
+// 2013-12-05 add constructor with parameter backendVersion
 
 using Npgsql;
 using NUnit.Framework;
@@ -20,6 +38,7 @@ namespace NpgsqlTests
     [TestFixture]
     class NotifyTest : TestBase
     {
+        public NotifyTest(string backendVersion) : base(backendVersion) { }
 
         readonly TimeSpan timeout = TimeSpan.FromSeconds(5); // timeout in case of failure
         const int rounds = 6; // number of rounds to test
