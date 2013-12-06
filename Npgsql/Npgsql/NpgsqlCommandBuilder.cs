33a745273b886dcd31483d8fb5ddb1736334d21c
NoeGarcia <elnoegarcia@gmail.com>
2013-12-06 16:16:05 -0700

    Detection of Parameter Direction in DeriveParameters method.
    This detection is necessary because currently this method defaults all parameters with Input direction and when using functions that have OUT or INOUT parameters they aren't updated.
    (PR - updated for current master)

--- Npgsql/Npgsql/NpgsqlCommandBuilder.cs
+++ Npgsql/Npgsql/NpgsqlCommandBuilder.cs
@@ -44,11 +44,18 @@ namespace Npgsql
         // Commented out because SetRowUpdatingHandler() is commented, and causes an "is never used" warning
         // private NpgsqlRowUpdatingEventHandler rowUpdatingHandler;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NpgsqlCommandBuilder"/> class.
+        /// </summary>
         public NpgsqlCommandBuilder()
             : this(null)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NpgsqlCommandBuilder"/> class.
+        /// </summary>
+        /// <param name="adapter">The adapter.</param>
         public NpgsqlCommandBuilder(NpgsqlDataAdapter adapter)
             : base()
         {
@@ -57,6 +64,15 @@ namespace Npgsql
             this.QuoteSuffix = "\"";
         }
 
+        /// <summary>
+        /// Gets or sets the beginning character or characters to use when specifying database objects (for example, tables or columns) whose names contain characters such as spaces or reserved tokens.
+        /// </summary>
+        /// <returns>
+        /// The beginning character or characters to use. The default is an empty string.
+        ///   </returns>
+        ///   <PermissionSet>
+        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" PathDiscovery="*AllFiles*" />
+        ///   </PermissionSet>
         public override string QuotePrefix
         {
             get { return base.QuotePrefix; }
@@ -73,6 +89,15 @@ namespace Npgsql
             }
         }
 
+        /// <summary>
+        /// Gets or sets the ending character or characters to use when specifying database objects (for example, tables or columns) whose names contain characters such as spaces or reserved tokens.
+        /// </summary>
+        /// <returns>
+        /// The ending character or characters to use. The default is an empty string.
+        ///   </returns>
+        ///   <PermissionSet>
+        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" PathDiscovery="*AllFiles*" />
+        ///   </PermissionSet>
         public override string QuoteSuffix
         {
             get { return base.QuoteSuffix; }
@@ -94,7 +119,8 @@ namespace Npgsql
         /// This method is reponsible to derive the command parameter list with values obtained from function definition.
         /// It clears the Parameters collection of command. Also, if there is any parameter type which is not supported by Npgsql, an InvalidOperationException will be thrown.
         /// Parameters name will be parameter1, parameter2, ...
-        /// For while, only parameter name and NpgsqlDbType are obtained.
+        /// For while, only parameter name, NpgsqlDbType and parameter direction (IN, INOUT) are obtained.
+        /// Out parameters are removed because PosgreSql does not consider them in function signatures.
         ///</summary>
         /// <param name="command">NpgsqlCommand whose function parameters will be obtained.</param>
         public static void DeriveParameters(NpgsqlCommand command)
@@ -107,14 +133,21 @@ namespace Npgsql
             string[] fullName = command.CommandText.Split('.');
             if (fullName.Length > 1 && fullName[0].Length > 0)
             {
-                query =
-                    "select proargnames, proargtypes from pg_proc p left join pg_namespace n on p.pronamespace = n.oid where proname=:proname and n.nspname=:nspname";
+                // proargsmodes is supported for Postgresql 8.1 and above
+                if (command.Connector.ServerVersion >= new System.Version(8, 1, 0))
+                    query = "select proargnames, proargtypes, proargmodes from pg_proc p left join pg_namespace n on p.pronamespace = n.oid where proname=:proname and n.nspname=:nspname";
+                else
+                    query = "select proargnames, proargtypes from pg_proc p left join pg_namespace n on p.pronamespace = n.oid where proname=:proname and n.nspname=:nspname";
                 schemaName = (fullName[0].IndexOf("\"") != -1) ? fullName[0] : fullName[0].ToLower();
                 procedureName = (fullName[1].IndexOf("\"") != -1) ? fullName[1] : fullName[1].ToLower();
             }
             else
             {
-                query = "select proargnames, proargtypes from pg_proc where proname = :proname";
+                // proargsmodes is supported for Postgresql 8.1 and above
+                if (command.Connector.ServerVersion >= new System.Version(8, 1, 0))
+                    query = "select proargnames, proargtypes, proargmodes from pg_proc where proname = :proname";
+                else
+                    query = "select proargnames, proargtypes from pg_proc where proname = :proname";
                 procedureName = (fullName[0].IndexOf("\"") != -1) ? fullName[0] : fullName[0].ToLower();
             }
 
@@ -130,6 +163,7 @@ namespace Npgsql
 
                 String[] names = null;
                 String[] types = null;
+                String[] directions = null;
 
                 using (NpgsqlDataReader rdr = c.ExecuteReader(CommandBehavior.SingleRow | CommandBehavior.SingleResult))
                 {
@@ -139,6 +173,8 @@ namespace Npgsql
                             names = rdr.GetValue(0) as String[];
                         if (!rdr.IsDBNull(1))
                             types = rdr.GetString(1).Split();
+                        if ((rdr.FieldCount > 2) && (!rdr.IsDBNull(2))) // When all parameters are IN, "proargmodes" column is returned as null
+                            directions = rdr.GetValue(2) as String[];
                     }
                 }
 
@@ -146,6 +182,22 @@ namespace Npgsql
                 {
                     throw new InvalidOperationException(
                         String.Format(resman.GetString("Exception_InvalidFunctionName"), command.CommandText));
+                }
+
+                if (directions != null)
+                {
+                    if (names != null)
+                    {
+                        // If names array is not null, it always be the same size as directions array.
+                        for (Int32 i = 0; i < directions.Length; i++)
+                        {
+                            // Remove all names and directions for OUT parameters because they are not included in PostgreSql's function signatures
+                            if (directions[i] == "o")
+                                names[i] = "";
+                        }
+                        names = (string.Join(",", names).Replace(",,", ",")).Split(',');
+                    }
+                    directions = (string.Join(",", directions).Replace("o", "").Replace(",,", ",")).Split(',');
                 }
 
                 command.Parameters.Clear();
@@ -161,20 +213,51 @@ namespace Npgsql
                             command.Parameters.Clear();
                             throw new InvalidOperationException(String.Format("Invalid parameter type: {0}", types[i]));
                         }
+
+                        NpgsqlParameter param = new NpgsqlParameter();
                         if (names != null && i < names.Length)
-                            command.Parameters.Add(new NpgsqlParameter(":" + names[i], typeInfo.NpgsqlDbType));
+                            param.ParameterName = ":" + names[i];
                         else
-                            command.Parameters.Add(new NpgsqlParameter("parameter" + (i + 1).ToString(), typeInfo.NpgsqlDbType));
+                            param.ParameterName = "parameter" + (i + 1).ToString();
+
+                        param.NpgsqlDbType = typeInfo.NpgsqlDbType;
+
+                        // See whether parameter direction is IN or INOUT
+                        // Note: OUT parameters are not checked because they were removed previously (just before this FOR loop).
+                        if ((directions != null) && (directions[i] == "b"))
+                            param.Direction = ParameterDirection.InputOutput;
+                        else
+                            param.Direction = ParameterDirection.Input;
+
+                        command.Parameters.Add(param);
                     }
                 }
             }
         }
 
+        /// <summary>
+        /// Gets the automatically generated <see cref="NpgsqlCommand"/> object required
+        /// to perform insertions at the data source.
+        /// </summary>
+        /// <returns>
+        /// The automatically generated <see cref="NpgsqlCommand"/> object required to perform insertions.
+        /// </returns>
         public new NpgsqlCommand GetInsertCommand()
         {
             return GetInsertCommand(false);
         }
 
+        /// <summary>
+        /// Gets the automatically generated <see cref="NpgsqlCommand"/> object required to perform insertions 
+        /// at the data source, optionally using columns for parameter names.
+        /// </summary>
+        /// <param name="useColumnsForParameterNames">
+        /// If <c>true</c>, generate parameter names matching column names, if possible. 
+        /// If <c>false</c>, generate @p1, @p2, and so on.
+        /// </param>
+        /// <returns>
+        /// The automatically generated <see cref="NpgsqlCommand"/> object required to perform insertions.
+        /// </returns>
         public new NpgsqlCommand GetInsertCommand(bool useColumnsForParameterNames)
         {
             NpgsqlCommand cmd = (NpgsqlCommand) base.GetInsertCommand(useColumnsForParameterNames);
@@ -182,11 +265,29 @@ namespace Npgsql
             return cmd;
         }
 
+        /// <summary>
+        /// Gets the automatically generated System.Data.Common.DbCommand object required
+        /// to perform updates at the data source.
+        /// </summary>
+        /// <returns>
+        /// The automatically generated System.Data.Common.DbCommand object required to perform updates.
+        /// </returns>
         public new NpgsqlCommand GetUpdateCommand()
         {
             return GetUpdateCommand(false);
         }
 
+        /// <summary>
+        /// Gets the automatically generated <see cref="NpgsqlCommand"/> object required to perform updates
+        /// at the data source, optionally using columns for parameter names.
+        /// </summary>
+        /// <param name="useColumnsForParameterNames">
+        /// If <c>true</c>, generate parameter names matching column names, if possible. 
+        /// If <c>false</c>, generate @p1, @p2, and so on.
+        /// </param>
+        /// <returns>
+        /// The automatically generated <see cref="NpgsqlCommand"/> object required to perform updates.
+        /// </returns>
         public new NpgsqlCommand GetUpdateCommand(bool useColumnsForParameterNames)
         {
             NpgsqlCommand cmd = (NpgsqlCommand)base.GetUpdateCommand(useColumnsForParameterNames);
@@ -194,11 +295,29 @@ namespace Npgsql
             return cmd;
         }
 
+        /// <summary>
+        /// Gets the automatically generated System.Data.Common.DbCommand object required
+        /// to perform deletions at the data source.
+        /// </summary>
+        /// <returns>
+        /// The automatically generated System.Data.Common.DbCommand object required to perform deletions.
+        /// </returns>
         public new NpgsqlCommand GetDeleteCommand()
         {
             return GetDeleteCommand(false);
         }
 
+        /// <summary>
+        /// Gets the automatically generated <see cref="NpgsqlCommand"/> object required to perform deletions 
+        /// at the data source, optionally using columns for parameter names.
+        /// </summary>
+        /// <param name="useColumnsForParameterNames">
+        /// If <c>true</c>, generate parameter names matching column names, if possible.
+        /// If <c>false</c>, generate @p1, @p2, and so on.
+        /// </param>
+        /// <returns>
+        /// The automatically generated <see cref="NpgsqlCommand"/> object required to perform deletions.
+        /// </returns>
         public new NpgsqlCommand GetDeleteCommand(bool useColumnsForParameterNames)
         {
             NpgsqlCommand cmd = (NpgsqlCommand) base.GetDeleteCommand(useColumnsForParameterNames);
@@ -229,6 +348,13 @@ namespace Npgsql
         }
 */
 
+        /// <summary>
+        /// Applies the parameter information.
+        /// </summary>
+        /// <param name="p">The parameter.</param>
+        /// <param name="row">The row.</param>
+        /// <param name="statementType">Type of the statement.</param>
+        /// <param name="whereClause">if set to <c>true</c> [where clause].</param>
         protected override void ApplyParameterInfo(DbParameter p, DataRow row, StatementType statementType, bool whereClause)
         {
 
@@ -252,21 +378,46 @@ namespace Npgsql
 
         }
 
+        /// <summary>
+        /// Returns the name of the specified parameter in the format of @p#.
+        /// </summary>
+        /// <param name="parameterOrdinal">The number to be included as part of the parameter's name..</param>
+        /// <returns>
+        /// The name of the parameter with the specified number appended as part of the parameter name.
+        /// </returns>
         protected override string GetParameterName(int parameterOrdinal)
         {
             return String.Format(CultureInfo.InvariantCulture, "@p{0}", parameterOrdinal);
         }
 
+        /// <summary>
+        /// Returns the full parameter name, given the partial parameter name.
+        /// </summary>
+        /// <param name="parameterName">The partial name of the parameter.</param>
+        /// <returns>
+        /// The full parameter name corresponding to the partial parameter name requested.
+        /// </returns>
         protected override string GetParameterName(string parameterName)
         {
             return String.Format(CultureInfo.InvariantCulture, "@{0}", parameterName);
         }
 
+        /// <summary>
+        /// Returns the placeholder for the parameter in the associated SQL statement.
+        /// </summary>
+        /// <param name="parameterOrdinal">The number to be included as part of the parameter's name.</param>
+        /// <returns>
+        /// The name of the parameter with the specified number appended.
+        /// </returns>
         protected override string GetParameterPlaceholder(int parameterOrdinal)
         {
             return GetParameterName(parameterOrdinal);
         }
 
+        /// <summary>
+        /// Registers the <see cref="T:NpgsqlCommandBuilder" /> to handle the <see cref="E:NpgsqlDataAdapter.RowUpdating"/> event for a <see cref="T:NpgsqlDataAdapter" />.
+        /// </summary>
+        /// <param name="adapter">The <see cref="T:System.Data.Common.DbDataAdapter" /> to be used for the update.</param>
         protected override void SetRowUpdatingHandler(DbDataAdapter adapter)
         {
 
@@ -289,12 +440,28 @@ namespace Npgsql
 
         }
 
+        /// <summary>
+        /// Adds an event handler for the <see cref="NpgsqlDataAdapter.RowUpdating"/> event.
+        /// </summary>
+        /// <param name="sender">The sender</param>
+        /// <param name="e">A <see cref="NpgsqlRowUpdatingEventArgs"/> instance containing information about the event.</param>
         private void RowUpdatingHandler(object sender, NpgsqlRowUpdatingEventArgs e)
 
         {
             base.RowUpdatingHandler(e);
         }
 
+        /// <summary>
+        /// Given an unquoted identifier in the correct catalog case, returns the correct quoted form of that identifier, including properly escaping any embedded quotes in the identifier.
+        /// </summary>
+        /// <param name="unquotedIdentifier">The original unquoted identifier.</param>
+        /// <returns>
+        /// The quoted version of the identifier. Embedded quotes within the identifier are properly escaped.
+        /// </returns>
+        /// <PermissionSet>
+        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" PathDiscovery="*AllFiles*" />
+        ///   </PermissionSet>
+        /// <exception cref="System.ArgumentNullException">Unquoted identifier parameter cannot be null</exception>
         public override string QuoteIdentifier(string unquotedIdentifier)
 
         {
@@ -307,6 +474,17 @@ namespace Npgsql
             return String.Format("{0}{1}{2}", this.QuotePrefix, unquotedIdentifier, this.QuoteSuffix);
         }
 
+        /// <summary>
+        /// Given a quoted identifier, returns the correct unquoted form of that identifier, including properly un-escaping any embedded quotes in the identifier.
+        /// </summary>
+        /// <param name="quotedIdentifier">The identifier that will have its embedded quotes removed.</param>
+        /// <returns>
+        /// The unquoted identifier, with embedded quotes properly un-escaped.
+        /// </returns>
+        /// <PermissionSet>
+        ///   <IPermission class="System.Security.Permissions.FileIOPermission, mscorlib, Version=2.0.3600.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" version="1" PathDiscovery="*AllFiles*" />
+        ///   </PermissionSet>
+        /// <exception cref="System.ArgumentNullException">Quoted identifier parameter cannot be null</exception>
         public override string UnquoteIdentifier(string quotedIdentifier)
 
         {
