b065ed8bcfb4486cf880a146535d2c0ec1227a6b
Francisco Figueiredo Jr <francisco@npgsql.org>
2014-03-12 12:45:34 -0300

    Add test for #184

--- tests/CommandTests.cs
+++ tests/CommandTests.cs
@@ -3508,5 +3508,36 @@ namespace NpgsqlTests
 
             Assert.IsTrue((bool)cmd.ExecuteScalar());
         }
+
+        [Test]
+        public void Bug184RollbackFailsOnAbortedTransaction()
+        {
+            NpgsqlConnectionStringBuilder csb = new NpgsqlConnectionStringBuilder(ConnectionString);
+            csb.CommandTimeout = 100000;
+
+            using (NpgsqlConnection connTimeoutChanged = new NpgsqlConnection(csb.ToString()))
+            {
+                connTimeoutChanged.Open();
+                using (var t = connTimeoutChanged.BeginTransaction())
+                {
+                    try
+                    {
+                        var command = new NpgsqlCommand("select count(*) from dta", connTimeoutChanged);
+                        command.Transaction = t;
+                        Object result = command.ExecuteScalar();
+
+
+                    }
+                    catch (Exception)
+                    {
+
+                        t.Rollback();
+                    }
+
+                }
+
+            }
+
+        }
     }
 }
