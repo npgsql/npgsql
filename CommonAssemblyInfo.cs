5ea3f3bc9a0f2effb7beda9c40df68d2a48a3990
Francisco Figueiredo Jr <francisco@npgsql.org>
2014-05-29 10:16:55 -0300

    Update Npgsql master branch version to 2.2.

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Resources;
 [assembly: AssemblyProductAttribute("Npgsql")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2002 - 2014 Npgsql Development Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("2.1.0")]
-[assembly: AssemblyFileVersionAttribute("2.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("2.1.0-rc1")]
+[assembly: AssemblyVersionAttribute("2.2.0")]
+[assembly: AssemblyFileVersionAttribute("2.2.0")]
+[assembly: AssemblyInformationalVersionAttribute("2.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en", UltimateResourceFallbackLocation.MainAssembly)]
 
