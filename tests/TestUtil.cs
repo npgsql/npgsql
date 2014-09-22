7cb67b24fd1abd16a8a31f706c7b4b11090f5a07
Shay Rojansky <roji@roji.org>
2014-09-22 08:03:00 +0300

    Added [IssueLink] for semantic markup of tests

--- tests/TestUtil.cs
+++ tests/TestUtil.cs
@@ -28,6 +28,20 @@ namespace NpgsqlTests
     }
 
     /// <summary>
+    /// Semantic attribute that points to an issue linked with this test (e.g. this
+    /// test reproduces the issue)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class IssueLink : Attribute
+    {
+        public string LinkAddress { get; private set; }
+        public IssueLink(string linkAddress)
+        {
+            LinkAddress = linkAddress;
+        }
+    }
+
+    /// <summary>
     /// Causes the test to be ignored on mono
     /// </summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false)]
