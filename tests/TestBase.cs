754199be00b3c98cf955c89ec00233696d8ce9a5
Shay Rojansky <roji@roji.org>
2014-09-22 08:03:00 +0300

    Added [IssueLink] for semantic markup of tests (cherry picked from commit 7cb67b24fd1abd16a8a31f706c7b4b11090f5a07)
    Conflicts:
    
    	tests/TestUtil.cs

--- tests/TestBase.cs
+++ tests/TestBase.cs
@@ -396,6 +396,20 @@ namespace NpgsqlTests
         #endregion
     }
 
+    /// <summary>
+    /// Semantic attribute that points to an issue linked with this test (e.g. this
+    /// test reproduces the issue)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class IssueLink : Attribute
+    {
+        public string LinkAddress { get; private set; }
+        public IssueLink(string linkAddress)
+        {
+            LinkAddress = linkAddress;
+        }
+    }
+
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false)]
     public class MonoIgnore : Attribute, ITestAction
     {
