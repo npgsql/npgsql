ac2bb1fac9fc3aa15174806099323de6068bcbf2
Shay Rojansky <roji@roji.org>
2014-11-23 22:30:34 +0200

    Various fixes - NextResult fix for multi-queries - Money type support - Brought back Japanese tweaks for column name lookup in RowDescription - NpgsqlDataReader.GetOrdinal() now works

--- Npgsql/TypeHandlers/MoneyHandler.cs
+++ Npgsql/TypeHandlers/MoneyHandler.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using Npgsql.Messages;
 
 namespace Npgsql.TypeHandlers
 {
-    internal class DecimalHandler : SimpleTypeHandler
+    internal class MoneyHandler : SimpleTypeHandler
     {
-        static readonly string[] _pgNames = { "decimal", "numeric" };
+        static readonly string[] _pgNames = { "money" };
         internal override string[] PgNames { get { return _pgNames; } }
         internal override Type FieldType { get { return typeof(decimal); } }
+        static readonly Regex EXCLUDE_DIGITS = new Regex("[^0-9\\-]", RegexOptions.Compiled | RegexOptions.CultureInvariant);
 
         internal override void ReadText(NpgsqlBufferedStream buf, int len, FieldDescription field, NpgsqlValue output)
         {
-            output.SetTo(Decimal.Parse(buf.ReadString(len)));
+            output.SetTo(Convert.ToDecimal(EXCLUDE_DIGITS.Replace(buf.ReadString(len), string.Empty), CultureInfo.InvariantCulture) / 100m);
         }
     }
 }
