3afdabd6a0fd1feae81c7c48c6a6d3aa8daca78b
Shay Rojansky <roji@roji.org>
2014-12-25 10:15:13 +0200

    Work on LastInsertedOID
    Moved from command to the reader, like RowsAffected, and made to work.

--- Npgsql/Messages/CommandCompleteMessage.cs
+++ Npgsql/Messages/CommandCompleteMessage.cs
@@ -8,13 +8,13 @@ namespace Npgsql.Messages
 {
     internal class CommandCompleteMessage : ServerMessage
     {
-        private int? _rowsAffected;
-        private long? _lastInsertedOID;
+        internal long? LastInsertedOID { get; private set; }
+        internal int? RowsAffected { get; private set; }
 
         internal CommandCompleteMessage Load(NpgsqlBuffer buf, int len)
         {
-            _rowsAffected = null;
-            _lastInsertedOID = null;
+            RowsAffected = null;
+            LastInsertedOID = null;
 
             var tag = buf.ReadString(len-1);
             buf.Skip(1);   // Null terminator
@@ -23,7 +23,10 @@ namespace Npgsql.Messages
             switch (tokens[0])
             {
                 case "INSERT":
-                    _lastInsertedOID = long.Parse(tokens[1]);
+                    var lastInsertedOID = long.Parse(tokens[1]);
+                    if (lastInsertedOID != 0) {
+                        LastInsertedOID = lastInsertedOID;
+                    }
                     goto case "UPDATE";
 
                 case "UPDATE":
@@ -31,21 +34,11 @@ namespace Npgsql.Messages
                 case "COPY":
                     int rowsAffected;
                     if (int.TryParse(tokens[tokens.Length - 1], out rowsAffected)) {
-                        _rowsAffected = rowsAffected;
+                        RowsAffected = rowsAffected;
                     }
                     break;
             }
             return this;
-        }
-
-        public long? LastInsertedOID
-        {
-            get { return _lastInsertedOID; }
-        }
-
-        public int? RowsAffected
-        {
-            get { return _rowsAffected; }
         }
 
         internal override BackEndMessageCode Code { get { return BackEndMessageCode.CompletedResponse; } }
