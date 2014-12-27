142b79c089607d142dc39faef1d4627c634531b7
Shay Rojansky <roji@roji.org>
2014-12-27 18:38:57 +0200

    Changes to CIDR network type
    New handler now always returns NpgsqlInet and not optionally IPAddress as before,
    since user will always be interested in the netmask component.
    
    Thanks @Emill

---
+++ Npgsql/TypeHandlers/NetworkHandlers/InetHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Npgsql.Messages;
+using NpgsqlTypes;
+
+namespace Npgsql.TypeHandlers.NetworkHandlers
+{
+    /// <remarks>
+    /// http://www.postgresql.org/docs/9.4/static/datatype-net-types.html
+    /// </remarks>
+    internal class InetHandler : TypeHandlerWithPsv<IPAddress, NpgsqlInet>, ITypeHandler<NpgsqlInet>,
+        ITypeHandler<string>
+    {
+        static readonly string[] _pgNames = { "inet" };
+        internal override string[] PgNames { get { return _pgNames; } }
+        public override bool SupportsBinaryRead { get { return true; } }
+
+        public override IPAddress Read(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
+        {
+            return ((ITypeHandler<NpgsqlInet>)this).Read(buf, fieldDescription, len).addr;
+        }
+
+        NpgsqlInet ITypeHandler<NpgsqlInet>.Read(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
+        {
+            switch (fieldDescription.FormatCode)
+            {
+                case FormatCode.Text:
+                    return new NpgsqlInet(buf.ReadString(len));
+                case FormatCode.Binary:
+                    return ReadBinary(buf, fieldDescription, len);
+                default:
+                    throw PGUtil.ThrowIfReached("Unknown format code: " + fieldDescription.FormatCode);
+            }
+        }
+
+        NpgsqlInet ReadBinary(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
+        {
+            var addressFamily = buf.ReadByte();
+            var mask = buf.ReadByte();
+            var isCidr = buf.ReadByte() == 1;
+            Contract.Assume(!isCidr);
+            var numBytes = buf.ReadByte();
+            var bytes = new byte[numBytes];
+            for (var i = 0; i < numBytes; i++) {
+                bytes[i] = buf.ReadByte();
+            }
+            return new NpgsqlInet(new IPAddress(bytes), mask);
+        }
+
+        string ITypeHandler<string>.Read(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
+        {
+            switch (fieldDescription.FormatCode)
+            {
+                case FormatCode.Text:
+                    return buf.ReadString(len);
+                case FormatCode.Binary:
+                    return ReadBinary(buf, fieldDescription, len).ToString();
+                default:
+                    throw PGUtil.ThrowIfReached("Unknown format code: " + fieldDescription.FormatCode);
+            }
+        }
+    }
+}
--- tests/Types/NetworkTypeTests.cs
+++ tests/Types/NetworkTypeTests.cs
@@ -80,27 +80,19 @@ namespace NpgsqlTests.Types
         [Test]
         public void ReadCidr([Values(PrepareOrNot.Prepared, PrepareOrNot.NotPrepared)] PrepareOrNot prepare)
         {
-            var expectedIp = IPAddress.Parse("192.168.1.0");
-            var expectedInet = new NpgsqlInet(expectedIp, 24);
+            var expectedInet = new NpgsqlInet("192.168.1.0/24");
             var cmd = new NpgsqlCommand("SELECT '192.168.1.0/24'::CIDR", Conn);
             if (prepare == PrepareOrNot.Prepared) { cmd.Prepare(); }
             var reader = cmd.ExecuteReader();
             reader.Read();
 
             // Regular type (IPAddress)
-            Assert.That(reader.GetFieldType(0), Is.EqualTo(typeof(IPAddress)));
-            Assert.That(reader.GetFieldValue<IPAddress>(0), Is.EqualTo(expectedIp));
-            Assert.That(reader[0], Is.EqualTo(expectedIp));
-            Assert.That(reader.GetValue(0), Is.EqualTo(expectedIp));
-            Assert.That(reader.GetString(0), Is.EqualTo("192.168.1.0/24"));
-            Assert.That(reader.GetFieldType(0), Is.EqualTo(typeof(IPAddress)));
-
-            // Provider-specific type (NpgsqlInet)
-            Assert.That(reader.GetProviderSpecificFieldType(0), Is.EqualTo(typeof(NpgsqlInet)));
-            Assert.That(reader.GetProviderSpecificValue(0), Is.EqualTo(expectedInet));
+            Assert.That(reader.GetFieldType(0), Is.EqualTo(typeof(NpgsqlInet)));
             Assert.That(reader.GetFieldValue<NpgsqlInet>(0), Is.EqualTo(expectedInet));
-            Assert.That(reader.GetString(0), Is.EqualTo(expectedInet.ToString()));
-            Assert.That(reader.GetProviderSpecificFieldType(0), Is.EqualTo(typeof(NpgsqlInet)));
+            Assert.That(reader[0], Is.EqualTo(expectedInet));
+            Assert.That(reader.GetValue(0), Is.EqualTo(expectedInet));
+            Assert.That(reader.GetString(0), Is.EqualTo("192.168.1.0/24"));
+            Assert.That(reader.GetFieldType(0), Is.EqualTo(typeof(NpgsqlInet)));
 
             reader.Dispose();
             cmd.Dispose();
