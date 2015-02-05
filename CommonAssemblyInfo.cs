7361b89efda819c70a451e68075e2505e6cd221a
Shay Rojansky <roji@roji.org>
2015-02-05 23:26:10 +0100

    And one last version bump, this time with AssemblyVersion

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Resources;
 [assembly: AssemblyProductAttribute("Npgsql")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2002 - 2014 Npgsql Development Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("2.2.4")]
-[assembly: AssemblyFileVersionAttribute("2.2.4")]
-[assembly: AssemblyInformationalVersionAttribute("2.2.4")]
+[assembly: AssemblyVersionAttribute("2.2.4.3")]
+[assembly: AssemblyFileVersionAttribute("2.2.4.3")]
+[assembly: AssemblyInformationalVersionAttribute("2.2.4.3")]
 [assembly: NeutralResourcesLanguageAttribute("en", UltimateResourceFallbackLocation.MainAssembly)]
 
