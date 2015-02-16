4d1dd4cb63b036e23a79bf09d14b21a3dc48523b
Shay Rojansky <roji@roji.org>
2015-02-16 09:18:13 +0200

    Removed some dead code

--- Npgsql/TypeHandlers/GeometricHandlers/PolygonHandler.cs
+++ Npgsql/TypeHandlers/GeometricHandlers/PolygonHandler.cs
@@ -16,7 +16,6 @@ namespace Npgsql.TypeHandlers.GeometricHandlers
     /// <remarks>
     /// http://www.postgresql.org/docs/9.4/static/datatype-geometric.html
     /// </remarks>
-    // TODO: Should be chunking
     [TypeMapping("polygon", NpgsqlDbType.Polygon, typeof(NpgsqlPolygon))]
     internal class PolygonHandler : TypeHandler<NpgsqlPolygon>,
         IChunkingTypeReader<NpgsqlPolygon>, IChunkingTypeWriter
@@ -100,24 +99,5 @@ namespace Npgsql.TypeHandlers.GeometricHandlers
         }
 
         #endregion
-
-#if OLD
-        public NpgsqlPolygon Read(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
-        {
-            var numPoints = buf.ReadInt32();
-            var points = new List<NpgsqlPoint>(numPoints);
-            for (var i = 0; i < numPoints; i++) {
-                if (buf.ReadBytesLeft < sizeof(double) * 2)
-                    buf.Ensure(Math.Min(sizeof(double) * 2 * (numPoints - i), buf.Size & -(sizeof(double) * 2)));
-                points.Add(new NpgsqlPoint(buf.ReadDouble(), buf.ReadDouble()));
-            }
-            return new NpgsqlPolygon(points);
-        }
-
-        string ISimpleTypeReader<string>.Read(NpgsqlBuffer buf, FieldDescription fieldDescription, int len)
-        {
-            return Read(buf, fieldDescription, len).ToString();
-        }
-#endif
     }
 }
