1d31a35fd885c4657bff0d2cf5cb8c88ebb1a2c5
Shay Rojansky <roji@roji.org>
2015-03-02 05:45:46 +0200

    Bump version to 2.2.5

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -15,8 +15,8 @@ using System.Resources;
 [assembly: AssemblyProductAttribute("Npgsql")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2002 - 2014 Npgsql Development Team")]
 [assembly: AssemblyTrademarkAttribute("")]
-[assembly: AssemblyVersionAttribute("2.2.4.3")]
-[assembly: AssemblyFileVersionAttribute("2.2.4.3")]
-[assembly: AssemblyInformationalVersionAttribute("2.2.4.3")]
+[assembly: AssemblyVersionAttribute("2.2.5")]
+[assembly: AssemblyFileVersionAttribute("2.2.5")]
+[assembly: AssemblyInformationalVersionAttribute("2.2.5")]
 [assembly: NeutralResourcesLanguageAttribute("en", UltimateResourceFallbackLocation.MainAssembly)]
 
