c6ed644dfee7ce52302176222bd1a1473ce03740
Shay Rojansky <roji@roji.org>
2015-04-16 10:59:57 +0300

    Commit missing file

---
+++ src/Npgsql/BackendMessages/BackendKeyDataMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Npgsql.BackendMessages
+{
+    internal class BackendKeyDataMessage : IBackendMessage
+    {
+        public BackendMessageCode Code { get { return BackendMessageCode.BackendKeyData; } }
+
+        internal int BackendProcessId { get; private set; }
+        internal int BackendSecretKey { get; private set; }
+
+        internal BackendKeyDataMessage(NpgsqlBuffer buf)
+        {
+            BackendProcessId = buf.ReadInt32();
+            BackendSecretKey = buf.ReadInt32();
+        }
+    }
+}
