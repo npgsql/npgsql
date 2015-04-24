8df5acf38ed10716bb11d67f5aeb41d4ec60a000
Shay Rojansky <roji@roji.org>
2015-04-24 15:16:08 +0300

    [EF7] Added QueryNpgsqlTest suite

---
+++ test/Npgsql.EntityFramework7.FunctionalTests/QueryNpgsqlTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.Data.Entity.FunctionalTests;
+using Microsoft.Data.Entity.FunctionalTests.TestModels.Northwind;
+using Microsoft.Data.Entity.Relational.FunctionalTests;
+using Microsoft.Data.Entity.Storage;
+using Xunit;
+
+#if DNXCORE50
+using System.Threading;
+#endif
+
+namespace Npgsql.EntityFramework7.FunctionalTests
+{
+    public class QueryNpgsqlTest : QueryTestBase<NorthwindQueryNpgsqlFixture>
+    {
+        public QueryNpgsqlTest(NorthwindQueryNpgsqlFixture fixture)
+            : base(fixture)
+        {
+        }
+    }
+}
