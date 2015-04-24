991a4ac3804c9d0d7604dee82eca1b33f45857d8
Shay Rojansky <roji@roji.org>
2015-04-24 14:05:10 +0300

    [EF7] Implemented Take() with LIMIT

--- src/Npgsql.EntityFramework7/Query/NpgsqlQueryGenerator.cs
+++ src/Npgsql.EntityFramework7/Query/NpgsqlQueryGenerator.cs
@@ -1,11 +1,13 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.Data.Entity.Relational.Query.Expressions;
 using Microsoft.Data.Entity.Relational.Query.Sql;
 using Microsoft.Data.Entity.Utilities;
+using System.Linq;
 
 namespace Npgsql.EntityFramework7.Query
 {
@@ -18,7 +20,27 @@ namespace Npgsql.EntityFramework7.Query
 
         protected override void GenerateTop([NotNull]SelectExpression selectExpression)
         {
-            // No TOP in postgres
+            // No TOP() in PostgreSQL, see GenerateLimitOffset
+        }
+
+        protected override void GenerateLimitOffset([NotNull] SelectExpression selectExpression)
+        {
+            Check.NotNull(selectExpression, nameof(selectExpression));
+
+            if (selectExpression.Limit != null)
+            {
+                Sql.Append(" LIMIT ").Append(selectExpression.Limit);
+            }
+
+            if (selectExpression.Offset != null)
+            {
+                if (!selectExpression.OrderBy.Any())
+                {
+                    throw new InvalidOperationException(Microsoft.Data.Entity.Relational.Strings.SkipNeedsOrderBy);
+                }
+
+                Sql.Append(" OFFSET ").Append(selectExpression.Offset);
+            }
         }
 
         public override Expression VisitCountExpression(CountExpression countExpression)
