df439ec707522ebf4d26f661cecc68c8cc996b8e
Shay Rojansky <roji@roji.org>
2015-06-23 17:16:30 +0300

    [EF7] Skip 4 tests
    Which depend on https://github.com/aspnet/EntityFramework/pull/2447

--- test/Npgsql.EntityFramework7.FunctionalTests/BuiltInDataTypesNpgsqlTest.cs
+++ test/Npgsql.EntityFramework7.FunctionalTests/BuiltInDataTypesNpgsqlTest.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.FunctionalTests;
+using Xunit;
 
 namespace Npgsql.EntityFramework7.FunctionalTests
 {
@@ -11,6 +12,18 @@ namespace Npgsql.EntityFramework7.FunctionalTests
             : base(fixture)
         {
         }
+
+        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
+        public override void Can_insert_and_read_back_all_nullable_data_types_with_values_set_to_non_null() {}
+
+        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
+        public override void Can_query_using_any_data_type() { }
+
+        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
+        public override void Can_insert_and_read_back_all_non_nullable_data_types() { }
+
+        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
+        public override void Can_query_using_any_nullable_data_type() { }
 
         // TODO: Other tests like in BuiltInDataTypesSqlServerTest?
     }
