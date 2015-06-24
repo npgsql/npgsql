530853d344f1fa7bdf911386a62830918a50e278
Shay Rojansky <roji@roji.org>
2015-06-24 22:03:25 +0300

    [EF7] Reenabled disabled tests

--- test/Npgsql.EntityFramework7.FunctionalTests/BuiltInDataTypesNpgsqlTest.cs
+++ test/Npgsql.EntityFramework7.FunctionalTests/BuiltInDataTypesNpgsqlTest.cs
@@ -13,18 +13,6 @@ namespace Npgsql.EntityFramework7.FunctionalTests
         {
         }
 
-        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
-        public override void Can_insert_and_read_back_all_nullable_data_types_with_values_set_to_non_null() {}
-
-        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
-        public override void Can_query_using_any_data_type() { }
-
-        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
-        public override void Can_insert_and_read_back_all_non_nullable_data_types() { }
-
-        [Fact(Skip = "https://github.com/aspnet/EntityFramework/pull/2447")]
-        public override void Can_query_using_any_nullable_data_type() { }
-
         // TODO: Other tests like in BuiltInDataTypesSqlServerTest?
     }
 }
