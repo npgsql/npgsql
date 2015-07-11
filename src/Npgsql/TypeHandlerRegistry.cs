d66bb06d1a75d0c8409ce479e8889f0d097ba5d8
Shay Rojansky <roji@roji.org>
2015-07-12 00:35:04 +0300

    Silence useless type loading warnings
    When loading types, some warnings were logged about arrays with absent element
    types. This was because the element types weren't loaded, because they had no
    binary I/O functions. Added an IgnoredType set.
    
    (cherry picked from commit 2e248d1ed2f28368d374d1a885b98c945a903608)

--- src/Npgsql/TypeHandlerRegistry.cs
+++ src/Npgsql/TypeHandlerRegistry.cs
@@ -56,6 +56,14 @@ namespace Npgsql
         static readonly Dictionary<Type, DbType> TypeToDbType;
 
         /// <summary>
+        /// Types that aren't to be loaded.
+        /// Currently contains arrays of types without binary I/O.
+        /// </summary>
+        static readonly HashSet<string> IgnoredTypes = new HashSet<string> {
+            "_aclitem", "_ghstore", "_gtsvector"
+        };
+
+        /// <summary>
         /// Caches, for each connection string, the results of the backend type query in the form of a list of type
         /// info structs keyed by the PG name.
         /// Repeated connections to the same connection string reuse the query results and avoid an additional
@@ -136,6 +144,10 @@ namespace Npgsql
 
                         Contract.Assume(backendType.Name != null);
                         Contract.Assume(backendType.OID != 0);
+
+                        if (IgnoredTypes.Contains(backendType.Name)) {
+                            continue;
+                        }
 
                         uint elementOID;
                         var typeChar = dr.GetString(2)[0];
@@ -648,6 +660,14 @@ namespace Npgsql
         internal BackendTypeType Type;
         internal BackendType Element;
         internal BackendType Array;
+
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 
     struct TypeAndMapping
