ac4ca25e5bf9715048a638a5c194d8aa4653bd80
Emil Lenngren <emil.lenngren@gmail.com>
2015-08-14 01:14:08 +0200

    Fix EF6 test for older PostgreSQL versions by changing json to varbit.
    (cherry picked from commit fe829d88a98d8a3e0eb02fdc3db6470c1c69c47e)

--- test/EntityFramework6.Npgsql.Tests/EntityFrameworkBasicTests.cs
+++ test/EntityFramework6.Npgsql.Tests/EntityFrameworkBasicTests.cs
@@ -36,7 +36,7 @@ namespace EntityFramework6.Npgsql.Tests
                 createSequenceConn.Open();
                 ExecuteNonQuery("create sequence blog_int_computed_value_seq", createSequenceConn);
                 ExecuteNonQuery("alter table \"dbo\".\"Blogs\" alter column \"IntComputedValue\" set default nextval('blog_int_computed_value_seq');", createSequenceConn);
-                ExecuteNonQuery("alter table \"dbo\".\"Posts\" alter column \"JsonColumn\" type json using null", createSequenceConn);
+                ExecuteNonQuery("alter table \"dbo\".\"Posts\" alter column \"VarbitColumn\" type varbit using null", createSequenceConn);
 
             }
 
@@ -76,7 +76,7 @@ namespace EntityFramework6.Npgsql.Tests
             public string Content { get; set; }
             public byte Rating { get; set; }
             public DateTime CreationDate { get; set; }
-            public string JsonColumn { get; set; }
+            public string VarbitColumn { get; set; }
             public int BlogId { get; set; }
             public virtual Blog Blog { get; set; }
         }
@@ -101,7 +101,7 @@ namespace EntityFramework6.Npgsql.Tests
         [Test]
         public void InsertAndSelect()
         {
-            var jsonVal = "{\"key\":\"value\"}";
+            var varbitVal = "10011";
 
             using (var context = new BloggingContext(ConnectionStringEF))
             {
@@ -116,7 +116,7 @@ namespace EntityFramework6.Npgsql.Tests
                         Content = "Some post content " + i,
                         Rating = (byte)i,
                         Title = "Some post Title " + i,
-                        JsonColumn = jsonVal
+                        VarbitColumn = varbitVal
                     });
                 context.Blogs.Add(blog);
                 context.NoColumnsEntities.Add(new NoColumnsEntity());
@@ -131,10 +131,12 @@ namespace EntityFramework6.Npgsql.Tests
                 foreach (var post in posts)
                 {
                     StringAssert.StartsWith("Some post Title ", post.Title);
-                    Assert.AreEqual(jsonVal, post.JsonColumn);
+                    Assert.AreEqual(varbitVal, post.VarbitColumn);
                 }
                 var someParameter = "Some";
                 Assert.IsTrue(context.Posts.Any(p => p.Title.StartsWith(someParameter)));
+                Assert.IsTrue(context.Posts.Select(p => p.VarbitColumn == varbitVal).First());
+                Assert.IsTrue(context.Posts.Select(p => p.VarbitColumn == "10011").First());
                 Assert.AreEqual(1, context.NoColumnsEntities.Count());
             }
         }
