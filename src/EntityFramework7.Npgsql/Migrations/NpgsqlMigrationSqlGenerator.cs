aea087722109708b0e7ee5895068c6b7f6e007f8
Cezar Cretu <cretucezarandrei@gmail.com>
2015-08-16 06:39:19 +0300

    fix generated sql for DropDatabaseOperation
    databse name was quoted as identifier even when used as string

--- src/EntityFramework7.Npgsql/Migrations/NpgsqlMigrationSqlGenerator.cs
+++ src/EntityFramework7.Npgsql/Migrations/NpgsqlMigrationSqlGenerator.cs
@@ -196,7 +196,7 @@ namespace EntityFramework7.Npgsql.Migrations
                 // TODO: For PG <= 9.1, the column name is prodpic, not pid (see http://stackoverflow.com/questions/5408156/how-to-drop-a-postgresql-database-if-there-are-active-connections-to-it)
                 .Append(
                     "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '")
-                .Append(dbName)
+                .Append(operation.Name)
                 .Append("'")
                 .EndBatch()
                 .Append("DROP DATABASE ")
