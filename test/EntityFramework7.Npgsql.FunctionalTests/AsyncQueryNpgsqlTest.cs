48ccbd77e0141ce3bb41ab3b83761e38feddd821
Cezar Cretu <cretucezarandrei@gmail.com>
2015-08-16 21:36:23 +0300

    include async tests + ~minimal test case for async deadlock

---
+++ test/EntityFramework7.Npgsql.FunctionalTests/AsyncQueryNpgsqlTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.FunctionalTests;
+using Xunit;
+
+namespace EntityFramework7.Npgsql.FunctionalTests
+{
+    public class AsyncQueryNpgsqlTest : AsyncQueryTestBase<NorthwindQueryNpgsqlFixture>
+    {
+        private readonly NorthwindQueryNpgsqlFixture _fixture;
+
+        public AsyncQueryNpgsqlTest(NorthwindQueryNpgsqlFixture fixture) : base(fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Completes_When_No_Results()
+        {
+            using (var db = _fixture.CreateContext())
+            {
+                await db.Customers.Where(r => r.Address == "notindatabase").FirstOrDefaultAsync();
+            }
+        }
+    }
+}
