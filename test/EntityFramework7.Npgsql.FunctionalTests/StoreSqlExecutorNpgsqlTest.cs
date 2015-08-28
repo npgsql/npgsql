efde78323ce7e924da38438ad615eff194f83007
Shay Rojansky <roji@roji.org>
2015-08-28 11:04:26 +0200

    [EF7] Introduced StoreSqlExecutor functional test suite

---
+++ test/EntityFramework7.Npgsql.FunctionalTests/StoreSqlExecutorNpgsqlTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.FunctionalTests;
+using Xunit;
+
+namespace EntityFramework7.Npgsql.FunctionalTests
+{
+    public class StoreSqlExecutorNpgsqlTest : StoreSqlExecutorTestBase<NorthwindQueryNpgsqlFixture>
+    {
+        public override void Executes_stored_procedure()
+        {
+            base.Executes_stored_procedure();
+
+            Assert.Equal(
+                TenMostExpensiveProductsSproc,
+                Sql.ToUnixNewlines());
+        }
+
+        public override void Executes_stored_procedure_with_parameter()
+        {
+            base.Executes_stored_procedure_with_parameter();
+
+            Assert.Equal(
+                @"@p0: ALFKI
+
+SELECT * FROM ""CustOrderHist""(@p0)",
+                Sql.ToUnixNewlines());
+        }
+
+        public StoreSqlExecutorNpgsqlTest(NorthwindQueryNpgsqlFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        protected override string TenMostExpensiveProductsSproc => @"SELECT * FROM ""Ten Most Expensive Products""()";
+
+        protected override string CustomerOrderHistorySproc => @"SELECT * FROM ""CustOrderHist""({0})";
+
+        private static string Sql => TestSqlLoggerFactory.Sql;
+    }
+}
