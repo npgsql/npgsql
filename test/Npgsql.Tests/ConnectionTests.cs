5621c33bed0121e222ecd459eed491e75b6bed03
Shay Rojansky <roji@roji.org>
2015-08-31 21:35:36 +0200

    Proper exception for BeginTransaction before Open

--- test/Npgsql.Tests/ConnectionTests.cs
+++ test/Npgsql.Tests/ConnectionTests.cs
@@ -523,6 +523,15 @@ namespace Npgsql.Tests
         }
 
         [Test]
+        public void BeginTransactionBeforeOpen()
+        {
+            using (var conn = new NpgsqlConnection())
+            {
+                Assert.That(() => conn.BeginTransaction(), Throws.Exception.TypeOf<InvalidOperationException>());
+            }
+        }
+
+        [Test]
         public void SequencialTransaction()
         {
             Conn.BeginTransaction().Rollback();
