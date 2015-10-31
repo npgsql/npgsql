fab5b4987dd76e7613395dc6165122bd51117a44
Shay Rojansky <roji@roji.org>
2015-10-31 19:36:40 +0200

    Socket async connect fix for CoreCLR

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -654,7 +654,11 @@ namespace Npgsql
                 Log.Trace("Attempting to connect to " + ips[i], Id);
                 var ep = new IPEndPoint(ips[i], Port);
                 var socket = new Socket(ep.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+#if DNXCORE50
+                var connectTask = socket.ConnectAsync(ep);
+#else
                 var connectTask = Task.Factory.FromAsync(socket.BeginConnect, socket.EndConnect, ep, null);
+#endif
                 try
                 {
                     try
