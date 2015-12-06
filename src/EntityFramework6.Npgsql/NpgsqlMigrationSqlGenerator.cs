1e51fe21469c3f54e713e246918d2ca6b6bc0f11
Shay Rojansky <roji@eaglets.co.il>
2015-12-06 17:57:04 +0200

    [EF6] No longer attempt to create the public schema
    If a table's schema is public, don't send a CREATE SCHEMA statement.
    This can be a workaround for users of PostgreSQL <= 9.2, which doesn't
    have CREATE SCHEMA IF NOT EXISTS statement.
    
    (cherry picked from commit 1edfa36a6bc6245e7c99e7ea1b0f1379dfc73d91)

--- src/EntityFramework6.Npgsql/NpgsqlMigrationSqlGenerator.cs
+++ src/EntityFramework6.Npgsql/NpgsqlMigrationSqlGenerator.cs
@@ -29,6 +29,7 @@ using System.Globalization;
 using System.Text;
 using System.Data.Entity.Core.Metadata.Edm;
 using System.Data.Entity.Spatial;
+using System.Linq;
 
 namespace Npgsql
 {
@@ -181,7 +182,12 @@ namespace Npgsql
         protected virtual void Convert(CreateTableOperation createTableOperation)
         {
             StringBuilder sql = new StringBuilder();
-            CreateSchema(createTableOperation.Name);
+            int dotIndex = createTableOperation.Name.IndexOf('.');
+            if (dotIndex != -1)
+            {
+                CreateSchema(createTableOperation.Name.Remove(dotIndex));
+            }
+
             sql.Append("CREATE TABLE ");
             AppendTableName(createTableOperation.Name, sql);
             sql.Append('(');
@@ -190,7 +196,8 @@ namespace Npgsql
                 AppendColumn(column, sql);
                 sql.Append(",");
             }
-            sql.Remove(sql.Length - 1, 1);
+            if (createTableOperation.Columns.Any())
+                sql.Remove(sql.Length - 1, 1);
             if (createTableOperation.PrimaryKey != null)
             {
                 sql.Append(",");
@@ -233,10 +240,7 @@ namespace Npgsql
 
         private void CreateSchema(string schemaName)
         {
-            int dotIndex = schemaName.IndexOf('.');
-            if (dotIndex != -1)
-                schemaName = schemaName.Remove(dotIndex);
-            if (addedSchemas.Contains(schemaName))
+            if (schemaName == "public" || addedSchemas.Contains(schemaName))
                 return;
             addedSchemas.Add(schemaName);
             if (serverVersion.Major > 9 || (serverVersion.Major == 9 && serverVersion.Minor >= 3))
