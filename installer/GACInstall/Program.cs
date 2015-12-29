6186352fdd726e6cbca503b0a0c1ab27d4d512e3
kenjiuno <h8mastre@gmail.com>
2015-12-29 19:05:59 +0900

    Add Npgsql V2.x setup.
    - Open notepad "installer/installer.csproj" for detail instructions.

---
+++ installer/GACInstall/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.EnterpriseServices.Internal;
+using System.IO;
+using System.Reflection;
+
+namespace GACInstall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (String a in args)
+            {
+                if (File.Exists(a))
+                {
+                    String fp = Path.GetFullPath(a);
+                    Console.WriteLine(Assembly.LoadFile(fp).FullName);
+                    new Publish().GacInstall(fp);
+                }
+            }
+        }
+    }
+}
