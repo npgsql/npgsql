b14cb649047cb18cecd10503f679fdb0fe345be6
Licshee <licshee@yen.ai>
2016-04-24 18:59:40 +0800

    Remove unnecessary extension methods

--- src/Npgsql/StringEx.cs
+++ src/Npgsql/StringEx.cs
@@ -31,65 +31,6 @@ static partial class StringEx
         return true;
     }
 
-#if !PCL
-    public static string IsInterned(this string value)
-    {
-        if (value == null)
-            throw new ArgumentNullException(nameof(value));
-
-        return string.IsInterned(value);
-    }
-
-    public static string Intern(this string value)
-    {
-        if (value == null)
-            throw new ArgumentNullException(nameof(value));
-
-        return string.Intern(value);
-    }
-#endif
-
-#if UNSAFE
-    public static unsafe string ToLowerForASCII(this string value)
-    {
-        if (value.IsNullOrWhiteSpace())
-            return value;
-
-        value = string.Copy(value);
-        fixed (char* low = value)
-        {
-            var end = low + value.Length;
-            for (var p = low; p < end; p++)
-            {
-                var c = *p;
-                if (c < 'A' || c > 'Z')
-                    continue;
-                *p = (char)(c + 0x20);
-            }
-        }
-        return value;
-    }
-
-    public static unsafe string ToUpperForASCII(this string value)
-    {
-        if (value.IsNullOrWhiteSpace())
-            return value;
-
-        value = string.Copy(value);
-        fixed (char* low = value)
-        {
-            var end = low + value.Length;
-            for (var p = low; p < end; p++)
-            {
-                var c = *p;
-                if (c < 'a' || c > 'z')
-                    continue;
-                *p = (char)(c - 0x20);
-            }
-        }
-        return value;
-    }
-#else
     public static string ToLowerForASCII(this string value)
     {
         if (value.IsNullOrWhiteSpace())
@@ -121,7 +62,6 @@ static partial class StringEx
         }
         return sb.ToString();
     }
-#endif
 
     #endregion
 
