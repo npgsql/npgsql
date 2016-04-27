4e7f3900463baa05aaf7fb2cc10debc6c0cd99c6
Shay Rojansky <roji@roji.org>
2016-04-27 18:17:44 +0300

    Fix Break/BreakFromOpen logic issue
    The fix for #918 introduced some nonsense where a break during connection
    was routed through a special BreakFromOpen method. This didn't really hold -
    some breaks down the stack still called into Break and triggered some
    contract violations. There's also need real need for this distinction.
    
    Fixes #1046
    
    (cherry picked from commit 4f9fceef99b0eb4a6c36fa3c8a83fe7483a0f39e)

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -456,7 +456,7 @@ namespace Npgsql
             }
             catch
             {
-                BreakFromOpen();
+                Break();
                 throw;
             }
         }
@@ -1420,7 +1420,6 @@ namespace Npgsql
         internal void Break()
         {
             Contract.Requires(!IsClosed);
-            Contract.Requires(State != ConnectorState.Connecting);
 
             if (State == ConnectorState.Broken)
                 return;
@@ -1434,24 +1433,15 @@ namespace Npgsql
             // We have no connection if we're broken by a keepalive occuring while the connector is in the pool
             if (conn != null)
             {
-                // The connection's full state is usually calculated from the connector's, but in states closed/broken the
-                // connector is null. We therefore need a way to distinguish between Closed and Broken on the connection.
+                // When we break, we normally need to call into NpgsqlConnection to reset its state.
+                // The exception to this is when we're connecting, in which case the try/catch in NpgsqlConnection.Open
+                // will handle things.
+                // Note also that the connection's full state is usually calculated from the connector's, but in
+                // states closed/broken the connector is null. We therefore need a way to distinguish between
+                // Closed and Broken on the connection.
                 if (prevState != ConnectorState.Connecting)
-                conn.ReallyClose(true);
+                    conn.ReallyClose(true);
             }
-        }
-
-        /// <summary>
-        /// Called when an open attempt fails (e.g. I/O error, timeout).
-        /// </summary>
-        internal void BreakFromOpen()
-        {
-            Contract.Requires(State == ConnectorState.Connecting);
-            Contract.Requires(Connection != null);
-
-            Log.Trace("Break connector during Open", Id);
-            State = ConnectorState.Broken;
-            Cleanup();
         }
 
         /// <summary>
