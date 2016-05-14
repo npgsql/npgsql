be4d5402643b27e4be37bb1c66cf36708ba477b8
Shay Rojansky <roji@roji.org>
2016-05-14 19:39:29 +0300

    Some fixes to hstore handler
    To make it possible to work with both Dictionary<string, string> and
    IDictionary<string, string>.
    
    Fixes #1088
    
    (cherry picked from commit 3c7af494a1a3692c9f08362013828f294c28bea1)

--- test/Npgsql.Tests/Types/MiscTypeTests.cs
+++ test/Npgsql.Tests/Types/MiscTypeTests.cs
@@ -210,18 +210,21 @@ namespace Npgsql.Tests.Types
               {"cd", "hello"}
             };
 
-            using (var cmd = new NpgsqlCommand("SELECT @p1, @p2", Conn)) {
+            using (var cmd = new NpgsqlCommand("SELECT @p1, @p2", Conn))
+            {
                 cmd.Parameters.AddWithValue("p1", NpgsqlDbType.Hstore, expected);
                 cmd.Parameters.AddWithValue("p2", expected);
-                var reader = cmd.ExecuteReader();
-                reader.Read();
-                for (var i = 0; i < cmd.Parameters.Count; i++)
+                using (var reader = cmd.ExecuteReader())
                 {
-                    Assert.That(reader.GetFieldType(i), Is.EqualTo(typeof(IDictionary<string, string>)));
-                    Assert.That(reader.GetValue(i), Is.EqualTo(expected));
-                    Assert.That(reader.GetString(i), Is.EqualTo(@"""a""=>""3"",""b""=>NULL,""cd""=>""hello"""));
+                    reader.Read();
+                    for (var i = 0; i < cmd.Parameters.Count; i++)
+                    {
+                        Assert.That(reader.GetFieldType(i), Is.EqualTo(typeof(Dictionary<string, string>)));
+                        Assert.That(reader.GetFieldValue<Dictionary<string, string>>(i), Is.EqualTo(expected));
+                        Assert.That(reader.GetFieldValue<IDictionary<string, string>>(i), Is.EqualTo(expected));
+                        Assert.That(reader.GetString(i), Is.EqualTo(@"""a""=>""3"",""b""=>NULL,""cd""=>""hello"""));
+                    }
                 }
-                reader.Close();
             }
         }
 
