2a113a1b6ce921acfe8af25dfae047d9ade8be15
Shay Rojansky <roji@roji.org>
2016-05-19 14:39:39 +0400

    Bump version to 3.1.1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Resources;
 // The following version attributes get rewritten by GitVersion as part of the build
 [assembly: AssemblyVersion("3.1.1")]
 [assembly: AssemblyFileVersion("3.1.1")]
-[assembly: AssemblyInformationalVersion("3.1.1-ci")]
+[assembly: AssemblyInformationalVersion("3.1.1")]
--- teamcity_set_version.cmd
+++ teamcity_set_version.cmd
@@ -1,1 +1,1 @@
-echo ##teamcity[buildNumber '3.1.1-ci-%1']
+echo ##teamcity[buildNumber '3.1.1-%1']
