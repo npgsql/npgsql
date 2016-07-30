42bb1e8d2031c99f18b2cf6ed71534ba13a08de4
Shay Rojansky <roji@roji.org>
2016-07-30 11:06:08 +0000

    Add Npgsql.Benchmarks project
    With BenchmarkDotNet
    
    Only one trivial benchmark for now, to test connection creation

---
+++ test/Npgsql.Benchmarks/Connection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace Npgsql.Benchmarks
+{
+    public class Connection
+    {
+        static readonly NpgsqlConnectionStringBuilder ConnectionStringBuilder = new NpgsqlConnectionStringBuilder(ConnectionString);
+        const string ConnectionString = "Host=foo;Database=bar;Username=user;Password=password";
+
+        [Benchmark]
+        public NpgsqlConnection CreateWithoutConnectionString()
+            => new NpgsqlConnection(ConnectionStringBuilder);
+
+        [Benchmark]
+        public NpgsqlConnection CreateWithConnectionString()
+            => new NpgsqlConnection(ConnectionString);
+    }
+}
