d95d6a9aa70c8f2ef1b479fa1f206b5179b1e0cb
rwasef1830 <beliskner.github.3psil0N@neverbox.com>
2016-10-02 23:34:53 +0200

    Don't mutate the shared Settings property to get a passwordless connection string. Fixes #1306.
    (cherry picked from commit bcbf936af68cfa42c306fb781bcd755c88bd247d)

--- src/Npgsql/NpgsqlConnectionStringBuilder.cs
+++ src/Npgsql/NpgsqlConnectionStringBuilder.cs
@@ -263,6 +263,11 @@ namespace Npgsql
             } else {
                 base[canonicalKeyword] = value;
             }
+
+            lock (_connectionStringWithoutPasswordLocker)
+            {
+                _connectionStringWithoutPassword = null;
+            }
         }
 
         #endregion
@@ -988,6 +993,25 @@ namespace Npgsql
         #endregion
 
         #region Misc
+
+        internal string ToStringWithoutPassword()
+        {
+            lock (_connectionStringWithoutPasswordLocker)
+            {
+                if (_connectionStringWithoutPassword != null)
+                {
+                    return _connectionStringWithoutPassword;
+                }
+
+                var clone = Clone();
+                clone.Password = null;
+                _connectionStringWithoutPassword = clone.ToString();
+                return _connectionStringWithoutPassword;
+            }
+        }
+
+        readonly object _connectionStringWithoutPasswordLocker = new object();
+        string _connectionStringWithoutPassword;
 
         internal NpgsqlConnectionStringBuilder Clone()
         {
