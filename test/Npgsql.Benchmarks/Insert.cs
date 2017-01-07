968d2df483c8fc7b7fbfc63c10d1a0f3d5211d50
Shay Rojansky <roji@roji.org>
2017-01-07 19:56:04 +0200

    Insert benchmarks

---
+++ test/Npgsql.Benchmarks/Insert.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using NpgsqlTypes;
+
+namespace Npgsql.Benchmarks
+{
+    public class Insert
+    {
+        NpgsqlConnection _conn;
+        NpgsqlCommand _truncateCmd;
+
+        [Params(1, 100, 1000, 10000)]
+        public int BatchSize { get; set; }
+
+        [Setup]
+        public void Setup()
+        {
+            var csb = new NpgsqlConnectionStringBuilder(BenchmarkEnvironment.ConnectionString)
+            {
+                Pooling = false
+            };
+            _conn = new NpgsqlConnection(csb);
+            _conn.Open();
+
+            using (var cmd = new NpgsqlCommand("CREATE TEMP TABLE data (int1 INT4, text1 TEXT, int2 INT4, text2 TEXT)", _conn))
+                cmd.ExecuteNonQuery();
+
+            _truncateCmd = new NpgsqlCommand("TRUNCATE data", _conn);
+        }
+
+        [Cleanup]
+        public void Cleanup()
+        {
+            _conn.Close();
+            _conn = null;
+        }
+
+        [Benchmark(Baseline = true)]
+        public void Unbatched()
+        {
+            var cmd = new NpgsqlCommand("INSERT INTO data VALUES (@p0, @p1, @p2, @p3)", _conn);
+            cmd.Parameters.AddWithValue("p0", NpgsqlDbType.Integer, 8);
+            cmd.Parameters.AddWithValue("p1", NpgsqlDbType.Text, "foo");
+            cmd.Parameters.AddWithValue("p2", NpgsqlDbType.Integer, 9);
+            cmd.Parameters.AddWithValue("p3", NpgsqlDbType.Text, "bar");
+            cmd.Prepare();
+
+            for (var i = 0; i < BatchSize; i++)
+                cmd.ExecuteNonQuery();
+            _truncateCmd.ExecuteNonQuery();
+        }
+
+        [Benchmark]
+        public void Batched()
+        {
+            var cmd = new NpgsqlCommand { Connection = _conn };
+            var sb = new StringBuilder();
+            for (var i = 0; i < BatchSize; i++)
+            {
+                var p1 = (i * 4).ToString();
+                var p2 = (i * 4 + 1).ToString();
+                var p3 = (i * 4 + 2).ToString();
+                var p4 = (i * 4 + 3).ToString();
+                sb.Append("INSERT INTO data VALUES (@").Append(p1).Append(", @").Append(p2).Append(", @").Append(p3).Append(", @").Append(p4).Append(");");
+                cmd.Parameters.AddWithValue(p1, NpgsqlDbType.Integer, 8);
+                cmd.Parameters.AddWithValue(p2, NpgsqlDbType.Text, "foo");
+                cmd.Parameters.AddWithValue(p3, NpgsqlDbType.Integer, 9);
+                cmd.Parameters.AddWithValue(p4, NpgsqlDbType.Text, "bar");
+            }
+            cmd.CommandText = sb.ToString();
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+            _truncateCmd.ExecuteNonQuery();
+        }
+
+        [Benchmark]
+        public void Copy()
+        {
+            using (var s = _conn.BeginBinaryImport("COPY data (int1, text1, int2, text2) FROM STDIN BINARY"))
+            {
+                for (var i = 0; i < BatchSize; i++)
+                {
+                    s.StartRow();
+                    s.Write(8);
+                    s.Write("foo");
+                    s.Write(9);
+                    s.Write("bar");
+                }
+            }
+            _truncateCmd.ExecuteNonQuery();
+        }
+    }
+}
