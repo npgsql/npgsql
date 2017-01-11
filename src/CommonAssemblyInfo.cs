dc1fe25105a50f0c7b2ce997b99eb5ff2a84058b
Shay Rojansky <roji@roji.org>
2017-01-11 12:56:37 +0200

    Bump version to 3.2.0-beta1

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Resources;
 // The following version attributes get rewritten by GitVersion as part of the build
 [assembly: AssemblyVersion("3.2.0")]
 [assembly: AssemblyFileVersion("3.2.0")]
-[assembly: AssemblyInformationalVersion("3.2.0-ci")]
+[assembly: AssemblyInformationalVersion("3.2.0-beta1")]
