c5b482ccfa6e7c2fb629f6a6d510d5a26c9f60ba
kenjiuno <h8mastre@gmail.com>
2017-01-24 22:20:23 +0900

    Fix DDEX loosing password on getting a connection.
    - When we drag a table from Server Explorer into DataSet designer,
      it says connecting to server without password.
    
    - When we implement IVsDataConnectionEquivalencyComparer,
      Microsoft.VisualStudio.Data.Package.DataConnectionManager.GetConnection()
      can find and reuse existing connection already opened for the table.
      Without it, try to create new connection with ConnectionString with
      no Password set. This may be bug of VS.
    
    - NpgsqlConnectionEquivalencyComparer extends DataConnectionEquivalencyComparer.
    
    - Add support registry key for IVsDataConnectionEquivalencyComparer.

---
+++ src/VSIX/NpgsqlConnectionEquivalencyComparer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.Data.Framework;
+using Microsoft.VisualStudio.Data.Services.SupportEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Npgsql.VSIX
+{
+    class NpgsqlConnectionEquivalencyComparer : DataConnectionEquivalencyComparer
+    {
+        protected override bool AreEquivalent(IVsDataConnectionProperties connectionProperties1, IVsDataConnectionProperties connectionProperties2)
+            => connectionProperties1["Host"].ToString() == connectionProperties2["Host"].ToString()
+            && connectionProperties1["Port"].ToString() == connectionProperties2["Port"].ToString()
+            && connectionProperties1["Database"].ToString() == connectionProperties2["Database"].ToString()
+            && connectionProperties1["Username"].ToString() == connectionProperties2["Username"].ToString()
+            ;
+    }
+}
