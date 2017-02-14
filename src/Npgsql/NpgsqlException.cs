079dfe801e654aff821c77bc79e2758d9e4364e7
Shay Rojansky <roji@roji.org>
2017-02-14 17:42:39 +0200

    Add public ctor to PostgresException
    Closes #1441

--- src/Npgsql/NpgsqlException.cs
+++ src/Npgsql/NpgsqlException.cs
@@ -28,21 +28,21 @@ namespace Npgsql
         /// <summary>
         /// Initializes a new instance of the <see cref="NpgsqlException"/> class.
         /// </summary>
-        protected internal NpgsqlException() {}
+        public NpgsqlException() {}
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NpgsqlException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception, or a null reference (<string>Nothing</string> in Visual Basic) if no inner exception is specified.</param>
-        protected internal NpgsqlException(string message, Exception innerException) 
+        public NpgsqlException(string message, Exception innerException) 
             : base(message, innerException) {}
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NpgsqlException"/> class with a specified error message.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        protected internal NpgsqlException(string message)
+        public NpgsqlException(string message)
             : base(message) { }
 
         #region Serialization
