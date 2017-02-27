fbd36d6e262e025b2d86f6f7040f600ca843f564
Shay Rojansky <roji@roji.org>
2017-02-27 19:25:44 +0200

    [VSIX] Tighter UI control
    Some refactoring of the UI control based on the DDEX sample.

--- src/VSIX/NpgsqlConnectionUIControl.cs
+++ src/VSIX/NpgsqlConnectionUIControl.cs
@@ -19,6 +19,7 @@ namespace Npgsql.VSIX
 
         public override void LoadProperties()
         {
+            _loading = true;
             try
             {
                 hostTextBox.Text = (string)Site["Host"];
@@ -36,8 +37,33 @@ namespace Npgsql.VSIX
             {
                 Trace.WriteLine(ex.ToString());
             }
+            finally
+            {
+                _loading = false;
+            }
         }
 
+        void SetProperty(object sender, EventArgs e)
+        {
+            if (_loading)
+                return;  // TODO: Match with sample?
+
+            if (sender == hostTextBox)
+                Site["Host"] = hostTextBox.Text.Trim();
+            else if (sender == portNumericUpDown)
+                Site["Port"] = portNumericUpDown.Value;
+            else if (sender == databaseTextBox)
+                Site["Database"] = databaseTextBox.Text;
+            else if (sender == usernameTextBox)
+                Site["Username"] = usernameTextBox.Text;
+            else if (sender == passwordTextBox)
+                Site["Password"] = passwordTextBox.Text;
+            else if (sender == savePasswordCheckBox)
+                Site["Persist Security Info"] = savePasswordCheckBox.Checked;
+
+            // TODO: Authentication!
+        }
+        /*
         void SetHost(object sender, EventArgs e) => Site["Host"] = hostTextBox.Text;
         void SetPort(object sender, EventArgs e) => Site["Port"] = portNumericUpDown.Value;
         void SetDatabase(object sender, EventArgs e) => Site["Database"] = databaseTextBox.Text;
@@ -61,5 +87,24 @@ namespace Npgsql.VSIX
                 savePasswordCheckBox.Enabled = true;
             }
         }
+        */
+
+        #region Private Fields
+
+        /// <summary>
+        /// It is necessary that we keep track of whether properties are
+        /// currently being loaded or not.  This is because the events
+        /// fired by each control that cause the SetProperty method to
+        /// be called are typically called when the text changes or the
+        /// value of the control is altered.  This happens when loading
+        /// the properties and when a user sets them.  In the case of
+        /// loading, we do not want to update the underlying connection
+        /// properties instance with the value so we set this to true
+        /// during load time so that SetProperty only causes UI state
+        /// changes and does not write to the connection properties.
+        /// </summary>
+        bool _loading = false;
+
+        #endregion
     }
 }
