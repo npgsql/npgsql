36762ad525632fe325ffd8a8479b50074b811010
Shay Rojansky <roji@roji.org>
2017-03-07 16:29:23 +0200

    Remove dead code

--- src/Npgsql/TypeHandlers/CompositeHandler.cs
+++ src/Npgsql/TypeHandlers/CompositeHandler.cs
@@ -119,75 +119,7 @@ namespace Npgsql.TypeHandlers
             }
             return (T)result;
         }
-        /*
-                public override bool Read(out T result)
-                {
-                    Debug.Assert(_members != null);
 
-                    result = default(T);
-
-                    if (_fieldIndex == -1)
-                    {
-                        if (_readBuf.ReadBytesLeft < 4) { return false; }
-                        var fieldCount = _readBuf.ReadInt32();
-                        if (fieldCount != _members.Count) {
-                            // PostgreSQL sanity check
-                            throw new Exception($"pg_attributes contains {_members.Count} rows for type {PgDisplayName}, but {fieldCount} fields were received!");
-                        }
-                        _fieldIndex = 0;
-                    }
-
-                    for (; _fieldIndex < _members.Count; _fieldIndex++)
-                    {
-                        var fieldDescriptor = _members[_fieldIndex];
-                        // Not yet started reading the field.
-                        // Read the type OID (not really needed), then the length.
-                        if (_len == -1)
-                        {
-                            if (_readBuf.ReadBytesLeft < 8) { return false; }
-                            _readBuf.ReadInt32();  // read typeOID, not used
-                            _len = _readBuf.ReadInt32();
-                            if (_len == -1)
-                            {
-                                // Null field, simply skip it and leave at default
-                                continue;
-                            }
-                        }
-
-                        // Get the field's type handler and read the value
-                        var handler = fieldDescriptor.Handler;
-                        object fieldValue;
-
-                        if (handler is ISimpleTypeHandler)
-                        {
-                            var asSimpleReader = (ISimpleTypeHandler)handler;
-                            if (_readBuf.ReadBytesLeft < _len)
-                                return false;
-                            fieldValue = asSimpleReader.ReadAsObject(_readBuf, _len);
-                        }
-                        else if (handler is IChunkingTypeHandler)
-                        {
-                            var asChunkingReader = (IChunkingTypeHandler)handler;
-                            if (!_preparedRead)
-                            {
-                                asChunkingReader.PrepareRead(_readBuf, _len);
-                                _preparedRead = true;
-                            }
-
-                            if (!asChunkingReader.ReadAsObject(out fieldValue))
-                                return false;
-                            _preparedRead = false;
-                        }
-                        else throw new InvalidOperationException("Internal Npgsql bug, please report.");
-
-                        fieldDescriptor.SetValue(_value, fieldValue);
-                        _len = -1;
-                    }
-
-                    result = (T)_value;
-                    return true;
-                }
-                */
         #endregion
 
         #region Write
