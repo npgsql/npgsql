a48f981892878ba28a86a2ec38274042a36e0735
Shay Rojansky <roji@roji.org>
2017-03-14 22:37:32 +0200

    Appveyor build improvements
    - SSL
    - PostGIS
    - Prepared transactions
    - Some general cleanup

---
+++ .build/setup-pg.ps1
@@ -0,0 +1,14 @@
+
+Write-Host Enabling PostgreSQL prepared transactions...
+Add-Content 'C:\Program Files\PostgreSQL\9.5\data\postgresql.conf' "`nmax_prepared_transactions = 10"
+
+Write-Host Enabling PostgreSQL SSL...
+Add-Content 'C:\Program Files\PostgreSQL\9.5\data\postgresql.conf' "`nssl = true"
+Copy-Item .build\server.* "C:\Program Files\PostgreSQL\9.5\data"
+
+Write-Host Enabling PostGIS...
+If (!(Test-Path $env:POSTGIS_EXE)) {
+  Write-Host Downloading PostGIS...
+  (New-Object Net.WebClient).DownloadFile("http://download.osgeo.org/postgis/windows/pg95/$env:POSTGIS_EXE", "$env:POSTGIS_EXE")
+}
+iex ".\$env:POSTGIS_EXE /S /D='C:\Program Files\PostgreSQL\9.5'"
