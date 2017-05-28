e623089297f621601048ebfd7298a69efe01e0c5
Shay Rojansky <roji@roji.org>
2017-05-28 23:46:17 +0200

    Fix CI detection in tests

--- test/Npgsql.Tests/TestUtil.cs
+++ test/Npgsql.Tests/TestUtil.cs
@@ -35,7 +35,8 @@ namespace Npgsql.Tests
 {
     public static class TestUtil
     {
-        public static bool IsOnBuildServer => Environment.GetEnvironmentVariable("TEAMCITY_VERSION") != null;
+        public static bool IsOnBuildServer
+            => Environment.GetEnvironmentVariable("CI") != null;
 
         /// <summary>
         /// Calls Assert.Ignore() unless we're on the build server, in which case calls
