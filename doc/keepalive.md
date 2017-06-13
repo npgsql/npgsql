84e00282a1042ce4147a8f5b5c1583877122a3fc
Shay Rojansky <roji@roji.org>
2017-06-13 13:58:40 +0200

    Support for TCP keepalive
    Closes #1596

--- doc/keepalive.md
+++ doc/keepalive.md
@@ -13,6 +13,8 @@ event if the keepalive fails.
 Note that you should only turn this feature on if you need it. Unless you know you'll have long-lived idle connections, and that your
 backend (or network equipment) will interfere with these connections, it's better to leave this off.
 
-Note: Npgsql does not turn on TCP keepalive because that feature isn't universally reliable across all network
-equipment.
+## TCP Keepalives
 
+Npgsql also supports TCP keepalive, which is different from the application-level keepalive described above. To better understand the different kinds of keepalives, see [this blog post](http://blog.stephencleary.com/2009/05/detection-of-half-open-dropped.html). In a nutshell, while application-level keepalive will always work, TCP keepalive depends on your networking stack properly supporting it, etc.
+
+One known case in which TCP keepalive may be necessary, is when you need keepalives during query processing, as opposed to between queries; this is the case if you send a query for processing, and plan to spend a lot of time before getting any response from the server (see [#1596](https://github.com/npgsql/npgsql/issues/1596) for an example). TCP keepalive may also be helpful in some other cases.
--- src/Npgsql/NpgsqlConnectionStringBuilder.cs
+++ src/Npgsql/NpgsqlConnectionStringBuilder.cs
@@ -884,6 +884,52 @@ namespace Npgsql
         int _keepAlive;
 
         /// <summary>
+        /// The number of seconds of connection inactivity before a TCP keepalive query is sent.
+        /// Use of this option is discouraged, use <see cref="KeepAlive"/> instead if possible.
+        /// Set to 0 (the default) to disable. Supported only on Windows.
+        /// </summary>
+        [Category("Advanced")]
+        [Description("The number of milliseconds of connection inactivity before a TCP keepalive query is sent.")]
+        [DisplayName("TCP Keepalive Time")]
+        [NpgsqlConnectionStringProperty]
+        public int TcpKeepAliveTime
+        {
+            get => _tcpKeepAliveTime;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "TcpKeepAliveTime can't be negative");
+
+                _tcpKeepAliveTime = value;
+                SetValue(nameof(TcpKeepAliveTime), value);
+            }
+        }
+        int _tcpKeepAliveTime;
+
+        /// <summary>
+        /// The interval, in milliseconds, between when successive keep-alive packets are sent if no acknowledgement is received.
+        /// Defaults to the value of <see cref="TcpKeepAliveTime"/>. <see cref="TcpKeepAliveTime"/> must be non-zero as well.
+        /// Supported only on Windows.
+        /// </summary>
+        [Category("Advanced")]
+        [Description("The interval, in milliseconds, between when successive keep-alive packets are sent if no acknowledgement is received.")]
+        [DisplayName("TCP Keepalive Interval")]
+        [NpgsqlConnectionStringProperty]
+        public int TcpKeepAliveInterval
+        {
+            get => _tcpKeepAliveInterval;
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "TcpKeepAliveInterval can't be negative");
+
+                _tcpKeepAliveInterval = value;
+                SetValue(nameof(TcpKeepAliveInterval), value);
+            }
+        }
+        int _tcpKeepAliveInterval;
+
+        /// <summary>
         /// Determines the size of the internal buffer Npgsql uses when reading. Increasing may improve performance if transferring large values from the database.
         /// </summary>
         [Category("Advanced")]
