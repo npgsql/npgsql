923f8f58c79114660b13cf6e83113ba8008b920b
Shay Rojansky <roji@roji.org>
2017-07-02 23:58:15 +0300

    First plugin: Json.NET

---
+++ src/Npgsql.Json.NET/JsonHandler.cs
@@ -0,0 +1,84 @@
+﻿#region License
+// The PostgreSQL License
+//
+// Copyright (C) 2017 The Npgsql Development Team
+//
+// Permission to use, copy, modify, and distribute this software and its
+// documentation for any purpose, without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph and the following two paragraphs appear in all copies.
+//
+// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
+// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
+// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Npgsql.BackendMessages;
+using Npgsql.PostgresTypes;
+using Npgsql.TypeHandling;
+using Npgsql.TypeMapping;
+
+namespace Npgsql.Json.NET
+{
+    class JsonHandlerFactory : NpgsqlTypeHandlerFactory
+    {
+        protected override NpgsqlTypeHandler Create(NpgsqlConnection conn)
+            => new JsonHandler(conn);
+    }
+
+    class JsonHandler : Npgsql.TypeHandlers.TextHandler
+    {
+        public JsonHandler(NpgsqlConnection connection)
+            : base(connection) {}
+
+        protected override async ValueTask<T> Read<T>(NpgsqlReadBuffer buf, int len, bool async, FieldDescription fieldDescription = null)
+        {
+            var s = await base.Read<string>(buf, len, async, fieldDescription);
+            if (typeof(T) == typeof(string))
+                return (T)(object)s;
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(s);
+            }
+            catch (Exception e)
+            {
+                throw new NpgsqlSafeReadException(e);
+            }
+        }
+
+        protected override int ValidateAndGetLength(object value, ref NpgsqlLengthCache lengthCache, NpgsqlParameter parameter = null)
+        {
+            var s = value as string;
+            if (s == null)
+            {
+                s = JsonConvert.SerializeObject(value);
+                if (parameter != null)
+                    parameter.ConvertedValue = s;
+            }
+            return base.ValidateAndGetLength(s, ref lengthCache, parameter);
+        }
+
+        protected override Task Write(object value, NpgsqlWriteBuffer buf, NpgsqlLengthCache lengthCache, NpgsqlParameter parameter, bool async)
+        {
+            if (parameter?.ConvertedValue != null)
+                value = parameter.ConvertedValue;
+            var s = value as string ?? JsonConvert.SerializeObject(value);
+            return base.Write(s, buf, lengthCache, parameter, async);
+        }
+    }
+}
---
+++ test/Npgsql.Tests/JsonNetTests.cs
@@ -0,0 +1,172 @@
+﻿#region License
+// The PostgreSQL License
+//
+// Copyright (C) 2017 The Npgsql Development Team
+//
+// Permission to use, copy, modify, and distribute this software and its
+// documentation for any purpose, without fee, and without a written
+// agreement is hereby granted, provided that the above copyright notice
+// and this paragraph and the following two paragraphs appear in all copies.
+//
+// IN NO EVENT SHALL THE NPGSQL DEVELOPMENT TEAM BE LIABLE TO ANY PARTY
+// FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+// INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS
+// DOCUMENTATION, EVEN IF THE NPGSQL DEVELOPMENT TEAM HAS BEEN ADVISED OF
+// THE POSSIBILITY OF SUCH DAMAGE.
+//
+// THE NPGSQL DEVELOPMENT TEAM SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS
+// ON AN "AS IS" BASIS, AND THE NPGSQL DEVELOPMENT TEAM HAS NO OBLIGATIONS
+// TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+#endregion
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using NpgsqlTypes;
+using Npgsql.Json.NET;
+
+namespace Npgsql.Tests
+{
+    /// <summary>
+    /// Tests for the Npgsql.Json.NET mapping plugin
+    /// </summary>
+    [Parallelizable(ParallelScope.None)]
+    [TestFixture(NpgsqlDbType.Jsonb)]
+    [TestFixture(NpgsqlDbType.Json)]
+    public class JsonNetTests : TestBase
+    {
+        [Test]
+        public void RoundtripObject()
+        {
+            var expected = new Foo { Bar = 8 };
+            using (var conn = OpenConnection())
+            using (var cmd = new NpgsqlCommand(@"SELECT @p", conn))
+            {
+                cmd.Parameters.Add(new NpgsqlParameter("p", _npgsqlDbType) { Value = expected });
+                using (var reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    Assert.That(reader.GetFieldValue<Foo>(0), Is.EqualTo(expected));
+                }
+            }
+        }
+
+        [Test]
+        public void DeserializeFailure()
+        {
+            using (var conn = OpenConnection())
+            using (var cmd = new NpgsqlCommand($@"SELECT '[1, 2, 3]'::{_pgTypeName}", conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                // Attempt to deserialize JSON array into object
+                Assert.That(() => reader.GetFieldValue<Foo>(0), Throws.TypeOf<JsonSerializationException>());
+                // State should still be OK to continue
+                var actual = reader.GetFieldValue<JArray>(0);
+                Assert.That((int)actual[0], Is.EqualTo(1));
+            }
+        }
+
+        class Foo
+        {
+            public int Bar { get; set; }
+            public override bool Equals(object obj) => (obj as Foo)?.Bar == Bar;
+            public override int GetHashCode() => Bar.GetHashCode();
+        }
+
+        [Test]
+        public void RoundtripJObject()
+        {
+            var expected = new JObject { ["Bar"] = 8 };
+
+            using (var conn = OpenConnection())
+            using (var cmd = new NpgsqlCommand(@"SELECT @p", conn))
+            {
+                cmd.Parameters.Add(new NpgsqlParameter("p", _npgsqlDbType) { Value = expected });
+                using (var reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    var actual = reader.GetFieldValue<JObject>(0);
+                    Assert.That((int)actual["Bar"], Is.EqualTo(8));
+                }
+            }
+        }
+
+        [Test]
+        public void RoundtripJArray()
+        {
+            var expected = new JArray(new[] { 1, 2, 3 });
+
+            using (var conn = OpenConnection())
+            using (var cmd = new NpgsqlCommand(@"SELECT @p", conn))
+            {
+                cmd.Parameters.Add(new NpgsqlParameter("p", _npgsqlDbType) { Value = expected });
+                using (var reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    var jarray = reader.GetFieldValue<JArray>(0);
+                    Assert.That(jarray.ToObject<int[]>(), Is.EqualTo(new[] { 1, 2, 3 }));
+                }
+            }
+        }
+
+        [Test]
+        public void ClrTypeMapping()
+        {
+            var expected = new Foo { Bar = 8 };
+            using (var conn = OpenConnection())
+            using (var cmd = new NpgsqlCommand(@"SELECT @p", conn))
+            {
+                conn.TypeMapper.UseJsonNet(new[] { typeof(Foo) });
+
+                cmd.Parameters.AddWithValue("p", expected);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    reader.Read();
+                    var actual = reader.GetFieldValue<Foo>(0);
+                    Assert.That(actual.Bar, Is.EqualTo(8));
+                }
+            }
+        }
+
+        [Test]
+        public void RoundtripClrArray()
+        {
+            var expected = new[] { 1, 2, 3 };
+
+            using (var conn = OpenConnection())
+            {
+                conn.TypeMapper.UseJsonNet(new[] { typeof(int[]) });
+
+                using (var cmd = new NpgsqlCommand($@"SELECT @p::{_pgTypeName}", conn))
+                {
+                    cmd.Parameters.AddWithValue("p", expected);
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        reader.Read();
+                        var actual = reader.GetFieldValue<int[]>(0);
+                        Assert.That(actual, Is.EqualTo(expected));
+                    }
+                }
+            }
+        }
+
+        protected override NpgsqlConnection OpenConnection(string connectionString = null)
+        {
+            var conn = base.OpenConnection(connectionString);
+            conn.TypeMapper.UseJsonNet();
+            return conn;
+        }
+
+        readonly NpgsqlDbType _npgsqlDbType;
+        readonly string _pgTypeName;
+
+        public JsonNetTests(NpgsqlDbType npgsqlDbType)
+        {
+            _npgsqlDbType = npgsqlDbType;
+            _pgTypeName = npgsqlDbType.ToString().ToLower();
+        }
+    }
+}
