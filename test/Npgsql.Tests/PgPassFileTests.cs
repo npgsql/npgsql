fa924058756d2e6df2b837818cffa94da7422df3
Taufiky1994 <Taufiky@gmail.com>
2017-07-23 00:37:34 +0700

    Changes for reature 1442, test script

--- test/Npgsql.Tests/PgPassFileTests.cs
+++ test/Npgsql.Tests/PgPassFileTests.cs
@@ -12,49 +12,27 @@ namespace Npgsql.Tests
         public void ShouldParseAllEntries()
         {
             var file = new PgPassFile(_pgpassFile);
-            var fileUnique = new PgPassFile(_pgpassUniqueFile);
             var entries = file.Entries.ToList();
-            var entriesUnique = fileUnique.Entries.ToList();
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(entries.Count, Is.EqualTo(3));
-                Assert.That(entriesUnique.Count, Is.EqualTo(3));
-            });
+            Assert.That(entries.Count, Is.EqualTo(3));
         }
 
         [Test]
         public void ShouldFindFirstEntryWhenMultipleMatch()
         {
             var file = new PgPassFile(_pgpassFile);
-            var fileUnique = new PgPassFile(_pgpassUniqueFile);
             var entry = file.GetFirstMatchingEntry("testhost");
-            var entryUnique = fileUnique.GetFirstMatchingEntry("testhost");
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(entry.Password, Is.EqualTo("testpass"));
-                Assert.That(entryUnique.Password, Is.EqualTo("testpass"));
-            });
+            Assert.That(entry.Password, Is.EqualTo("testpass"));
         }
 
         [Test]
         public void ShouldFindDefaultForNoMatches()
         {
             var file = new PgPassFile(_pgpassFile);
-            var fileUnique = new PgPassFile(_pgpassUniqueFile);
             var entry = file.GetFirstMatchingEntry("notarealhost");
-            var entryUnique = fileUnique.GetFirstMatchingEntry("notarealhost");
-
-            Assert.Multiple(() =>
-            {
-                Assert.That(entry.Password, Is.EqualTo("defaultpass"));
-                Assert.That(entryUnique.Password, Is.EqualTo("defaultpass"));
-            });
+            Assert.That(entry.Password, Is.EqualTo("defaultpass"));
         }
 
         string _pgpassFile;
-        string _pgpassUniqueFile;
 
         [OneTimeSetUp]
         public void CreateTestFile()
@@ -65,13 +43,8 @@ testhost:*:*:*:testdefaultpass
 # helpful comment goes here
 *:*:*:*:defaultpass";
 
-            // create a long filename with unusual allowed characters
-            const string uniqueFileName = "!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE!#$%&'()+,.;=@[]^_`{}~FREE.tmp";
-
             _pgpassFile = Path.GetTempFileName();
-            _pgpassUniqueFile = Path.Combine(Path.GetTempPath(), uniqueFileName);
 
-            File.WriteAllText(_pgpassUniqueFile, content);
             File.WriteAllText(_pgpassFile, content);
         }
 
@@ -80,9 +53,6 @@ testhost:*:*:*:testdefaultpass
         {
             if (File.Exists(_pgpassFile))
                 File.Delete(_pgpassFile);
-
-            if (File.Exists(_pgpassUniqueFile))
-                File.Delete(_pgpassUniqueFile);
         }
     }
 }
