c5719895ba791fdfeaa90d0fa3268d85128ac523
apircher <apircher@luxbox.net>
2017-08-12 16:24:55 +0200

    Create a SupportsTransactions feature on connector and silently ignore calls to NpgsqlConnection.BeginTransaction if transactions are not supported.

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -1932,6 +1932,7 @@ namespace Npgsql
         bool SupportsDiscardTemp => ServerVersion >= new Version(8, 3, 0) && !IsCrateDB;
         bool SupportsDiscard => ServerVersion >= new Version(8, 3, 0) && !IsCrateDB; // Redshift is 8.0.2
         internal bool SupportsRangeTypes => ServerVersion >= new Version(9, 2, 0) && !IsCrateDB;
+        internal bool SupportsTransactions => !IsCrateDB;
         internal bool UseConformantStrings { get; private set; }
         internal bool IntegerDateTimes { get; private set; }
 
