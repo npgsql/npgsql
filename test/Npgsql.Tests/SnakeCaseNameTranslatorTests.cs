0896fc2b2e2b7f3ddc3128d3bba0df5d8a5379f5
Kristian Hellang <kristian@hellang.com>
2017-10-25 15:01:50 +0200

    Clean up tests

--- test/Npgsql.Tests/SnakeCaseNameTranslatorTests.cs
+++ test/Npgsql.Tests/SnakeCaseNameTranslatorTests.cs
@@ -33,51 +33,41 @@ namespace Npgsql.Tests
     {
         [Test, TestCaseSource(typeof(SnakeCaseNameTranslatorTests), nameof(TestCases))]
         public string TranslateTypeName(string value, bool compatibilityMode)
-        {
-            return new NpgsqlSnakeCaseNameTranslator(compatibilityMode).TranslateTypeName(value);
-        }
+            => new NpgsqlSnakeCaseNameTranslator(compatibilityMode).TranslateTypeName(value);
 
         [Test, TestCaseSource(typeof(SnakeCaseNameTranslatorTests), nameof(TestCases))]
         public string TranslateMemberName(string value, bool compatibilityMode)
-        {
-            return new NpgsqlSnakeCaseNameTranslator(compatibilityMode).TranslateMemberName(value);
-        }
+            => new NpgsqlSnakeCaseNameTranslator(compatibilityMode).TranslateMemberName(value);
 
-        static IEnumerable<TestCaseData> TestCases
-            => CompatibilityTestCases.SelectMany(x => new[]
-            {
-                new TestCaseData(x.value, true).Returns(x.compatResult),
-                new TestCaseData(x.value, false).Returns(x.result), 
-            });
-
-        static IEnumerable<(string value, string compatResult, string result)> CompatibilityTestCases
+        static IEnumerable<TestCaseData> TestCases => new (string value, string compatResult, string result)[]
         {
-            get
-            {
-                yield return ("URLValue", "u_r_l_value", "url_value");
-                yield return ("URL", "u_r_l", "url");
-                yield return ("ID", "i_d", "id");
-                yield return ("I", "i", "i");
-                yield return ("", "", "");
-                yield return ("Person", "person", "person");
-                yield return ("iPhone", "i_phone", "i_phone");
-                yield return ("IPhone", "i_phone", "i_phone");
-                yield return ("I Phone", "i _phone", "i_phone");
-                yield return ("I  Phone", "i  _phone", "i_phone");
-                yield return (" IPhone", " _i_phone", "i_phone");
-                yield return (" IPhone ", " _i_phone ", "i_phone");
-                yield return ("IsCIA", "is_c_i_a", "is_cia");
-                yield return ("VmQ", "vm_q", "vm_q");
-                yield return ("Xml2Json", "xml2_json", "xml2_json");
-                yield return ("SnAkEcAsE", "sn_ak_ec_as_e", "sn_ak_ec_as_e");
-                yield return ("SnA__kEcAsE", "sn_a__k_ec_as_e", "sn_a__k_ec_as_e");
-                yield return ("SnA__ kEcAsE", "sn_a__ k_ec_as_e", "sn_a__k_ec_as_e");
-                yield return ("already_snake_case_ ", "already_snake_case_ ", "already_snake_case_");
-                yield return ("IsJSONProperty", "is_j_s_o_n_property", "is_json_property");
-                yield return ("SHOUTING_CASE", "s_h_o_u_t_i_n_g__c_a_s_e", "shouting_case");
-                yield return ("9999-12-31T23:59:59.9999999Z", "9999-12-31_t23:59:59.9999999_z", "9999-12-31_t23:59:59.9999999_z");
-                yield return ("Hi!! This is text. Time to test.", "hi!! _this is text. _time to test.", "hi!!_this_is_text._time_to_test.");
-            }
-        }
+            ("Hi!! This is text. Time to test.", "hi!! _this is text. _time to test.", "hi!!_this_is_text._time_to_test."),
+            ("9999-12-31T23:59:59.9999999Z", "9999-12-31_t23:59:59.9999999_z", "9999-12-31_t23:59:59.9999999_z"),
+            ("already_snake_case_ ", "already_snake_case_ ", "already_snake_case_"),
+            ("SHOUTING_CASE", "s_h_o_u_t_i_n_g__c_a_s_e", "shouting_case"),
+            ("IsJSONProperty", "is_j_s_o_n_property", "is_json_property"),
+            ("SnA__ kEcAsE", "sn_a__ k_ec_as_e", "sn_a__k_ec_as_e"),
+            ("SnA__kEcAsE", "sn_a__k_ec_as_e", "sn_a__k_ec_as_e"),
+            ("SnAkEcAsE", "sn_ak_ec_as_e", "sn_ak_ec_as_e"),
+            ("URLValue", "u_r_l_value", "url_value"),
+            ("Xml2Json", "xml2_json", "xml2_json"),
+            (" IPhone ", " _i_phone ", "i_phone"),
+            ("I  Phone", "i  _phone", "i_phone"),
+            (" IPhone", " _i_phone", "i_phone"),
+            ("I Phone", "i _phone", "i_phone"),
+            ("IPhone", "i_phone", "i_phone"),
+            ("iPhone", "i_phone", "i_phone"),
+            ("IsCIA", "is_c_i_a", "is_cia"),
+            ("Person", "person", "person"),
+            ("VmQ", "vm_q", "vm_q"),
+            ("URL", "u_r_l", "url"),
+            ("ID", "i_d", "id"),
+            ("I", "i", "i"),
+            ("", "", ""),
+        }.SelectMany(x => new[]
+        {
+            new TestCaseData(x.value, true).Returns(x.compatResult),
+            new TestCaseData(x.value, false).Returns(x.result), 
+        });
     }
 }
