b7a4500e3c62ffd6eb51482b24f47f1382b3ace8
rwasef1830 <beliskner.github.3psil0N@neverbox.com>
2018-02-19 21:45:16 +0200

    Support for the tsquery 'FOLLOWED BY' operator introduced in 9.6

--- test/Npgsql.Tests/Types/MiscTypeTests.cs
+++ test/Npgsql.Tests/Types/MiscTypeTests.cs
@@ -732,7 +732,7 @@ namespace Npgsql.Tests.Types
             using (var conn = OpenConnection())
             using (var cmd = conn.CreateCommand())
             {
-                var query = NpgsqlTsQuery.Parse("(a & !(c | d)) & (!!a&b) | ä | d | e");
+                var query = NpgsqlTsQuery.Parse("(a & !(c | d)) & (!!a&b) | ä | x <-> y | x <10> y | d <0> e | f");
 
                 cmd.CommandText = "Select :p";
                 cmd.Parameters.AddWithValue("p", query);
