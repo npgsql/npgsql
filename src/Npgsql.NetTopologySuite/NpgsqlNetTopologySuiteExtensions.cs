cb5457737fdf3ded832a904d2f141e67d33a46fb
Yoh Deadfall <yoh.deadfall@hotmail.com>
2018-07-25 19:06:10 +0300

    Specified ordinates for writer

--- src/Npgsql.NetTopologySuite/NpgsqlNetTopologySuiteExtensions.cs
+++ src/Npgsql.NetTopologySuite/NpgsqlNetTopologySuiteExtensions.cs
@@ -56,7 +56,9 @@ namespace Npgsql
         /// <param name="mapper">The type mapper to set up (global or connection-specific).</param>
         /// <param name="coordinateSequenceFactory">The factory which knows how to build a particular implementation of ICoordinateSequence from an array of Coordinates.</param>
         /// <param name="precisionModel">Specifies the grid of allowable points.</param>
-        /// <param name="handleOrdinates">Specifies the ordinates which will be handled. Not specified ordinates will be ignored.</param>
+        /// <param name="handleOrdinates">Specifies the ordinates which will be handled. Not specified ordinates will be ignored.
+        /// If <see cref="F:GeoAPI.Geometries.Ordiantes.None" /> is specified, an actual value will be taken from
+        /// the <see cref="P:GeoAPI.Geometries.ICoordinateSequenceFactory.Ordinates"/> property of <paramref name="coordinateSequenceFactory"/>.</param>
         /// <param name="geographyAsDefault">Specifies that the geography type is used for mapping by default.</param>
         public static INpgsqlTypeMapper UseNetTopologySuite(
             this INpgsqlTypeMapper mapper,
@@ -76,7 +78,7 @@ namespace Npgsql
 
             var typeHandlerFactory = new NetTopologySuiteHandlerFactory(
                 new PostGisReader(coordinateSequenceFactory, precisionModel, handleOrdinates),
-                new PostGisWriter());
+                new PostGisWriter() { HandleOrdinates = handleOrdinates });
 
             return mapper
                 .AddMapping(new NpgsqlTypeMappingBuilder
