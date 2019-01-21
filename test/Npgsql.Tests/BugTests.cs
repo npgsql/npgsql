640c5ad36bcb85e73eb09020f326e1ea3b44123e
Shay Rojansky <roji@roji.org>
2019-01-21 13:42:16 -0800

    Fix mapping when two types exist with same name
    When two PostgreSQL types exist with the same name (e.g. user created
    a type called "boolean"), the built-in mappings fail because they use
    relative type names. Added a fallback to try a full-name lookup in
    pg_catalog for the built-in mappings.
    
    Fixes #2296
    
    (cherry picked from commit 92aa87abb8f817378ddfbcfa74a6dfd39281db24)

--- test/Npgsql.Tests/BugTests.cs
+++ test/Npgsql.Tests/BugTests.cs
@@ -403,6 +403,29 @@ namespace Npgsql.Tests
             Right
         }
 
+        [Test]
+        public void Bug2296()
+        {
+            using (var conn = OpenConnection())
+            {
+                try
+                {
+                    conn.ExecuteNonQuery("CREATE DOMAIN pg_temp.\"boolean\" AS bool");
+                    conn.ExecuteNonQuery("CREATE TEMP TABLE data (mybool \"boolean\")");
+                    conn.ExecuteNonQuery("INSERT INTO data (mybool) VALUES (TRUE)");
+
+                    conn.ReloadTypes();
+
+                    conn.ExecuteScalar("SELECT mybool FROM data");
+                }
+                finally
+                {
+                    conn.ExecuteNonQuery("DROP TABLE IF EXISTS data; DROP TYPE IF EXISTS \"boolean\"");
+                    conn.ReloadTypes();
+                }
+            }
+        }
+
         #region Bug1285
 
         [Test, IssueLink("https://github.com/npgsql/npgsql/issues/1285")]
