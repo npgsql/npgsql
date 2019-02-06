7c8a37a285fa59530f8c68b8e9ee5c2791ca8d65
Yoh Deadfall <yoh.deadfall@hotmail.com>
2019-02-06 19:39:20 +0300

    Fixed snake case name translation

--- test/Npgsql.Tests/SnakeCaseNameTranslatorTests.cs
+++ test/Npgsql.Tests/SnakeCaseNameTranslatorTests.cs
@@ -18,8 +18,8 @@ namespace Npgsql.Tests
 
         static IEnumerable<TestCaseData> TestCases => new (string value, string legacyResult, string result)[]
         {
-            ("Hi!! This is text. Time to test.", "hi!! _this is text. _time to test.", "hi!!_this_is_text._time_to_test."),
-            ("9999-12-31T23:59:59.9999999Z", "9999-12-31_t23:59:59.9999999_z", "9999-12-31_t23:59:59.9999999_z"),
+            ("Hi!! This is text. Time to test.", "hi!! _this is text. _time to test.", "hi_this_is_text_time_to_test"),
+            ("9999-12-31T23:59:59.9999999Z", "9999-12-31_t23:59:59.9999999_z", "9999_12_31t23_59_59_9999999z"),
             ("already_snake_case_ ", "already_snake_case_ ", "already_snake_case_"),
             ("SHOUTING_CASE", "s_h_o_u_t_i_n_g__c_a_s_e", "shouting_case"),
             ("IsJSONProperty", "is_j_s_o_n_property", "is_json_property"),
@@ -27,7 +27,7 @@ namespace Npgsql.Tests
             ("SnA__kEcAsE", "sn_a__k_ec_as_e", "sn_a__k_ec_as_e"),
             ("SnAkEcAsE", "sn_ak_ec_as_e", "sn_ak_ec_as_e"),
             ("URLValue", "u_r_l_value", "url_value"),
-            ("Xml2Json", "xml2_json", "xml2_json"),
+            ("Xml2Json", "xml2_json", "xml2json"),
             (" IPhone ", " _i_phone ", "i_phone"),
             ("I  Phone", "i  _phone", "i_phone"),
             (" IPhone", " _i_phone", "i_phone"),
