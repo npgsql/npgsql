f876a05016166dc117705bfde639c9e5feb7b821
Kristian Hellang <kristian@hellang.com>
2019-03-18 11:26:35 -0700

    Don't allocate a string for ReadOnlySpan StringBuilder overload

--- src/Npgsql/Util/StringBuilderExtensions.cs
+++ src/Npgsql/Util/StringBuilderExtensions.cs
@@ -10,13 +10,25 @@ namespace Npgsql.Util
     static class StringBuilderExtensions
     {
         /// <summary>
-        /// Appends the provided <see cref="ReadOnlySpan{T}"/> to the <see cref="StringBuilder"/> by calling ToString on
-        /// the span.
+        /// Appends the provided <see cref="ReadOnlySpan{T}"/> to the <see cref="StringBuilder"/>.
         /// </summary>
         /// <param name="stringBuilder">The <see cref="StringBuilder"/> to append to.</param>
         /// <param name="span">The <see cref="ReadOnlySpan{T}"/> to append.</param>
-        public static void Append(this StringBuilder stringBuilder, ReadOnlySpan<char> span)
-            => stringBuilder.Append(span.ToString());
+        public static StringBuilder Append(this StringBuilder stringBuilder, ReadOnlySpan<char> span)
+        {
+            if (span.Length > 0)
+            {
+                unsafe
+                {
+                    fixed (char* value = &span.GetPinnableReference())
+                    {
+                        return stringBuilder.Append(value, span.Length);
+                    }
+                }
+            }
+
+            return stringBuilder;
+        }
     }
 }
 #endif
