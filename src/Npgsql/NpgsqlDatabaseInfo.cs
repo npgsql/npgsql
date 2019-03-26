1edc6c418bd2fbb0875cbfafd11a89aca7261d29
Hennadii Zabula <hennadii.zabula@apriorit.com>
2019-03-26 13:06:49 +0200

    Supports* methods allocate #2391

--- src/Npgsql/NpgsqlDatabaseInfo.cs
+++ src/Npgsql/NpgsqlDatabaseInfo.cs
@@ -52,35 +52,35 @@ namespace Npgsql
         /// <summary>
         /// Whether the backend supports range types.
         /// </summary>
-        public virtual bool SupportsRangeTypes => Version >= new Version(9, 2, 0);
+        public virtual bool SupportsRangeTypes => VersionGreaterOrEqual(Version, 9, 2, 0);
         /// <summary>
         /// Whether the backend supports enum types.
         /// </summary>
-        public virtual bool SupportsEnumTypes => Version >= new Version(8, 3, 0);
+        public virtual bool SupportsEnumTypes => VersionGreaterOrEqual(Version, 8, 3, 0);
         /// <summary>
         /// Whether the backend supports the CLOSE ALL statement.
         /// </summary>
-        public virtual bool SupportsCloseAll => Version >= new Version(8, 3, 0);
+        public virtual bool SupportsCloseAll => VersionGreaterOrEqual(Version, 8, 3, 0);
         /// <summary>
         /// Whether the backend supports advisory locks.
         /// </summary>
-        public virtual bool SupportsAdvisoryLocks => Version >= new Version(8, 2, 0);
+        public virtual bool SupportsAdvisoryLocks => VersionGreaterOrEqual(Version, 8, 2, 0);
         /// <summary>
         /// Whether the backend supports the DISCARD SEQUENCES statement.
         /// </summary>
-        public virtual bool SupportsDiscardSequences => Version >= new Version(9, 4, 0);
+        public virtual bool SupportsDiscardSequences => VersionGreaterOrEqual(Version, 9, 4, 0);
         /// <summary>
         /// Whether the backend supports the UNLISTEN statement.
         /// </summary>
-        public virtual bool SupportsUnlisten => Version >= new Version(6, 4, 0);  // overridden by PostgresDatabase
+        public virtual bool SupportsUnlisten => VersionGreaterOrEqual(Version, 6, 4, 0);  // overridden by PostgresDatabase
         /// <summary>
         /// Whether the backend supports the DISCARD TEMP statement.
         /// </summary>
-        public virtual bool SupportsDiscardTemp => Version >= new Version(8, 3, 0);
+        public virtual bool SupportsDiscardTemp => VersionGreaterOrEqual(Version, 8, 3, 0);
         /// <summary>
         /// Whether the backend supports the DISCARD statement.
         /// </summary>
-        public virtual bool SupportsDiscard => Version >= new Version(8, 3, 0);
+        public virtual bool SupportsDiscard => VersionGreaterOrEqual(Version, 8, 3, 0);
 
         /// <summary>
         /// Reports whether the backend uses the newer integer timestamp representation.
@@ -202,6 +202,44 @@ namespace Npgsql
             if (!versionString.Contains("."))
                 versionString += ".0";
             return new Version(versionString);
+        }
+
+        /// <summary>
+        /// Allocation free helper function to find if version is greater than expected
+        /// </summary>
+        private static bool VersionGreaterOrEqual(Version version, int major, int minor, int build)
+        {
+            if (version.Major != major)
+            {
+                if (version.Major > major)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (version.Minor != minor)
+            {
+                if (version.Minor > minor)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (version.Build != build)
+            {
+                if (version.Build > build)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            return true;
         }
 
         #endregion Misc
