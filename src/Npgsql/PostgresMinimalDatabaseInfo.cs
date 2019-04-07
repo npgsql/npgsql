c461272e9da1010c2e04960809c3789b941c7436
Shay Rojansky <roji@roji.org>
2019-04-07 11:48:24 +0200

    Fix NoTypeLoading compatibility mode
    Broken by #2395

--- src/Npgsql/PostgresMinimalDatabaseInfo.cs
+++ src/Npgsql/PostgresMinimalDatabaseInfo.cs
@@ -21,10 +21,12 @@
 // TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #endregion
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Npgsql.PostgresTypes;
 using NpgsqlTypes;
 
@@ -35,13 +37,15 @@ namespace Npgsql
         public Task<NpgsqlDatabaseInfo> Load(NpgsqlConnection conn, NpgsqlTimeout timeout, bool async)
             => Task.FromResult(
                 new NpgsqlConnectionStringBuilder(conn.ConnectionString).ServerCompatibilityMode == ServerCompatibilityMode.NoTypeLoading
-                    ? (NpgsqlDatabaseInfo)new PostgresMinimalDatabaseInfo()
+                    ? (NpgsqlDatabaseInfo)new PostgresMinimalDatabaseInfo(conn)
                     : null
             );
     }
 
     class PostgresMinimalDatabaseInfo : PostgresDatabaseInfo
     {
+        static readonly Version DefaultVersion = new Version(10, 0);
+
         static readonly PostgresBaseType[] Types = typeof(NpgsqlDbType).GetFields()
             .Select(f => f.GetCustomAttribute<BuiltInPostgresType>())
             .Where(a => a != null)
@@ -49,5 +53,20 @@ namespace Npgsql
             .ToArray();
 
         protected override IEnumerable<PostgresType> GetTypes() => Types;
+
+        internal PostgresMinimalDatabaseInfo([NotNull] NpgsqlConnection conn)
+        {
+            var csb = new NpgsqlConnectionStringBuilder(conn.ConnectionString);
+            Host = csb.Host;
+            Port = csb.Port;
+            Name = csb.Database;
+
+            Version = conn.PostgresParameters.TryGetValue("server_version", out string versionString)
+                ? ParseServerVersion(versionString)
+                : DefaultVersion;
+
+            HasIntegerDateTimes = !conn.PostgresParameters.TryGetValue("integer_datetimes", out var intDateTimes) ||
+                                  intDateTimes == "on";
+        }
     }
 }
