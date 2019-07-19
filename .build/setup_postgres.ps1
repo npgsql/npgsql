f1cef9dfd13695bdee084454ffb827ce3da6247d
Austin Drenski <austin.drenski@gmail.com>
2019-07-19 17:51:32 -0400

    Driver-specific configurations

--- .build/setup_postgres.ps1
+++ .build/setup_postgres.ps1
@@ -9,10 +9,22 @@ bigsql/pgc install pg11
 bigsql/pgc list --extensions pg11
 bigsql/pgc install postgis25-pg11
 
-# Start PostgreSQL
+# Initialize PostgreSQL
 bigsql/pg11/bin/initdb -D PGDATA -E UTF8 -U postgres
-bigsql/pg11/bin/pg_ctl register -D PGDATA -N postgresql -S demand
-net start postgresql
+
+# Configure PostgreSQL
+# TODO: For some reason PostgreSQL doesn't start when these are included?
+#echo "max_prepared_transactions = 10" >> PGDATA/postgresql.conf
+#echo "ssl = true"                     >> PGDATA/postgresql.conf
+#cp .build/server.crt PGDATA/
+#cp .build/server.key PGDATA/
+
+# Start PostgreSQL
+bigsql/pg11/bin/pg_ctl -D PGDATA -l logfile start
 
 # Configure test account
-bigsql/pg11/bin/psql -U postgres -c "CREATE USER npgsql_tests WITH PASSWORD 'npgsql_tests' SUPERUSER";
+bigsql/pg11/bin/psql -U postgres -c "CREATE USER vsts"
+bigsql/pg11/bin/psql -U postgres -c "CREATE USER npgsql_tests WITH PASSWORD 'npgsql_tests' SUPERUSER"
+bigsql/pg11/bin/psql -U postgres -c "CREATE DATABASE npgsql_tests OWNER npgsql_tests"
+bigsql/pg11/bin/psql -U postgres -d npgsql_tests -c "CREATE EXTENSION citext"
+bigsql/pg11/bin/psql -U postgres -d npgsql_tests -c "CREATE EXTENSION postgis"
--- .build/setup_postgres.sh
+++ .build/setup_postgres.sh
@@ -12,8 +12,18 @@ sudo apt-get -y install postgresql-11
 sudo apt-get -y install postgresql-11-postgis-2.5
 sudo apt-get -y install postgresql-11-postgis-2.5-scripts
 
+# Configure PostgreSQL
+echo "max_prepared_transactions = 10" >> /etc/postgresql/11/main/postgresql.conf
+echo "ssl = true"                     >> /etc/postgresql/11/main/postgresql.conf
+cp .build/server.crt /var/lib/postgresql/11/main/
+cp .build/server.key /var/lib/postgresql/11/main/
+
 # Start PostgreSQL
 sudo service postgresql start
 
 # Configure test account
+sudo -u postgres psql -c "CREATE USER vsts"
 sudo -u postgres psql -c "CREATE USER npgsql_tests WITH PASSWORD 'npgsql_tests' SUPERUSER"
+sudo -u postgres psql -c "CREATE DATABASE npgsql_tests OWNER npgsql_tests"
+sudo -u postgres psql -d npgsql_tests -c "CREATE EXTENSION citext"
+sudo -u postgres psql -d npgsql_tests -c "CREATE EXTENSION postgis"
