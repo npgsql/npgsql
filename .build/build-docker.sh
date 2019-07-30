f0257ba968afa2ba06a298ae3bbe203f20a53084
Austin Drenski <austin.drenski@gmail.com>
2019-07-30 11:58:35 -0400

    Switch to npgsql docker image

--- .build/build-docker.sh
+++ .build/build-docker.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 set -e
 
-cp ./docker/initdb-npgsql.sh ./docker/11
-cp ./docker/initdb-npgsql.sh ./docker/10
-cp ./docker/initdb-npgsql.sh ./docker/9.6
+CONTEXT_DIR=$(dirname $0)/docker
 
-cp ./docker/server.crt ./docker/11
-cp ./docker/server.crt ./docker/10
-cp ./docker/server.crt ./docker/9.6
+sudo docker build -t npgsql/postgres:11  --build-arg PG_VERSION=11  $CONTEXT_DIR
+sudo docker build -t npgsql/postgres:10  --build-arg PG_VERSION=10  $CONTEXT_DIR
+sudo docker build -t npgsql/postgres:9.6 --build-arg PG_VERSION=9.6 $CONTEXT_DIR
 
-cp ./docker/server.key ./docker/11
-cp ./docker/server.key ./docker/10
-cp ./docker/server.key ./docker/9.6
-
-docker build -t austindrenski/npgsql-postgres:11  ./docker/11
-docker build -t austindrenski/npgsql-postgres:10  ./docker/10
-docker build -t austindrenski/npgsql-postgres:9.6 ./docker/9.6
+sudo docker push npgsql/postgres:11
+sudo docker push npgsql/postgres:10
+sudo docker push npgsql/postgres:9.6
