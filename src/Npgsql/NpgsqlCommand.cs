ffb42537baaa5dfddc56f17db8dc9d9b0508c974
Shay Rojansky <roji@roji.org>
2019-08-14 13:17:15 +0200

    Revert "Align empty CommandText behavior to SqlClient"
    Removal of breaking changes from 4.1 release.
    
    This reverts commit 9deeb37d5c28117de10daecc692cbed22c00be7b.

--- src/Npgsql/NpgsqlCommand.cs
+++ src/Npgsql/NpgsqlCommand.cs
@@ -75,7 +75,7 @@ namespace Npgsql
         /// <summary>
         /// Initializes a new instance of the <see cref="NpgsqlCommand">NpgsqlCommand</see> class.
         /// </summary>
-        public NpgsqlCommand() : this(null, null, null) {}
+        public NpgsqlCommand() : this(string.Empty, null, null) {}
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NpgsqlCommand">NpgsqlCommand</see> class with the text of the query.
@@ -124,6 +124,9 @@ namespace Npgsql
             get => _commandText;
             set
             {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
                 _commandText = State == CommandState.Idle
                     ? value
                     : throw new InvalidOperationException("An open data reader exists for this command.");
@@ -676,9 +679,6 @@ GROUP BY pg_proc.proargnames, pg_proc.proargtypes, pg_proc.proallargtypes, pg_pr
 
         void ProcessRawQuery(bool deriveParameters = false)
         {
-            if (string.IsNullOrEmpty(CommandText))
-                throw new InvalidOperationException("CommandText property has not been initialized");
-
             NpgsqlStatement statement;
             switch (CommandType) {
             case CommandType.Text:
