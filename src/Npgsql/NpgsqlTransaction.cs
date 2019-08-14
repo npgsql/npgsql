93aa7b7790ebd2d26992ce0f6656d4fbcd0184c7
Shay Rojansky <roji@roji.org>
2019-08-14 13:10:05 +0200

    Brought back some useless overloads to prevent binary break
    See #2414

--- src/Npgsql/NpgsqlTransaction.cs
+++ src/Npgsql/NpgsqlTransaction.cs
@@ -149,6 +149,13 @@ namespace Npgsql
                 return Commit(true);
         }
 
+        // This overload exists only to avoid introducing a binary breaking change in 4.1. Removed in 5.0.
+        /// <summary>
+        /// Commits the database transaction.
+        /// </summary>
+        [PublicAPI]
+        public Task CommitAsync() => CommitAsync(CancellationToken.None);
+
         #endregion
 
         #region Rollback
@@ -181,6 +188,13 @@ namespace Npgsql
             using (NoSynchronizationContextScope.Enter())
                 return Rollback(true);
         }
+
+        // This overload exists only to avoid introducing a binary breaking change in 4.1. Removed in 5.0.
+        /// <summary>
+        /// Rolls back a transaction from a pending state.
+        /// </summary>
+        [PublicAPI]
+        public Task RollbackAsync() => RollbackAsync(CancellationToken.None);
 
         #endregion
 
