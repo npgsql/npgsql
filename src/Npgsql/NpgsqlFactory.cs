b4df2a0e6629529eaf0ae0b7f754da874516e7c7
Shay Rojansky <roji@roji.org>
2019-08-15 02:59:36 +0200

    Stop targeting netcoreapp3.0
    Everything we need is present in netstandard2.1
    
    Closes #2561

--- src/Npgsql/NpgsqlFactory.cs
+++ src/Npgsql/NpgsqlFactory.cs
@@ -51,7 +51,7 @@ namespace Npgsql
         /// </summary>
         public override DbDataAdapter CreateDataAdapter() => new NpgsqlDataAdapter();
 
-#if !NET461 && !NETSTANDARD2_0 && !NETSTANDARD2_1
+#if !NET461 && !NETSTANDARD2_0
         /// <summary>
         /// Specifies whether the specific <see cref="DbProviderFactory"/> supports the <see cref="DbDataAdapter"/> class.
         /// </summary>
