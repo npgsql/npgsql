npgsql/npgsql

commit f1bf198a65a925e6a060e19a7b9b6117a9888788

Small SQL parser refactor around conformant strings

src/Npgsql/NpgsqlConnector.cs
@@ -176,7 +176,7 @@ namespace Npgsql
 
         // This is used by NpgsqlCommand, but we place it on the connector because only one instance is needed
         // at any one time (per connection).
-        internal SqlQueryParser SqlParser { get; } = new SqlQueryParser();
+        internal SqlQueryParser SqlParser { get; }
 
         /// <summary>
         /// A lock that's taken while a user action is in progress, e.g. a command being executed.
@@ -276,7 +276,7 @@ namespace Npgsql
             Settings = settings;
             ConnectionString = connectionString;
             PostgresParameters = new Dictionary<string, string>();
-
+            SqlParser = new SqlQueryParser(UseConformantStrings);
             CancelLock = new object();
 
             _isKeepAliveEnabled = Settings.KeepAlive > 0;

src/Npgsql/SqlQueryParser.cs
@@ -10,10 +10,14 @@ namespace Npgsql
     {
         readonly Dictionary<string, int> _paramIndexMap = new Dictionary<string, int>();
         readonly StringBuilder _rewrittenSql = new StringBuilder();
+        readonly bool _standardConformantStrings;
 
         List<NpgsqlStatement> _statements;
         NpgsqlStatement _statement;
         int _statementIndex;
+
+        internal SqlQueryParser(bool standardConformantStrings)
+            => _standardConformantStrings = standardConformantStrings;
 
         /// <summary>
         /// Receives a raw SQL query as passed in by the user, and performs some processing necessary
@@ -22,15 +26,14 @@ namespace Npgsql
         /// up by semicolons if needed (SELECT 1; SELECT 2)
         /// </summary>
         /// <param name="sql">Raw user-provided query.</param>
-        /// <param name="standardConformantStrings">Whether the PostgreSQL session is configured to use standard conformant strings.</param>
         /// <param name="parameters">The parameters configured on the <see cref="NpgsqlCommand"/> of this query
         /// or an empty <see cref="NpgsqlParameterCollection"/> if deriveParameters is set to true.</param>
         /// <param name="statements">An empty list to be populated with the statements parsed by this method</param>
         /// <param name="deriveParameters">A bool indicating whether parameters contains a list of preconfigured parameters or an empty list to be filled with derived parameters.</param>
-        internal void ParseRawQuery(string sql, bool standardConformantStrings, NpgsqlParameterCollection parameters, List<NpgsqlStatement> statements, bool deriveParameters = false)
-            => ParseRawQuery(sql.AsSpan(), standardConformantStrings, parameters, statements, deriveParameters);
+        internal void ParseRawQuery(string sql, NpgsqlParameterCollection parameters, List<NpgsqlStatement> statements, bool deriveParameters = false)
+            => ParseRawQuery(sql.AsSpan(), parameters, statements, deriveParameters);
 
-        void ParseRawQuery(ReadOnlySpan<char> sql, bool standardConformantStrings, NpgsqlParameterCollection parameters, List<NpgsqlStatement> statements, bool deriveParameters)
+        void ParseRawQuery(ReadOnlySpan<char> sql, NpgsqlParameterCollection parameters, List<NpgsqlStatement> statements, bool deriveParameters)
         {
             Debug.Assert(sql != null);
             Debug.Assert(statements != null);
@@ -65,7 +68,7 @@ namespace Npgsql
                 case '-':
                     goto LineCommentBegin;
                 case '\'':
-                    if (standardConformantStrings)
+                    if (_standardConformantStrings)
                         goto Quoted;
                     else
                         goto Escaped;

test/Npgsql.Tests/SqlQueryParserTests.cs
@@ -12,7 +12,7 @@ namespace Npgsql.Tests
         {
             _params.AddWithValue(":p1", "foo");
             _params.AddWithValue(":p2", "bar");
-            _parser.ParseRawQuery("SELECT :p1, :p2", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT :p1, :p2", _params, _queries);
             Assert.That(_queries.Single().InputParameters, Is.EqualTo(_params));
         }
 
@@ -20,7 +20,7 @@ namespace Npgsql.Tests
         public void ParamNameWithDot()
         {
             _params.AddWithValue(":a.parameter", "foo");
-            _parser.ParseRawQuery("INSERT INTO data (field_char5) VALUES (:a.parameter)", true, _params, _queries);
+            _parser.ParseRawQuery("INSERT INTO data (field_char5) VALUES (:a.parameter)", _params, _queries);
             Assert.That(_queries.Single().InputParameters.Single(), Is.SameAs(_params.Single()));
         }
 
@@ -38,7 +38,7 @@ namespace Npgsql.Tests
         public void Untouched(string sql)
         {
             _params.AddWithValue(":param", "foo");
-            _parser.ParseRawQuery(sql, true, _params, _queries);
+            _parser.ParseRawQuery(sql, _params, _queries);
             Assert.That(_queries.Single().SQL, Is.EqualTo(sql));
             Assert.That(_queries.Single().InputParameters, Is.Empty);
         }
@@ -51,7 +51,7 @@ namespace Npgsql.Tests
         public void ParamGetsBound(string sql)
         {
             _params.AddWithValue(":param", "foo");
-            _parser.ParseRawQuery(sql, true, _params, _queries);
+            _parser.ParseRawQuery(sql, _params, _queries);
             Assert.That(_queries.Single().InputParameters.Single(), Is.SameAs(_params.Single()));
         }
 
@@ -59,7 +59,7 @@ namespace Npgsql.Tests
         public void ParamGetsBoundNonAscii()
         {
             _params.AddWithValue("漢字", "foo");
-            _parser.ParseRawQuery("SELECT @漢字", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT @漢字", _params, _queries);
             Assert.That(_queries.Single().InputParameters.Single(), Is.SameAs(_params.Single()));
         }
 
@@ -72,14 +72,15 @@ namespace Npgsql.Tests
         public void ParamDoesntGetBound(string sql)
         {
             _params.AddWithValue(":param", "foo");
-            _parser.ParseRawQuery(sql, true, _params, _queries);
+            _parser.ParseRawQuery(sql, _params, _queries);
             Assert.That(_queries.Single().InputParameters, Is.Empty);
         }
 
         [Test]
         public void NonConformantStrings()
         {
-            _parser.ParseRawQuery(@"SELECT 'abc\':str''a:str'", false, _params, _queries);
+            var parser = new SqlQueryParser(false);
+            parser.ParseRawQuery(@"SELECT 'abc\':str''a:str'", _params, _queries);
             Assert.That(_queries.Single().SQL, Is.EqualTo(@"SELECT 'abc\':str''a:str'"));
             Assert.That(_queries.Single().InputParameters, Is.Empty);
         }
@@ -94,7 +95,7 @@ namespace Npgsql.Tests
             var p3 = new NpgsqlParameter("p3", DbType.String);
             _params.Add(p3);
 
-            _parser.ParseRawQuery("SELECT @p3, @p1; SELECT @p2, @p3", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT @p3, @p1; SELECT @p2, @p3", _params, _queries);
 
             Assert.That(_queries, Has.Count.EqualTo(2));
             Assert.That(_queries[0].InputParameters[0], Is.SameAs(p3));
@@ -108,13 +109,13 @@ namespace Npgsql.Tests
         {
             var p = new NpgsqlParameter("p", DbType.String) { Direction = ParameterDirection.Output };
             _params.Add(p);
-            Assert.That(() => _parser.ParseRawQuery("SELECT @p", true, _params, _queries), Throws.Exception);
+            Assert.That(() => _parser.ParseRawQuery("SELECT @p", _params, _queries), Throws.Exception);
         }
 
         [Test]
         public void MissingParamIsIgnored()
         {
-            _parser.ParseRawQuery("SELECT @p; SELECT 1", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT @p; SELECT 1", _params, _queries);
             Assert.That(_queries[0].SQL, Is.EqualTo("SELECT @p"));
             Assert.That(_queries[1].SQL, Is.EqualTo("SELECT 1"));
             Assert.That(_queries[0].InputParameters, Is.Empty);
@@ -124,44 +125,44 @@ namespace Npgsql.Tests
         [Test]
         public void ConsecutiveSemicolons()
         {
-            _parser.ParseRawQuery(";;SELECT 1", true, _params, _queries);
+            _parser.ParseRawQuery(";;SELECT 1", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(1));
         }
 
         [Test]
         public void TrailingSemicolon()
         {
-            _parser.ParseRawQuery("SELECT 1;", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT 1;", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(1));
         }
 
         [Test]
         public void Empty()
         {
-            _parser.ParseRawQuery("", true, _params, _queries);
+            _parser.ParseRawQuery("", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(1));
         }
 
         [Test]
         public void SemicolonInParentheses()
         {
-            _parser.ParseRawQuery("CREATE OR REPLACE RULE test AS ON UPDATE TO test DO (SELECT 1; SELECT 1)", true, _params, _queries);
+            _parser.ParseRawQuery("CREATE OR REPLACE RULE test AS ON UPDATE TO test DO (SELECT 1; SELECT 1)", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(1));
         }
 
         [Test]
         public void SemicolonAfterParentheses()
         {
-            _parser.ParseRawQuery("CREATE OR REPLACE RULE test AS ON UPDATE TO test DO (SELECT 1); SELECT 1", true, _params, _queries);
+            _parser.ParseRawQuery("CREATE OR REPLACE RULE test AS ON UPDATE TO test DO (SELECT 1); SELECT 1", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(2));
         }
 
         [Test]
         public void ReduceNumberOfStatements()
         {
-            _parser.ParseRawQuery("SELECT 1; SELECT 2", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT 1; SELECT 2", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(2));
-            _parser.ParseRawQuery("SELECT 1", true, _params, _queries);
+            _parser.ParseRawQuery("SELECT 1", _params, _queries);
             Assert.That(_queries, Has.Count.EqualTo(1));
         }
 
@@ -169,7 +170,7 @@ namespace Npgsql.Tests
         [Test]
         public void TrimWhitespace()
         {
-            _parser.ParseRawQuery("   SELECT 1\t", true, _params, _queries);
+            _parser.ParseRawQuery("   SELECT 1\t", _params, _queries);
             Assert.That(_queries.Single().Sql, Is.EqualTo("SELECT 1"));
         }
 #endif
@@ -183,7 +184,7 @@ namespace Npgsql.Tests
         [SetUp]
         public void SetUp()
         {
-            _parser = new SqlQueryParser();
+            _parser = new SqlQueryParser(true);
             _queries = new List<NpgsqlStatement>();
             _params = new NpgsqlParameterCollection();
         }

src/Npgsql/NpgsqlCommand.cs
@@ -695,7 +695,7 @@ GROUP BY pg_proc.proargnames, pg_proc.proargtypes, pg_proc.proallargtypes, pg_pr
             case CommandType.Text:
                 Debug.Assert(_connection?.Connector != null);
                 var connector = _connection.Connector;
-                connector.SqlParser.ParseRawQuery(CommandText, connector.UseConformantStrings, _parameters, _statements, deriveParameters);
+                connector.SqlParser.ParseRawQuery(CommandText, _parameters, _statements, deriveParameters);
                 if (_statements.Count > 1 && _parameters.HasOutputParameters)
                     throw new NotSupportedException("Commands with multiple queries cannot have out parameters");
                 break;

commit 82553fb87fd9ab32ccea70d9edde49502c0f2c33

Fix SQL query parsing
Our SQL query parser supports an ancient "non-conforming string" mode
from pre PG 9.1. Unfortunately, commit
f1bf198a65a925e6a060e19a7b9b6117a9888788 introduced a bug where we
erroneously went into this mode.

Fixes #2660

src/Npgsql/NpgsqlConnector.cs
@@ -262,7 +262,7 @@ namespace Npgsql
             Settings = settings;
             ConnectionString = connectionString;
             PostgresParameters = new Dictionary<string, string>();
-            SqlParser = new SqlQueryParser(UseConformantStrings);
+            SqlParser = new SqlQueryParser();
             Transaction = new NpgsqlTransaction(this);
 
             CancelLock = new object();
@@ -1872,7 +1872,7 @@ namespace Npgsql
 
         #region Supported features and PostgreSQL settings
 
-        internal bool UseConformantStrings { get; private set; }
+        internal bool UseConformingStrings { get; private set; }
 
         /// <summary>
         /// The connection's timezone as reported by PostgreSQL, in the IANA/Olson database format.
@@ -1941,7 +1941,8 @@ namespace Npgsql
             switch (name)
             {
             case "standard_conforming_strings":
-                UseConformantStrings = (value == "on");
+                UseConformingStrings = value == "on";
+                SqlParser.StandardConformingStrings = UseConformingStrings;
                 return;
 
             case "TimeZone":

src/Npgsql/SqlQueryParser.cs
@@ -13,14 +13,11 @@ namespace Npgsql
     {
         readonly Dictionary<string, int> _paramIndexMap = new Dictionary<string, int>();
         readonly StringBuilder _rewrittenSql = new StringBuilder();
-        readonly bool _standardConformantStrings;
+        internal bool StandardConformingStrings { get; set; } = true;
 
         List<NpgsqlStatement> _statements = default!;
         NpgsqlStatement _statement = default!;
         int _statementIndex;
-
-        internal SqlQueryParser(bool standardConformantStrings)
-            => _standardConformantStrings = standardConformantStrings;
 
         /// <summary>
         /// Receives a raw SQL query as passed in by the user, and performs some processing necessary
@@ -68,7 +65,7 @@ namespace Npgsql
                 case '-':
                     goto LineCommentBegin;
                 case '\'':
-                    if (_standardConformantStrings)
+                    if (StandardConformingStrings)
                         goto Quoted;
                     else
                         goto Escaped;

test/Npgsql.Tests/BugTests.cs
@@ -462,6 +462,23 @@ namespace Npgsql.Tests
             }
         }
 
+        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/2660")]
+        public void StandardConformingStrings()
+        {
+            using var conn = OpenConnection();
+
+            var sql = @"
+SELECT table_name
+FROM information_schema.views
+WHERE table_name LIKE @p0 escape '\' AND (is_updatable = 'NO') = @p1";
+
+            using var cmd = new NpgsqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@p0", "%trig%");
+            cmd.Parameters.AddWithValue("@p1", true);
+            using var reader = cmd.ExecuteReader();
+            reader.Read();
+        }
+
         #region Bug1285
 
         [Test, IssueLink("https://github.com/npgsql/npgsql/issues/1285")]

test/Npgsql.Tests/SqlQueryParserTests.cs
@@ -77,9 +77,9 @@ namespace Npgsql.Tests
         }
 
         [Test]
-        public void NonConformantStrings()
+        public void NonConformingStrings()
         {
-            var parser = new SqlQueryParser(false);
+            var parser = new SqlQueryParser() { StandardConformingStrings = false };
             parser.ParseRawQuery(@"SELECT 'abc\':str''a:str'", _params, _queries);
             Assert.That(_queries.Single().SQL, Is.EqualTo(@"SELECT 'abc\':str''a:str'"));
             Assert.That(_queries.Single().InputParameters, Is.Empty);
@@ -184,7 +184,7 @@ namespace Npgsql.Tests
         [SetUp]
         public void SetUp()
         {
-            _parser = new SqlQueryParser(true);
+            _parser = new SqlQueryParser();
             _queries = new List<NpgsqlStatement>();
             _params = new NpgsqlParameterCollection();
         }