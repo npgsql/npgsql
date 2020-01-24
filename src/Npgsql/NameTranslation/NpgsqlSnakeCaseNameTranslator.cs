ddc9ecdbf5f8dbeb4e53071c864147dfe9e495c1
YohDeadfall <yoh.deadfall@hotmail.com>
2020-01-24 17:58:56 +0300

    Fixed broken snake case translator

--- src/Npgsql/NameTranslation/NpgsqlSnakeCaseNameTranslator.cs
+++ src/Npgsql/NameTranslation/NpgsqlSnakeCaseNameTranslator.cs
@@ -51,10 +51,9 @@ namespace Npgsql.NameTranslation
         public static string ConvertToSnakeCase(string value)
         {
             const char underscore = '_';
-            const UnicodeCategory noneCategory = UnicodeCategory.Control;
 
             var builder = new StringBuilder();
-            var previousCategory = noneCategory;
+            var previousCategory = default(UnicodeCategory?);
 
             for (var currentIndex = 0; currentIndex< value.Length; currentIndex++)
             {
@@ -62,7 +61,7 @@ namespace Npgsql.NameTranslation
                 if (currentChar == underscore)
                 {
                     builder.Append(underscore);
-                    previousCategory = noneCategory;
+                    previousCategory = null;
                     continue;
                 }
 
@@ -92,7 +91,7 @@ namespace Npgsql.NameTranslation
                         break;
 
                     default:
-                        if (previousCategory != noneCategory)
+                        if (previousCategory != null)
                             previousCategory = UnicodeCategory.SpaceSeparator;
                         continue;
                 }
