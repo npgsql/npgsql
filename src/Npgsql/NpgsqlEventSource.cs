07bf156ac8522c92a98d24f878f0319d32466a95
Victor Gladkikh <victor.gladkikh@gmail.com>
2020-02-20 13:46:43 +0200

    Remove Console.WriteLine from NpgsqlEventSource
    If application starts listening to `NpgsqlEventSource` events, then console ouput is polluted with "random numbers".
    
    Introduced in https://github.com/npgsql/npgsql/pull/2571
    
    (cherry picked from commit cb46ddb87ba86c1b4812e284fba1c044c9a6c847)

--- src/Npgsql/NpgsqlEventSource.cs
+++ src/Npgsql/NpgsqlEventSource.cs
@@ -147,12 +147,10 @@ namespace Npgsql
                     DisplayName = "Failed Commands"
                 };
 
-//                _preparedCommandsRatioCounter = new PollingCounter("prepared-commands-ratio", this, () => (double)_totalPreparedCommands / (double)_totalCommands)
-                _preparedCommandsRatioCounter = new PollingCounter("prepared-commands-ratio", this, () =>
-                {
-                    Console.WriteLine($"{(double)_totalPreparedCommands} / {(double)_totalCommands}");
-                    return (double)_totalPreparedCommands / (double)_totalCommands;
-                })
+                _preparedCommandsRatioCounter = new PollingCounter(
+                    "prepared-commands-ratio",
+                    this,
+                    () => (double)_totalPreparedCommands / (double)_totalCommands)
                 {
                     DisplayName = "Prepared Commands Ratio",
                     DisplayUnits = "%"
