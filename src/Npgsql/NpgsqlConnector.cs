916141eb8236bab95b9be501281e8d044b6e1778
Clemens Ladisch <clemens@ladisch.de>
2020-03-09 12:27:05 +0100

    Fix check for wait with timeout + SSL
    #1501
    
    (cherry picked from commit 572918fc3a94cf201ba947dac341d1a1de38f7b9)

--- src/Npgsql/NpgsqlConnector.cs
+++ src/Npgsql/NpgsqlConnector.cs
@@ -1751,7 +1751,7 @@ namespace Npgsql
 
         public bool Wait(int timeout)
         {
-            if ((timeout == 0 || timeout == -1) && IsSecure)
+            if ((timeout != 0 && timeout != -1) && IsSecure)
                 throw new NotSupportedException("Wait() with timeout isn't supported when SSL is used, see https://github.com/npgsql/npgsql/issues/1501");
 
             using (StartUserAction(ConnectorState.Waiting))
