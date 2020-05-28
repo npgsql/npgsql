318b9075ba9f81fa09355550abb7732df13814d8
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-05-28 12:31:28 +0300

    Optimized and made AOT friendly composite mappings

--- src/Npgsql/TypeMapping/TypeMapperBase.cs
+++ src/Npgsql/TypeMapping/TypeMapperBase.cs
@@ -80,43 +80,40 @@ namespace Npgsql.TypeMapping
         #region Composite mapping
 
         public INpgsqlTypeMapper MapComposite<T>(string? pgName = null, INpgsqlNameTranslator? nameTranslator = null)
-            => MapComposite(typeof(T), pgName, nameTranslator);
+            => MapComposite(pgName, nameTranslator, typeof(T), t => new CompositeTypeHandlerFactory<T>(t));
+
+        public INpgsqlTypeMapper MapComposite(Type clrType, string? pgName = null, INpgsqlNameTranslator? nameTranslator = null)
+            => MapComposite(pgName, nameTranslator, clrType, t => (NpgsqlTypeHandlerFactory)
+                Activator.CreateInstance(typeof(CompositeTypeHandlerFactory<>).MakeGenericType(clrType), t)!);
+
+        INpgsqlTypeMapper MapComposite(string? pgName, INpgsqlNameTranslator? nameTranslator, Type type, Func<INpgsqlNameTranslator, NpgsqlTypeHandlerFactory> factory)
+        {
+            if (pgName != null && string.IsNullOrWhiteSpace(pgName))
+                throw new ArgumentException("pgName can't be empty.", nameof(pgName));
+
+            nameTranslator ??= DefaultNameTranslator;
+            pgName ??= GetPgName(type, nameTranslator);
+
+            return AddMapping(
+                new NpgsqlTypeMappingBuilder
+                {
+                    PgTypeName = pgName,
+                    ClrTypes = new[] { type },
+                    TypeHandlerFactory = factory(nameTranslator),
+                }
+                .Build());
+        }
 
         public bool UnmapComposite<T>(string? pgName = null, INpgsqlNameTranslator? nameTranslator = null)
             => UnmapComposite(typeof(T), pgName, nameTranslator);
 
-        public INpgsqlTypeMapper MapComposite(Type compType, string? pgName = null, INpgsqlNameTranslator? nameTranslator = null) {
-            if (pgName != null && pgName.Trim() == "")
-                throw new ArgumentException("pgName can't be empty", nameof(pgName));
+        public bool UnmapComposite(Type clrType, string? pgName = null, INpgsqlNameTranslator? nameTranslator = null)
+        {
+            if (pgName != null && string.IsNullOrWhiteSpace(pgName))
+                throw new ArgumentException("pgName can't be empty.", nameof(pgName));
 
-            if (nameTranslator == null)
-                nameTranslator = DefaultNameTranslator;
-            if (pgName == null)
-                pgName = GetPgName(compType, nameTranslator);
-
-            var thfType = typeof(CompositeTypeHandlerFactory<>);
-            var thf = (NpgsqlTypeHandlerFactory)Activator.CreateInstance(
-                thfType.MakeGenericType(compType),
-                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic,
-                binder: null,
-                args: new object[] { nameTranslator },
-                culture: null)!;
-
-            return AddMapping(new NpgsqlTypeMappingBuilder {
-                PgTypeName = pgName,
-                ClrTypes = new[] { compType },
-                TypeHandlerFactory = thf,
-            }.Build());
-        }
-
-        public bool UnmapComposite(Type compType, string? pgName = null, INpgsqlNameTranslator? nameTranslator = null) {
-            if (pgName != null && pgName.Trim() == "")
-                throw new ArgumentException("pgName can't be empty", nameof(pgName));
-
-            if (nameTranslator == null)
-                nameTranslator = DefaultNameTranslator;
-            if (pgName == null)
-                pgName = GetPgName(compType, nameTranslator);
+            nameTranslator ??= DefaultNameTranslator;
+            pgName ??= GetPgName(clrType, nameTranslator);
 
             return RemoveMapping(pgName);
         }
@@ -127,9 +124,9 @@ namespace Npgsql.TypeMapping
 
         // TODO: why does ReSharper think `GetCustomAttribute<T>` is non-nullable?
         // ReSharper disable once ConstantConditionalAccessQualifier ConstantNullCoalescingCondition
-        static string GetPgName(Type compType, INpgsqlNameTranslator nameTranslator)
-            => compType.GetCustomAttribute<PgNameAttribute>()?.PgName
-               ?? nameTranslator.TranslateTypeName(compType.Name);
+        static string GetPgName(Type clrType, INpgsqlNameTranslator nameTranslator)
+            => clrType.GetCustomAttribute<PgNameAttribute>()?.PgName
+               ?? nameTranslator.TranslateTypeName(clrType.Name);
 
         #endregion Misc
     }
