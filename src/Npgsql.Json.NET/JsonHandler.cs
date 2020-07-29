f97342855824499118caddd54d9cef7ba09c23ce
warcha <wchan_syd@yahoo.com.au>
2020-07-29 18:52:48 +1000

    Fixed reading of string like types by JSON.NET plugin
    (cherry picked from commit 8a78e44a446a408d4b8aaa7ab9cc3456e312152e)

--- src/Npgsql.Json.NET/JsonHandler.cs
+++ src/Npgsql.Json.NET/JsonHandler.cs
@@ -29,12 +29,18 @@ namespace Npgsql.Json.NET
 
         protected override async ValueTask<T> Read<T>(NpgsqlReadBuffer buf, int len, bool async, FieldDescription? fieldDescription = null)
         {
-            var s = await base.Read<string>(buf, len, async, fieldDescription);
-            if (typeof(T) == typeof(string))
-                return (T)(object)s;
+            if (typeof(T) == typeof(string) ||
+                typeof(T) == typeof(char[]) ||
+                typeof(T) == typeof(ArraySegment<char>) ||
+                typeof(T) == typeof(char) ||
+                typeof(T) == typeof(byte[]))
+            {
+                return await base.Read<T>(buf, len, async, fieldDescription);
+            }
+
             try
             {
-                return JsonConvert.DeserializeObject<T>(s, _settings);
+                return JsonConvert.DeserializeObject<T>(await base.Read<string>(buf, len, async, fieldDescription), _settings);
             }
             catch (Exception e)
             {
