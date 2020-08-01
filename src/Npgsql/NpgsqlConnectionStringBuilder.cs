24bf11366ff4ef50c758a0a360718ddd531a618e
Rob Emery <mintsoft@users.noreply.github.com>
2020-08-01 22:29:20 +0100

    Reintroducing The connectionstring property for Primary/Secondary server type

--- src/Npgsql/NpgsqlConnectionStringBuilder.cs
+++ src/Npgsql/NpgsqlConnectionStringBuilder.cs
@@ -502,6 +502,25 @@ namespace Npgsql
         }
         string? _timezone;
 
+        /// <summary>
+        /// Gets or sets the PostgreSQL target server.
+        /// </summary>
+        [Category("Connection")]
+        [Description("Gets or sets the PostgreSQL target server.")]
+        [DisplayName("TargetServerType")]
+        [DefaultValue(TargetServerType.Any)]
+        [NpgsqlConnectionStringProperty]
+        public TargetServerType TargetServerType
+        {
+            get => _targetservertype;
+            set
+            {
+                _targetservertype = value;
+                SetValue(nameof(TargetServerType), value);
+            }
+        }
+        TargetServerType _targetservertype;
+
         #endregion
 
         #region Properties - Security
@@ -1736,5 +1755,24 @@ namespace Npgsql
         /// </summary>
         Logical
     }
+
+    /// Specifies server type preference.
+    /// </summary>
+    public enum TargetServerType
+    {
+        /// <summary>
+        ///  Try to connect every server in order.
+        /// </summary>
+        Any,
+        /// <summary>
+        ///  Primary (writable) server is selected for the connection.
+        /// </summary>
+        Primary,
+        /// <summary>
+        ///  Secondary (readonly) server is selected for the connection.
+        /// </summary>
+        Secondary,
+    }
+
     #endregion
 }
