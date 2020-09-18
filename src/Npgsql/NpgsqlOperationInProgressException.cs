8a51a7c52fc8a428bd4bea643e8e91dc211535dd
Shay Rojansky <roji@roji.org>
2020-09-18 15:32:46 +0300

    Make NpgsqlOperationInProgress inherit from NpgsqlException
    Closes #2864

--- src/Npgsql/NpgsqlOperationInProgressException.cs
+++ src/Npgsql/NpgsqlOperationInProgressException.cs
@@ -1,14 +1,18 @@
-﻿using System;
-
-namespace Npgsql
+﻿namespace Npgsql
 {
     /// <summary>
     /// Thrown when trying to use a connection that is already busy performing some other operation.
     /// Provides information on the already-executing operation to help with debugging.
     /// </summary>
-    public sealed class NpgsqlOperationInProgressException : InvalidOperationException
+    public sealed class NpgsqlOperationInProgressException : NpgsqlException
     {
-        internal NpgsqlOperationInProgressException(NpgsqlCommand command)
+        /// <summary>
+        /// Creates a new instance of <see cref="NpgsqlOperationInProgressException" />.
+        /// </summary>
+        /// <param name="command">
+        /// A command which was in progress when the operation which triggered this exception was executed.
+        /// </param>
+        public NpgsqlOperationInProgressException(NpgsqlCommand command)
             : base("A command is already in progress: " + command.CommandText)
         {
             CommandInProgress = command;
@@ -21,7 +25,8 @@ namespace Npgsql
 
         /// <summary>
         /// If the connection is busy with another command, this will contain a reference to that command.
-        /// Otherwise, if the connection if busy with another type of operation (e.g. COPY), contains null.
+        /// Otherwise, if the connection if busy with another type of operation (e.g. COPY), contains
+        /// <see langword="null" />.
         /// </summary>
         public NpgsqlCommand? CommandInProgress { get; }
     }
