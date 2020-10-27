5b047828eb96a4f26cd97b0bd6adf448d67e36b5
Nikita Kazmin <vonzshik@gmail.com>
2020-10-27 08:13:32 +0300

    Made block a readonly struct

--- src/Npgsql/HardCancellationBlock.cs
+++ src/Npgsql/HardCancellationBlock.cs
@@ -3,31 +3,28 @@ using System.Threading;
 
 namespace Npgsql
 {
-    internal class HardCancellationBlock : IDisposable
+    readonly struct HardCancellationBlock : IDisposable
     {
-        bool disposed;
-
         static readonly AsyncLocal<bool> _entered = new AsyncLocal<bool>();
 
         public static bool Entered => _entered.Value;
 
+        readonly bool _outerBlock;
+
+        public HardCancellationBlock(bool outerBlock) =>_outerBlock = outerBlock;
+
         public static IDisposable Enter()
         {
-            var context = new HardCancellationBlock();
-            if (Entered)
-                context.disposed = true;
-            else
+            var context = new HardCancellationBlock(!Entered);
+            if (!Entered)
                 _entered.Value = true;
             return context;
         }
 
         public void Dispose()
         {
-            if (!disposed)
-            {
+            if (_outerBlock)
                 _entered.Value = false;
-                disposed = true;
-            }
         }
     }
 }
