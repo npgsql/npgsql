4aae8c443050adf68d049f020c4adb9f3be037ed
Shay Rojansky <roji@roji.org>
2020-11-01 14:20:33 +0200

    Cleanup of LSN

--- src/Npgsql/NpgsqlTypes/NpgsqlLogSequenceNumber.cs
+++ src/Npgsql/NpgsqlTypes/NpgsqlLogSequenceNumber.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using JetBrains.Annotations;
 
 // ReSharper disable once CheckNamespace
 namespace NpgsqlTypes
@@ -29,7 +28,6 @@ namespace NpgsqlTypes
         /// Initializes a new instance of <see cref="NpgsqlLogSequenceNumber"/>.
         /// </summary>
         /// <param name="value">The value to wrap.</param>
-        [PublicAPI]
         public NpgsqlLogSequenceNumber(ulong value)
             => _value = value;
 
@@ -38,19 +36,17 @@ namespace NpgsqlTypes
         /// instance.
         /// </summary>
         /// <param name="other">A <see cref="NpgsqlLogSequenceNumber"/> instance to compare to this instance.</param>
-        /// <returns><c>true</c> if the current instance is equal to the value parameter;
-        /// otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns><see langword="true" /> if the current instance is equal to the value parameter;
+        /// otherwise, <see langword="false" />.</returns>
         public bool Equals(NpgsqlLogSequenceNumber other)
-            => _value.Equals(other._value);
+            => _value == other._value;
 
         /// <summary>
         /// Compares this instance to a specified <see cref="NpgsqlLogSequenceNumber"/> and returns an indication of their
         /// relative values.
         /// </summary>
         /// <param name="value">A <see cref="NpgsqlLogSequenceNumber"/> instance to compare to this instance.</param>
-        /// <returns>A signed number indicating the relative values of this instance and <c>value</c>.</returns>
-        [PublicAPI]
+        /// <returns>A signed number indicating the relative values of this instance and <paramref name="value" />.</returns>
         public int CompareTo(NpgsqlLogSequenceNumber value)
             => _value.CompareTo(value._value);
 
@@ -58,17 +54,15 @@ namespace NpgsqlTypes
         /// Returns a value indicating whether this instance is equal to a specified object.
         /// </summary>
         /// <param name="obj">An object to compare to this instance</param>
-        /// <returns><c>true</c> if the current instance is equal to the value parameter;
-        /// otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns><see langword="true" /> if the current instance is equal to the value parameter;
+        /// otherwise, <see langword="false" />.</returns>
         public override bool Equals(object? obj)
-            => obj is NpgsqlLogSequenceNumber lsn && lsn._value.Equals(_value);
+            => obj is NpgsqlLogSequenceNumber lsn && lsn._value == _value;
 
         /// <summary>
         /// Returns the hash code for this instance.
         /// </summary>
         /// <returns>A 32-bit signed integer hash code.</returns>
-        [PublicAPI]
         public override int GetHashCode()
             => _value.GetHashCode();
 
@@ -77,7 +71,6 @@ namespace NpgsqlTypes
         /// </summary>
         /// <returns>The string representation of the value of this instance, consisting of two hexadecimal numbers of
         /// up to 8 digits each, separated by a slash</returns>
-        [PublicAPI]
         public override string ToString()
             => unchecked($"{(uint)(_value >> 32):X}/{(uint)_value:X}");
 
@@ -86,19 +79,17 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="s">A string that represents the Log Sequence Number to convert.</param>
         /// <returns>
-        /// A <see cref="NpgsqlLogSequenceNumber"/> equivalent to the Log Sequence Number specified in <c>s</c>.
+        /// A <see cref="NpgsqlLogSequenceNumber"/> equivalent to the Log Sequence Number specified in <paramref name="s" />.
         /// </returns>
-        /// <exception cref="ArgumentNullException">The <c>s</c> parameter is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="s" /> parameter is <see langword="null"/>.</exception>
         /// <exception cref="OverflowException">
-        /// The <c>s</c> parameter represents a number less than <see cref="ulong.MinValue"/> or greater than
+        /// The <paramref name="s" /> parameter represents a number less than <see cref="ulong.MinValue"/> or greater than
         /// <see cref="ulong.MaxValue"/>.
         /// </exception>
-        /// <exception cref="FormatException">The <c>s</c> parameter is not in the right format.</exception>
-        [PublicAPI]
+        /// <exception cref="FormatException">The <paramref name="s" /> parameter is not in the right format.</exception>
         public static NpgsqlLogSequenceNumber Parse(string s)
-            // We certainly want to play it safe on a public API
             // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-            => s == null
+            => s is null
                 ? throw new ArgumentNullException(nameof(s))
                 : Parse(s.AsSpan());
 
@@ -107,80 +98,59 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="s">A span containing the characters that represent the Log Sequence Number to convert.</param>
         /// <returns>
-        /// A <see cref="NpgsqlLogSequenceNumber"/> equivalent to the Log Sequence Number specified in <c>s</c>.
+        /// A <see cref="NpgsqlLogSequenceNumber"/> equivalent to the Log Sequence Number specified in <paramref name="s" />.
         /// </returns>
         /// <exception cref="OverflowException">
-        /// The <c>s</c> parameter represents a number less than <see cref="ulong.MinValue"/> or greater than
+        /// The <paramref name="s" /> parameter represents a number less than <see cref="ulong.MinValue"/> or greater than
         /// <see cref="ulong.MaxValue"/>.
         /// </exception>
-        /// <exception cref="FormatException">The <c>s</c> parameter is not in the right format.</exception>
-        [PublicAPI]
+        /// <exception cref="FormatException">The <paramref name="s" /> parameter is not in the right format.</exception>
         public static NpgsqlLogSequenceNumber Parse(ReadOnlySpan<char> s)
-        {
-            for (var i = 0; i < s.Length; i++)
-            {
-                if (s[i] == '/')
-                {
-
-#if NET461 || NETSTANDARD2_0
-                    var firstPart = s.Slice(0, i).ToString();
-                    var secondPart = s.Slice(++i).ToString();
-#else
-                    var firstPart = s.Slice(0, i);
-                    var secondPart = s.Slice(++i);
-#endif
-                    return new NpgsqlLogSequenceNumber(((ulong)uint.Parse(firstPart, NumberStyles.AllowHexSpecifier) << 32) + uint.Parse(secondPart, NumberStyles.AllowHexSpecifier));
-                }
-            }
-            throw new FormatException($"Invalid Log Sequence Number: '{s.ToString()}'.");
-        }
+            => TryParse(s, out var parsed)
+                ? parsed
+                : throw new FormatException($"Invalid Log Sequence Number: '{s.ToString()}'.");
 
         /// <summary>
-        /// Tries to convert the string representation of a Log Sequence Number to a <see cref="NpgsqlLogSequenceNumber"/>
+        /// Tries to convert the string representation of a Log Sequence Number to an <see cref="NpgsqlLogSequenceNumber"/>
         /// instance. A return value indicates whether the conversion succeeded or failed.
         /// </summary>
         /// <param name="s">A string that represents the Log Sequence Number to convert.</param>
         /// <param name="result">
         /// When this method returns, contains a <see cref="NpgsqlLogSequenceNumber"/> instance equivalent to the Log Sequence
-        /// Number contained in s, if the conversion succeeded, or the default value for
-        /// <see cref="NpgsqlLogSequenceNumber"/> (<c>0</c>) if the conversion failed. The conversion fails if the <c>s</c>
+        /// Number contained in <paramref name="s"/>, if the conversion succeeded, or the default value for
+        /// <see cref="NpgsqlLogSequenceNumber"/> (<c>0</c>) if the conversion failed. The conversion fails if the <paramref name="s" />
         /// parameter is <see langword="null"/> or <see cref="string.Empty"/>, is not in the right format, or represents a number
         /// less than <see cref="ulong.MinValue"/> or greater than <see cref="ulong.MaxValue"/>. This parameter is
         /// passed uninitialized; any value originally supplied in result will be overwritten.
         /// </param>
-        /// <returns><c>true</c> if <c>s</c> was converted successfully; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="s"/>c> was converted successfully; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool TryParse(string s, out NpgsqlLogSequenceNumber result)
-        {
-            if (s != null)
-                return TryParse(s.AsSpan(), out result);
-
-            result = default;
-            return false;
-        }
+            => TryParse(s.AsSpan(), out result);
 
         /// <summary>
-        /// Tries to convert the span representation of a Log Sequence Number to a <see cref="NpgsqlLogSequenceNumber"/>
+        /// Tries to convert the span representation of a Log Sequence Number to an <see cref="NpgsqlLogSequenceNumber"/>
         /// instance. A return value indicates whether the conversion succeeded or failed.
         /// </summary>
         /// <param name="s">A span containing the characters that represent the Log Sequence Number to convert.</param>
         /// <param name="result">
         /// When this method returns, contains a <see cref="NpgsqlLogSequenceNumber"/> instance equivalent to the Log Sequence
-        /// Number contained in s, if the conversion succeeded, or the default value for
-        /// <see cref="NpgsqlLogSequenceNumber"/> (<c>0</c>) if the conversion failed. The conversion fails if the <c>s</c>
+        /// Number contained in <paramref name="s"/>, if the conversion succeeded, or the default value for
+        /// <see cref="NpgsqlLogSequenceNumber"/> (<c>0</c>) if the conversion failed. The conversion fails if the <paramref name="s" />
         /// parameter is empty, is not in the right format, or represents a number less than
         /// <see cref="ulong.MinValue"/> or greater than <see cref="ulong.MaxValue"/>. This parameter is passed
         /// uninitialized; any value originally supplied in result will be overwritten.
         /// </param>
-        /// <returns><c>true</c> if <c>s</c> was converted successfully; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="s"/> was converted successfully; otherwise, <see langword="false" />.</returns>
         public static bool TryParse(ReadOnlySpan<char> s, out NpgsqlLogSequenceNumber result)
         {
             for (var i = 0; i < s.Length; i++)
             {
                 if (s[i] != '/') continue;
 
-#if NET461 || NETSTANDARD2_0
+#if NETSTANDARD2_0
                 var firstPart = s.Slice(0, i).ToString();
                 var secondPart = s.Slice(++i).ToString();
 #else
@@ -209,8 +179,7 @@ namespace NpgsqlTypes
         /// Converts the value of a 64-bit unsigned integer to a <see cref="NpgsqlLogSequenceNumber"/> instance.
         /// </summary>
         /// <param name="value">A 64-bit unsigned integer.</param>
-        /// <returns>A new instance of <see cref="NpgsqlLogSequenceNumber"/> initialized to <c>value</c>.</returns>
-        [PublicAPI]
+        /// <returns>A new instance of <see cref="NpgsqlLogSequenceNumber"/> initialized to <paramref name="value" />.</returns>
         public static explicit operator NpgsqlLogSequenceNumber(ulong value)
             => new NpgsqlLogSequenceNumber(value);
 
@@ -218,8 +187,7 @@ namespace NpgsqlTypes
         /// Converts the value of a <see cref="NpgsqlLogSequenceNumber"/> instance to a 64-bit unsigned integer value.
         /// </summary>
         /// <param name="value">A <see cref="NpgsqlLogSequenceNumber"/> instance</param>
-        /// <returns>The contents of <c>value</c> as 64-bit unsigned integer.</returns>
-        [PublicAPI]
+        /// <returns>The contents of <paramref name="value" /> as 64-bit unsigned integer.</returns>
         public static explicit operator ulong(NpgsqlLogSequenceNumber value)
             => value._value;
 
@@ -228,10 +196,11 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="value1">The first Log Sequence Number to compare.</param>
         /// <param name="value2">The second Log Sequence Number to compare.</param>
-        /// <returns><c>true</c> if <c>value1</c> equals <c>value2</c>; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="value1" /> equals <paramref name="value2" />; otherwise, <see langword="false" />.
+        /// </returns>
         public static bool operator ==(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
-            => value1._value.Equals(value2._value);
+            => value1._value == value2._value;
 
         /// <summary>
         /// Returns a value that indicates whether two specified instances of <see cref="NpgsqlLogSequenceNumber"/> are not
@@ -239,10 +208,12 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="value1">The first Log Sequence Number to compare.</param>
         /// <param name="value2">The second Log Sequence Number to compare.</param>
-        /// <returns><c>true</c> if <c>value1</c> does not equal <c>value2</c>; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="value1" /> does not equal <paramref name="value2" />; otherwise,
+        /// <see langword="false" />.
+        /// </returns>
         public static bool operator !=(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
-            => !value1._value.Equals(value2._value);
+            => value1._value != value2._value;
 
         /// <summary>
         /// Returns a value indicating whether a specified <see cref="NpgsqlLogSequenceNumber"/> instance is greater than
@@ -250,8 +221,10 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="value1">The first value to compare.</param>
         /// <param name="value2">The second value to compare.</param>
-        /// <returns><c>true</c> if <c>value1</c> is greater than <c>value2</c>; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="value1" /> is greater than <paramref name="value2" />; otherwise,
+        /// <see langword="false" />.
+        /// </returns>
         public static bool operator >(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
             => value1._value > value2._value;
 
@@ -261,8 +234,10 @@ namespace NpgsqlTypes
         /// </summary>
         /// <param name="value1">The first value to compare.</param>
         /// <param name="value2">The second value to compare.</param>
-        /// <returns><c>true</c> if <c>value1</c> is less than <c>value2</c>; otherwise, <c>false</c>.</returns>
-        [PublicAPI]
+        /// <returns>
+        /// <see langword="true" /> if <paramref name="value1" /> is less than <paramref name="value2" />; otherwise,
+        /// <see langword="false" />.
+        /// </returns>
         public static bool operator <(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
             => value1._value < value2._value;
 
@@ -273,9 +248,9 @@ namespace NpgsqlTypes
         /// <param name="value1">The first value to compare.</param>
         /// <param name="value2">The second value to compare.</param>
         /// <returns>
-        /// <c>true</c> if <c>value1</c> is greater than or equal to <c>value2</c>; otherwise, <c>false</c>.
+        /// <see langword="true" /> if <paramref name="value1" /> is greater than or equal to <paramref name="value2" />;
+        /// otherwise, <see langword="false" />.
         /// </returns>
-        [PublicAPI]
         public static bool operator >=(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
             => value1._value >= value2._value;
 
@@ -287,9 +262,8 @@ namespace NpgsqlTypes
         /// <returns>
         /// The larger of the two <see cref="NpgsqlLogSequenceNumber"/> values.
         /// </returns>
-        [PublicAPI]
         public static NpgsqlLogSequenceNumber Larger(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
-            => (value1._value > value2._value) ? value1 : value2;
+            => value1._value > value2._value ? value1 : value2;
 
         /// <summary>
         /// Returns the smaller of two <see cref="NpgsqlLogSequenceNumber"/> values.
@@ -299,9 +273,8 @@ namespace NpgsqlTypes
         /// <returns>
         /// The smaller of the two <see cref="NpgsqlLogSequenceNumber"/> values.
         /// </returns>
-        [PublicAPI]
         public static NpgsqlLogSequenceNumber Smaller(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
-            => (value1._value < value2._value) ? value1 : value2;
+            => value1._value < value2._value ? value1 : value2;
 
         /// <summary>
         /// Returns a value indicating whether a specified <see cref="NpgsqlLogSequenceNumber"/> instance is less than or
@@ -310,9 +283,9 @@ namespace NpgsqlTypes
         /// <param name="value1">The first value to compare.</param>
         /// <param name="value2">The second value to compare.</param>
         /// <returns>
-        /// <c>true</c> if <c>value1</c> is less than or equal to <c>value2</c>; otherwise, <c>false</c>.
+        /// <see langword="true" /> if <paramref name="value1" /> is less than or equal to <paramref name="value2" />;
+        /// otherwise, <see langword="false" />.
         /// </returns>
-        [PublicAPI]
         public static bool operator <=(NpgsqlLogSequenceNumber value1, NpgsqlLogSequenceNumber value2)
             => value1._value <= value2._value;
 
@@ -322,7 +295,6 @@ namespace NpgsqlTypes
         /// <param name="first">The first <see cref="NpgsqlLogSequenceNumber"/> value.</param>
         /// <param name="second">The second <see cref="NpgsqlLogSequenceNumber"/> value.</param>
         /// <returns>The number of bytes separating those write-ahead log locations.</returns>
-        [PublicAPI]
         public static ulong operator -(NpgsqlLogSequenceNumber first, NpgsqlLogSequenceNumber second)
             => first._value < second._value
                 ? second._value - first._value
@@ -342,7 +314,6 @@ namespace NpgsqlTypes
         /// The resulting <see cref="NpgsqlLogSequenceNumber"/> instance would represent a number less than
         /// <see cref="ulong.MinValue"/>.
         /// </exception>
-        [PublicAPI]
         public static NpgsqlLogSequenceNumber operator -(NpgsqlLogSequenceNumber lsn, double nbytes)
             => double.IsNaN(nbytes) || double.IsInfinity(nbytes)
                 ? throw new NotFiniteNumberException($"Cannot subtract {nbytes} from {nameof(NpgsqlLogSequenceNumber)}", nbytes)
@@ -362,7 +333,6 @@ namespace NpgsqlTypes
         /// The resulting <see cref="NpgsqlLogSequenceNumber"/> instance would represent a number greater than
         /// <see cref="ulong.MaxValue"/>.
         /// </exception>
-        [PublicAPI]
         public static NpgsqlLogSequenceNumber operator +(NpgsqlLogSequenceNumber lsn, double nbytes)
             => double.IsNaN(nbytes) || double.IsInfinity(nbytes)
                 ? throw new NotFiniteNumberException($"Cannot add {nbytes} to {nameof(NpgsqlLogSequenceNumber)}", nbytes)
