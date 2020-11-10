8eacde8b8d99f1be24047f1e3b7c29e5e8048dbe
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-11-10 15:29:54 +0300

    Added more mapping tests

--- test/Npgsql.Tests/Types/LTreeTests.cs
+++ test/Npgsql.Tests/Types/LTreeTests.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
+using NpgsqlTypes;
+using NUnit.Framework;
 
 namespace Npgsql.Tests.Types
 {
+    [TestFixture(MultiplexingMode.NonMultiplexing, false)]
+    [TestFixture(MultiplexingMode.NonMultiplexing, true)]
+    [TestFixture(MultiplexingMode.Multiplexing, false)]
+    [TestFixture(MultiplexingMode.Multiplexing, true)]
     public class LTreeTests : TypeHandlerTestBase<string>
     {
-        public LTreeTests(MultiplexingMode multiplexingMode)
-            : base(multiplexingMode, "ltree", "13.0") { }
+        public LTreeTests(MultiplexingMode multiplexingMode, bool useTypeName) : base(
+            multiplexingMode,
+            useTypeName ? null : NpgsqlDbType.LTree,
+            useTypeName ? "ltree" : null,
+            minVersion: "13.0")
+        { }
 
         public static IEnumerable TestCases() => new[]
         {
--- test/Npgsql.Tests/Types/LTxtQueryTests.cs
+++ test/Npgsql.Tests/Types/LTxtQueryTests.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
+using NpgsqlTypes;
+using NUnit.Framework;
 
 namespace Npgsql.Tests.Types
 {
+    [TestFixture(MultiplexingMode.NonMultiplexing, false)]
+    [TestFixture(MultiplexingMode.NonMultiplexing, true)]
+    [TestFixture(MultiplexingMode.Multiplexing, false)]
+    [TestFixture(MultiplexingMode.Multiplexing, true)]
     public class LTxtQueryTests : TypeHandlerTestBase<string>
     {
-        public LTxtQueryTests(MultiplexingMode multiplexingMode)
-            : base(multiplexingMode, "ltxtquery", "13.0") { }
+        public LTxtQueryTests(MultiplexingMode multiplexingMode, bool useTypeName) : base(
+            multiplexingMode,
+            useTypeName ? null : NpgsqlDbType.LTxtQuery,
+            useTypeName ? "ltxtquery" : null,
+            minVersion: "13.0")
+        { }
 
         public static IEnumerable TestCases() => new[]
         {
