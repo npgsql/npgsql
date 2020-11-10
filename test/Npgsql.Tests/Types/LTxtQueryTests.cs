ed2f47d08d5cbc0165474bd627fecac239f176c8
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-11-10 16:06:34 +0300

    Add binary protocol support for label tree (ltree)
    Fixes #699
    
    Co-authored-by: Nino Floris <mail@ninofloris.com>

---
+++ test/Npgsql.Tests/Types/LTxtQueryTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using NpgsqlTypes;
+using NUnit.Framework;
+
+namespace Npgsql.Tests.Types
+{
+    [TestFixture(MultiplexingMode.NonMultiplexing, false)]
+    [TestFixture(MultiplexingMode.NonMultiplexing, true)]
+    [TestFixture(MultiplexingMode.Multiplexing, false)]
+    [TestFixture(MultiplexingMode.Multiplexing, true)]
+    public class LTxtQueryTests : TypeHandlerTestBase<string>
+    {
+        public LTxtQueryTests(MultiplexingMode multiplexingMode, bool useTypeName) : base(
+            multiplexingMode,
+            useTypeName ? null : NpgsqlDbType.LTxtQuery,
+            useTypeName ? "ltxtquery" : null,
+            minVersion: "13.0")
+        { }
+
+        public static IEnumerable TestCases() => new[]
+        {
+            new object[] { "'Science & Astronomy'::ltxtquery", "Science & Astronomy" }
+        };
+    }
+}
