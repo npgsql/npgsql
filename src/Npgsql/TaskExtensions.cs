68abd5e2c0ef9df99320248a841def4d492aa639
Shay Rojansky <roji@roji.org>
2020-11-11 12:38:04 +0200

    Revert "Remove a bit of dead code"
    This reverts commit bb206843cf49961bc26107469589702f45b7a922.

--- src/Npgsql/TaskExtensions.cs
+++ src/Npgsql/TaskExtensions.cs
@@ -27,6 +27,28 @@ namespace Npgsql
         }
 
         /// <summary>
+        /// Utility that simplifies awaiting a task with a timeout. If the given task does not
+        /// complete within <paramref name="timeout"/>, a <see cref="TimeoutException"/> is thrown.
+        /// </summary>
+        /// <param name="task">The task to be awaited</param>
+        /// <param name="timeout">How much time to allow <paramref name="task"/> to complete before throwing a <see cref="TimeoutException"/></param>
+        /// <returns>An awaitable task that represents the original task plus the timeout</returns>
+        internal static async Task WithTimeout(this Task task, NpgsqlTimeout timeout)
+        {
+            if (!timeout.IsSet)
+            {
+                await task;
+                return;
+            }
+            var timeLeft = timeout.TimeLeft;
+            if (timeLeft <= TimeSpan.Zero)
+                throw new TimeoutException();
+            if (task != await Task.WhenAny(task, Task.Delay(timeLeft)))
+                throw new TimeoutException();
+            await task;
+        }
+
+        /// <summary>
         /// Allows you to cancel awaiting for a non-cancellable task.
         /// </summary>
         /// <remarks>
@@ -42,7 +64,26 @@ namespace Npgsql
             return await task;
         }
 
+        /// <summary>
+        /// Allows you to cancel awaiting for a non-cancellable task.
+        /// </summary>
+        /// <remarks>
+        /// Read https://blogs.msdn.com/b/pfxteam/archive/2012/10/05/how-do-i-cancel-non-cancelable-async-operations.aspx
+        /// and be very careful with this.
+        /// </remarks>
+        internal static async Task WithCancellation(this Task task, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
+                if (task != await Task.WhenAny(task, tcs.Task))
+                    throw new TaskCanceledException(task);
+            await task;
+        }
+
         internal static Task<T> WithCancellationAndTimeout<T>(this Task<T> task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
+            => task.WithCancellation(cancellationToken).WithTimeout(timeout);
+
+        internal static Task WithCancellationAndTimeout(this Task task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
             => task.WithCancellation(cancellationToken).WithTimeout(timeout);
     }
 }
