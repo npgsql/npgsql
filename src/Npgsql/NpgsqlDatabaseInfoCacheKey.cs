58ee69f19fbf7ea4fcf82c98bc551a39e1fcbccd
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-11-15 20:03:22 +0300

    Made database info cache key struct

---
+++ src/Npgsql/NpgsqlDatabaseInfoCacheKey.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Npgsql
+{
+    readonly struct NpgsqlDatabaseInfoCacheKey : IEquatable<NpgsqlDatabaseInfoCacheKey>
+    {
+        public readonly int Port;
+        public readonly string? Host;
+        public readonly string? Database;
+        public readonly ServerCompatibilityMode CompatibilityMode;
+
+        public NpgsqlDatabaseInfoCacheKey(NpgsqlConnectionStringBuilder connectionString)
+        {
+            Port = connectionString.Port;
+            Host = connectionString.Host;
+            Database = connectionString.Database;
+            CompatibilityMode = connectionString.ServerCompatibilityMode;
+        }
+
+        public bool Equals(NpgsqlDatabaseInfoCacheKey other) =>
+            Port == other.Port &&
+            Host == other.Host &&
+            Database == other.Database &&
+            CompatibilityMode == other.CompatibilityMode;
+
+        public override bool Equals(object? obj) =>
+            obj is NpgsqlDatabaseInfoCacheKey key && key.Equals(this);
+
+        public override int GetHashCode() =>
+            Port.GetHashCode() ^
+            Host?.GetHashCode() ?? 0 ^
+            Database?.GetHashCode() ?? 0 ^
+            CompatibilityMode.GetHashCode();
+    }
+}
