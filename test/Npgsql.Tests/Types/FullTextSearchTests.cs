2c3870ef7acb293154f9df43099b7820879bc664
Yoh Deadfall <yoh.deadfall@hotmail.com>
2020-11-18 15:57:06 +0300

    Unified tsquery tests

--- test/Npgsql.Tests/Types/FullTextSearchTests.cs
+++ test/Npgsql.Tests/Types/FullTextSearchTests.cs
@@ -24,31 +24,5 @@ namespace Npgsql.Tests.Types
                 Assert.AreEqual(inputVec.ToString(), outputVec!.ToString());
             }
         }
-
-        [Test]
-        public async Task TsQuery()
-        {
-            using var conn = await OpenConnectionAsync();
-            var queryString = conn.PostgreSqlVersion < new Version(9, 6)
-                ? "'a' & !( 'c' | 'd' ) & !!'a' & 'b' | 'ä' | 'f'"
-                : "'a' & !( 'c' | 'd' ) & !!'a' & 'b' | 'ä' | 'x' <-> 'y' | 'x' <10> 'y' | 'd' <0> 'e' | 'f'";
-            var query = NpgsqlTsQuery.Parse(queryString);
-
-            using var cmd = new NpgsqlCommand("SELECT @s::tsquery, @q, @q::text", conn)
-            {
-                Parameters =
-                {
-                    new NpgsqlParameter("s", queryString),
-                    new NpgsqlParameter("q", query),
-                }
-            };
-
-            using var reader = await cmd.ExecuteReaderAsync();
-            await reader.ReadAsync();
-
-            Assert.AreEqual(query.ToString(), reader.GetFieldValue<NpgsqlTsQuery>(0).ToString());
-            Assert.AreEqual(query.ToString(), reader.GetFieldValue<NpgsqlTsQuery>(1).ToString());
-            Assert.AreEqual(queryString, reader.GetFieldValue<string>(2));
-        }
     }
 }
