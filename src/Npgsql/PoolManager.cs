67f67f5a6630d94d74d518a518fbe893bf955c62
Nikita Kazmin <vonzshik@gmail.com>
2020-12-11 14:11:09 +0300

    Fixed a race condition

--- src/Npgsql/PoolManager.cs
+++ src/Npgsql/PoolManager.cs
@@ -27,10 +27,15 @@ namespace Npgsql
             // Note that pools never get removed. _pools is strictly append-only.
             var nextSlot = _nextSlot;
             var pools = _pools;
+            // There is a race condition between TryGetValue and ClearAll.
+            // When the nextSlot has already been read as a non-zero value, but the pools have already been reset.
+            // And in this case, we will iterate beyond the size of the array.
+            // As a safeguard, we take a min length between the array and the nextSlot.
+            var minNextSlot = Math.Min(nextSlot, pools.Length);
             var sw = new SpinWait();
 
             // First scan the pools and do reference equality on the connection strings
-            for (var i = 0; i < nextSlot; i++)
+            for (var i = 0; i < minNextSlot; i++)
             {
                 var cp = pools[i];
                 if (ReferenceEquals(cp.Key, key))
@@ -46,7 +51,7 @@ namespace Npgsql
             }
 
             // Next try value comparison on the strings
-            for (var i = 0; i < nextSlot; i++)
+            for (var i = 0; i < minNextSlot; i++)
             {
                 var cp = pools[i];
                 if (cp.Key == key)
@@ -99,11 +104,15 @@ namespace Npgsql
                 for (var i = 0; i < _nextSlot; i++)
                 {
                     var cp = pools[i];
+                    // We shouldn't ever get here, as _nextSlot is only incremented under a lock
+                    // and there shouldn't be null values in-between. But just in case...
                     if (cp.Key == null)
-                        return;
+                        break;
                     cp.Pool?.Clear();
                 }
 
+                // _nextSlot should be changed before the _pools.
+                // Otherwise, there will be a race condition with TryGetValue.
                 _nextSlot = 0;
                 _pools = new (string, ConnectorPool)[InitialPoolsSize];
             }
