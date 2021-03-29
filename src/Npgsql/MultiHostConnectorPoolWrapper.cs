ca52b7804514fbafbc066e6439389aa0b1a1556f
Nikita Kazmin <vonzshik@gmail.com>
2021-03-29 13:50:39 +0300

    Yet another few missing sealed

--- src/Npgsql/MultiHostConnectorPoolWrapper.cs
+++ src/Npgsql/MultiHostConnectorPoolWrapper.cs
@@ -13,19 +13,19 @@ namespace Npgsql
         public MultiHostConnectorPoolWrapper(NpgsqlConnectionStringBuilder settings, string connString, MultiHostConnectorPool source) : base(settings, connString)
             => _wrappedSource = source;
 
-        internal override (int Total, int Idle, int Busy) Statistics => _wrappedSource.Statistics;
+        internal sealed override (int Total, int Idle, int Busy) Statistics => _wrappedSource.Statistics;
 
-        internal override void Clear() => _wrappedSource.Clear();
-        internal override ValueTask<NpgsqlConnector> Get(NpgsqlConnection conn, NpgsqlTimeout timeout, bool async, CancellationToken cancellationToken)
+        internal sealed override void Clear() => _wrappedSource.Clear();
+        internal sealed override ValueTask<NpgsqlConnector> Get(NpgsqlConnection conn, NpgsqlTimeout timeout, bool async, CancellationToken cancellationToken)
             => _wrappedSource.Get(conn, timeout, async, cancellationToken);
-        internal override void Return(NpgsqlConnector connector)
+        internal sealed override void Return(NpgsqlConnector connector)
             => _wrappedSource.Return(connector);
 
-        internal override void AddPendingEnlistedConnector(NpgsqlConnector connector, Transaction transaction)
+        internal sealed override void AddPendingEnlistedConnector(NpgsqlConnector connector, Transaction transaction)
             => _wrappedSource.AddPendingEnlistedConnector(connector, transaction);
-        internal override void TryRemovePendingEnlistedConnector(NpgsqlConnector connector, Transaction transaction)
+        internal sealed override void TryRemovePendingEnlistedConnector(NpgsqlConnector connector, Transaction transaction)
             => _wrappedSource.TryRemovePendingEnlistedConnector(connector, transaction);
-        internal override bool TryRentEnlistedPending(Transaction transaction, [NotNullWhen(true)] out NpgsqlConnector? connector)
+        internal sealed override bool TryRentEnlistedPending(Transaction transaction, [NotNullWhen(true)] out NpgsqlConnector? connector)
             => _wrappedSource.TryRentEnlistedPending(transaction, out connector);
     }
 }
