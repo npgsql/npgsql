c7831f17e9136a49c29a4be8fb9b715f799c0517
Shay Rojansky <roji@roji.org>
2021-05-06 12:12:18 +0200

    Use Scriban in source generator (#3707)

--- src/Npgsql.SourceGenerators/TypeHandlerSourceGenerator.cs
+++ src/Npgsql.SourceGenerators/TypeHandlerSourceGenerator.cs
@@ -6,6 +6,8 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
+using Scriban;
+using Scriban.Runtime;
 
 namespace Npgsql.SourceGenerators
 {
@@ -48,8 +50,6 @@ namespace Npgsql.SourceGenerators
 
             void AugmentTypeHandler(INamedTypeSymbol typeSymbol, ClassDeclarationSyntax classDeclarationSyntax, bool isSimple)
             {
-                var typeName = FormatTypeName(typeSymbol);
-
                 var usings = new HashSet<string>(
                     new[]
                     {
@@ -61,67 +61,27 @@ namespace Npgsql.SourceGenerators
                         .Where(u => u.Alias is null)
                         .Select(u => u.Name.ToString())));
 
-                var ns = typeSymbol.ContainingNamespace.ToDisplayString();
-
                 var interfaces = typeSymbol.AllInterfaces
                     .Where(i => i.OriginalDefinition.Equals(isSimple ? simpleTypeHandlerInterfaceSymbol : typeHandlerInterfaceSymbol,
                                     SymbolEqualityComparer.Default) &&
                                 !i.TypeArguments[0].IsAbstract);
 
-                var validateAccess = isSimple ? "protected" : "public";
-                var validationDispatchLines = interfaces.Select(i =>
-                    $"{FormatTypeName(i.TypeArguments[0])} converted => (({FormatTypeName(i)})this).ValidateAndGetLength(converted, {(isSimple ? "" : "ref lengthCache, ")}parameter),");
+                var template = Template.Parse(EmbeddedResource.GetContent("TypeHandler.snbtxt"), "TypeHandler.snbtxt");
+                var output = template.Render(new
+                {
+                    Usings = usings,
+                    TypeName = FormatTypeName(typeSymbol),
+                    Namespace = typeSymbol.ContainingNamespace.ToDisplayString(),
+                    IsSimple = isSimple,
+                    ValidateAccess = isSimple ? "protected" : "public",
+                    Interfaces = interfaces.Select(i => new
+                    {
+                        Name = FormatTypeName(i),
+                        HandledType = FormatTypeName(i.TypeArguments[0]),
+                    })
+                });
 
-                var writeDispatchLines = interfaces.Select(i =>
-                    $"{FormatTypeName(i.TypeArguments[0])} converted => WriteWithLengthInternal(converted, buf, lengthCache, parameter, async, cancellationToken),");
-
-                var sourceBuilder = new StringBuilder();
-
-                foreach (var usingNamespace in usings.OrderBy(n => n))
-                    sourceBuilder.Append("using ").Append(usingNamespace).AppendLine(";");
-
-                sourceBuilder.Append($@"
-
-#nullable enable
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-#pragma warning disable RS0016 // Add public types and members to the declared API
-#pragma warning disable 618 // Member is obsolete
-
-namespace {ns}
-{{
-    partial class {typeName}
-    {{
-        {validateAccess} override int ValidateObjectAndGetLength(object value, {(isSimple ? "" : "ref NpgsqlLengthCache? lengthCache, ")}NpgsqlParameter? parameter)
-            => value switch
-            {{");
-                foreach (var line in validationDispatchLines)
-                    sourceBuilder.Append(@$"
-                {line}");
-
-                sourceBuilder.Append($@"
-
-                DBNull => -1,
-                null => -1,
-                _ => throw new InvalidCastException($""Can't write CLR type {{value.GetType()}} with handler type {typeName}"")
-            }};
-
-        public override Task WriteObjectWithLength(object value, NpgsqlWriteBuffer buf, NpgsqlLengthCache? lengthCache, NpgsqlParameter? parameter, bool async, CancellationToken cancellationToken = default)
-            => value switch
-            {{");
-                foreach (var line in writeDispatchLines)
-                    sourceBuilder.Append(@$"
-                {line}");
-
-                sourceBuilder.Append($@"
-
-                DBNull => WriteWithLengthInternal(DBNull.Value, buf, lengthCache, parameter, async, cancellationToken),
-                null => WriteWithLengthInternal(DBNull.Value, buf, lengthCache, parameter, async, cancellationToken),
-                _ => throw new InvalidCastException($""Can't write CLR type {{value.GetType()}} with handler type {typeName}"")
-            }};
-    }}
-}}");
-
-                context.AddSource(typeSymbol.Name + ".Generated.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+                context.AddSource(typeSymbol.Name + ".Generated.cs", SourceText.From(output, Encoding.UTF8));
             }
 
             static string FormatTypeName(ITypeSymbol typeSymbol)
