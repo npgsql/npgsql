64771798901da3f1d099046aef22aa053a74b6de
Shay Rojansky <roji@roji.org>
2021-07-17 13:47:14 +0200

    Ignore test on PG < 13

--- test/Npgsql.Tests/Types/NumericTypeTests.cs
+++ test/Npgsql.Tests/Types/NumericTypeTests.cs
@@ -5,6 +5,7 @@ using System.Globalization;
 using System.Threading.Tasks;
 using NpgsqlTypes;
 using NUnit.Framework;
+using static Npgsql.Tests.TestUtil;
 
 namespace Npgsql.Tests.Types
 {
@@ -107,11 +108,13 @@ namespace Npgsql.Tests.Types
         [TestCase(NpgsqlDbType.Xid8, TestName="XID8")]
         public async Task UInt64(NpgsqlDbType npgsqlDbType)
         {
+            await using var conn = await OpenConnectionAsync();
+            MinimumPgVersion(conn, "13.0", "The xid8 type was introduced in PostgreSQL 13");
+
             var expected = 8ul;
-            using var conn = await OpenConnectionAsync();
-            using var cmd = new NpgsqlCommand("SELECT @p", conn);
+            await using var cmd = new NpgsqlCommand("SELECT @p", conn);
             cmd.Parameters.Add(new NpgsqlParameter("p", npgsqlDbType) { Value = expected });
-            using var reader = await cmd.ExecuteReaderAsync();
+            await using var reader = await cmd.ExecuteReaderAsync();
             reader.Read();
             Assert.That(reader[0], Is.EqualTo(expected));
             Assert.That(reader.GetProviderSpecificValue(0), Is.EqualTo(expected));
