876c80025abce312c6342e93553ab932e11ae09a
Shay Rojansky <roji@roji.org>
2021-08-22 21:03:07 +0200

    Fixup for multirange support (#3941)

--- test/Npgsql.Tests/Types/MultirangeTests.cs
+++ test/Npgsql.Tests/Types/MultirangeTests.cs
@@ -32,26 +32,34 @@ namespace Npgsql.Tests.Types
         [Test]
         public async Task Write()
         {
-            var multirange = new NpgsqlRange<int>[]
+            var multirangeArray = new NpgsqlRange<int>[]
             {
                 new(3, true, false, 7, false, false),
                 new(8, false, false, 0, false, true)
             };
 
+            var multirangeList = new List<NpgsqlRange<int>>(multirangeArray);
+
             await using var conn = await OpenConnectionAsync();
             await using var cmd = new NpgsqlCommand("SELECT $1::text", conn);
 
-            conn.ReloadTypes();
-            cmd.Parameters.Add(new() { Value = multirange });
-            Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
+            await WriteInternal(multirangeArray);
+            await WriteInternal(multirangeList);
 
-            conn.ReloadTypes();
-            cmd.Parameters[0] = new() { Value = multirange, NpgsqlDbType = NpgsqlDbType.Multirange | NpgsqlDbType.Integer };
-            Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
+            async Task WriteInternal(IList<NpgsqlRange<int>> multirange)
+            {
+                conn.ReloadTypes();
+                cmd.Parameters.Add(new() { Value = multirange });
+                Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
 
-            conn.ReloadTypes();
-            cmd.Parameters[0] = new() { Value = multirange, DataTypeName = "int4multirange" };
-            Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
+                conn.ReloadTypes();
+                cmd.Parameters[0] = new() { Value = multirange, NpgsqlDbType = NpgsqlDbType.Multirange | NpgsqlDbType.Integer };
+                Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
+
+                conn.ReloadTypes();
+                cmd.Parameters[0] = new() { Value = multirange, DataTypeName = "int4multirange" };
+                Assert.That(await cmd.ExecuteScalarAsync(), Is.EqualTo("{[3,7),[9,)}"));
+            }
         }
 
         [OneTimeSetUp]
