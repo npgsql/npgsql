05fb4e94b43b5dcde79131374121b5e49e769a1b
Nikita Kazmin <vonzshik@gmail.com>
2021-09-09 17:32:47 +0300

    Indentationnit

--- src/Npgsql/NpgsqlActivitySource.cs
+++ src/Npgsql/NpgsqlActivitySource.cs
@@ -16,30 +16,30 @@ namespace Npgsql
         {
             var settings = connector.Settings;
             var activity = Source.StartActivity(settings.Database!, ActivityKind.Client);
-            if (activity is not null)
-            {
-                activity.SetTag("db.system", "postgresql");
-                activity.SetTag("db.connection_string", connector.UserFacingConnectionString);
-                activity.SetTag("db.user", settings.Username);
-                activity.SetTag("db.name", settings.Database);
-                activity.SetTag("db.statement", sql);
-                activity.SetTag("db.connection.id", connector.Id);
+            if (activity is null)
+                return null;
 
-                var endPoint = connector.ConnectedEndPoint;
-                Debug.Assert(endPoint is not null);
-                if (endPoint is IPEndPoint ipEndPoint)
-                {
-                    activity.SetTag("net.transport", "ip_tcp");
-                    activity.SetTag("net.peer.ip", ipEndPoint.Address.ToString());
-                    if (ipEndPoint.Port != 5432)
-                        activity.SetTag("net.peer.port", ipEndPoint.Port);
-                    activity.SetTag("net.peer.name", settings.Host);
-                }
-                else
-                {
-                    activity.SetTag("net.transport", "unix");
-                    activity.SetTag("net.peer.name", settings.Host);
-                }
+            activity.SetTag("db.system", "postgresql");
+            activity.SetTag("db.connection_string", connector.UserFacingConnectionString);
+            activity.SetTag("db.user", settings.Username);
+            activity.SetTag("db.name", settings.Database);
+            activity.SetTag("db.statement", sql);
+            activity.SetTag("db.connection.id", connector.Id);
+
+            var endPoint = connector.ConnectedEndPoint;
+            Debug.Assert(endPoint is not null);
+            if (endPoint is IPEndPoint ipEndPoint)
+            {
+                activity.SetTag("net.transport", "ip_tcp");
+                activity.SetTag("net.peer.ip", ipEndPoint.Address.ToString());
+                if (ipEndPoint.Port != 5432)
+                    activity.SetTag("net.peer.port", ipEndPoint.Port);
+                activity.SetTag("net.peer.name", settings.Host);
+            }
+            else
+            {
+                activity.SetTag("net.transport", "unix");
+                activity.SetTag("net.peer.name", settings.Host);
             }
 
             return activity;
@@ -47,18 +47,18 @@ namespace Npgsql
 
         public static void SetException(IDisposable? currentActivity, Exception ex, bool escaped = true)
         {
-            if (currentActivity is Activity activity)
+            if (currentActivity is not Activity activity)
+                return;
+
+            var tags = new ActivityTagsCollection
             {
-                var tags = new ActivityTagsCollection
-                {
-                    { "exception.type", ex.GetType().FullName },
-                    { "exception.message", ex.Message },
-                    { "exception.stacktrace", ex.ToString() },
-                    { "exception.escaped", escaped }
-                };
-                var activityEvent = new ActivityEvent("exception", tags: tags);
-                activity.AddEvent(activityEvent);
-            }
+                { "exception.type", ex.GetType().FullName },
+                { "exception.message", ex.Message },
+                { "exception.stacktrace", ex.ToString() },
+                { "exception.escaped", escaped }
+            };
+            var activityEvent = new ActivityEvent("exception", tags: tags);
+            activity.AddEvent(activityEvent);
         }
 #else
         public static bool IsEnabled => false;
