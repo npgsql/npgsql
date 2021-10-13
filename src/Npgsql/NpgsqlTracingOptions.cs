36cd27cd489eb5a9f5f3965bcb4ed05433d5304c
Shay Rojansky <roji@roji.org>
2021-10-14 00:15:30 +0200

    Add AddNpgsql to setup OpenTelemetry tracing (#4040)
    Part of #4037

---
+++ src/Npgsql/NpgsqlTracingOptions.cs
@@ -0,0 +1,10 @@
+namespace Npgsql
+{
+    /// <summary>
+    /// Options to configure Npgsql's support for OpenTelemetry tracing.
+    /// Currently no options are available.
+    /// </summary>
+    public class NpgsqlTracingOptions
+    {
+    }
+}
