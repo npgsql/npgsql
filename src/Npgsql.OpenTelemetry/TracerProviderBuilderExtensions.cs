44bf76199d71b7c2ed3fb6a6123473796652da94
Shay Rojansky <roji@roji.org>
2021-10-14 00:02:38 +0200

    Add AddNpgsql to setup OpenTelemetry tracing
    Part of #4037

---
+++ src/Npgsql.OpenTelemetry/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenTelemetry.Trace;
+
+// ReSharper disable once CheckNamespace
+namespace Npgsql
+{
+    /// <summary>
+    /// Extension method for setting up Npgsql OpenTelemetry tracing.
+    /// </summary>
+    public static class TracerProviderBuilderExtensions
+    {
+        /// <summary>
+        /// Subscribes to the Npgsql activity source to enable OpenTelemetry tracing.
+        /// </summary>
+        public static TracerProviderBuilder AddNpgsql(
+            this TracerProviderBuilder builder,
+            Action<NpgsqlTracingOptions>? options = null)
+            => builder.AddSource("Npgsql");
+    }
+}
