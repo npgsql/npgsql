7503fe4bebac5fac56d4754c38ad39f65012f77a
Nikita Kazmin <vonzshik@gmail.com>
2021-11-08 23:05:32 +0300

    Move TypeMappingInfo to Npgsql.Internal.TypeHandling namespace

--- src/Npgsql/Internal/TypeHandling/TypeHandlerResolverFactory.cs
+++ src/Npgsql/Internal/TypeHandling/TypeHandlerResolverFactory.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Data;
-using Npgsql.TypeMapping;
-using NpgsqlTypes;
 
 namespace Npgsql.Internal.TypeHandling
 {
@@ -22,22 +19,4 @@ namespace Npgsql.Internal.TypeHandling
         internal static TypeMappingInfo? GetMappingByValueDependentValue(this TypeHandlerResolverFactory factory, object value)
             => factory.GetDataTypeNameByValueDependentValue(value) is { } dataTypeName ? factory.GetMappingByDataTypeName(dataTypeName) : null;
     }
-}
-
-public class TypeMappingInfo
-{
-    public TypeMappingInfo(NpgsqlDbType? npgsqlDbType, string? dataTypeName, Type clrType)
-        => (NpgsqlDbType, DataTypeName, ClrTypes) = (npgsqlDbType, dataTypeName, new[] { clrType });
-
-    public TypeMappingInfo(NpgsqlDbType? npgsqlDbType, string? dataTypeName, params Type[] clrTypes)
-        => (NpgsqlDbType, DataTypeName, ClrTypes) = (npgsqlDbType, dataTypeName, clrTypes);
-
-    public NpgsqlDbType? NpgsqlDbType { get; }
-    DbType? dbType;
-    public DbType DbType
-        => dbType ??= NpgsqlDbType is null ? DbType.Object : GlobalTypeMapper.NpgsqlDbTypeToDbType(NpgsqlDbType.Value);
-    public string? DataTypeName { get; }
-    public Type[] ClrTypes { get; }
-
-    internal void Reset() => dbType = null;
 }
---
+++ src/Npgsql/Internal/TypeHandling/TypeMappingInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+using Npgsql.TypeMapping;
+using NpgsqlTypes;
+
+namespace Npgsql.Internal.TypeHandling
+{
+    public class TypeMappingInfo
+    {
+        public TypeMappingInfo(NpgsqlDbType? npgsqlDbType, string? dataTypeName, Type clrType)
+            => (NpgsqlDbType, DataTypeName, ClrTypes) = (npgsqlDbType, dataTypeName, new[] { clrType });
+
+        public TypeMappingInfo(NpgsqlDbType? npgsqlDbType, string? dataTypeName, params Type[] clrTypes)
+            => (NpgsqlDbType, DataTypeName, ClrTypes) = (npgsqlDbType, dataTypeName, clrTypes);
+
+        public NpgsqlDbType? NpgsqlDbType { get; }
+        DbType? dbType;
+        public DbType DbType
+            => dbType ??= NpgsqlDbType is null ? DbType.Object : GlobalTypeMapper.NpgsqlDbTypeToDbType(NpgsqlDbType.Value);
+        public string? DataTypeName { get; }
+        public Type[] ClrTypes { get; }
+
+        internal void Reset() => dbType = null;
+    }
+}
