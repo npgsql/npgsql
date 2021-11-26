fcc689c82cdb3a3a9d15dc3e8085f79133843abd
Serge Pavlov <sergepavlov@geotab.com>
2021-11-26 04:43:16 -0500

    Observe and ignore failed task exception when the task is preempted.

--- src/Npgsql/TaskExtensions.cs
+++ src/Npgsql/TaskExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Npgsql.Util;
@@ -16,13 +17,13 @@ static class TaskExtensions
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
     /// <typeparam name="TResult">The result <see cref="Type"/>.</typeparam>
     /// <returns>The <see cref="Task{TResult}"/> representing the asynchronous wait.</returns>
-    internal static Task<TResult> ExecuteWithCancellationAndTimeoutAsync<TResult>(Func<CancellationToken, Task<TResult>> getTaskFunc,
+    internal static async Task<TResult> ExecuteWithCancellationAndTimeoutAsync<TResult>(Func<CancellationToken, Task<TResult>> getTaskFunc,
         NpgsqlTimeout timeout, CancellationToken cancellationToken)
-        => ExecuteWithTimeout(
-                getTaskFunc, timeout, cancellationToken)
-            // As the cancellation support of getResultAsync is not guaranteed on all platforms/frameworks
-            // we apply the fake-timeout/cancel mechanism in all cases.
-            .WithCancellationAndTimeout(timeout, cancellationToken);
+    {
+        Task<TResult>? task = default;
+        await ExecuteWithCancellationAndTimeoutAsync(ct => (Task)(task = getTaskFunc(ct)), timeout, cancellationToken);
+        return await task!;
+    }
 
     /// <summary>
     /// Utility that executes a non-cancellable task and allows you to timeout and cancel awaiting for it.
@@ -32,91 +33,86 @@ static class TaskExtensions
     /// <param name="timeout">The timeout after which the <see cref="Task"/> should be faulted with a <see cref="TimeoutException"/> if it hasn't otherwise completed.</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for a cancellation request.</param>
     /// <returns>The <see cref="Task"/> representing the asynchronous wait.</returns>
-    internal static Task ExecuteWithCancellationAndTimeoutAsync(Func<CancellationToken, Task> getTaskFunc, NpgsqlTimeout timeout,
+    internal static async Task ExecuteWithCancellationAndTimeoutAsync(Func<CancellationToken, Task> getTaskFunc, NpgsqlTimeout timeout,
         CancellationToken cancellationToken)
-        => ExecuteWithTimeout(
-                ct => getTaskFunc(ct).ContinueWith(_ => true,
-                    TaskContinuationOptions.ExecuteSynchronously | TaskContinuationOptions.OnlyOnRanToCompletion), timeout,
-                cancellationToken)
-            // As the cancellation support of getResultAsync is not guaranteed on all platforms/frameworks
-            // we apply the fake-timeout/cancel mechanism in all cases.
-            .WithCancellationAndTimeout(timeout, cancellationToken);
+    {
+        Task? task = default;
+        using var combinedCts = timeout.IsSet ? CancellationTokenSource.CreateLinkedTokenSource(cancellationToken) : null;
+        try
+        {
+            try
+            {
+                await (task = getTaskFunc(combinedCts?.Token ?? cancellationToken)).WithCancellationAndTimeout(timeout, cancellationToken);
+            }
+            catch (TimeoutException) when (!task!.IsCompleted)
+            {
+                // Attempt to stop the Task in progress.
+                combinedCts?.Cancel();
+                throw;
+            }
+        }
+        catch
+        {
+            // Prevent unobserved Task notifications by observing the failed Task exception.
+            _ = task!.ContinueWith(t => _ = t.Exception, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted,
+                TaskScheduler.Current);
+            throw;
+        }
+    }
 
 #if NET6_0_OR_GREATER
-        static Task<T> WithCancellationAndTimeout<T>(this Task<T> task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
+        static Task WithCancellationAndTimeout(this Task task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
             => task.WaitAsync(timeout.CheckAndGetTimeLeft(), cancellationToken);
 #else
-    /// <summary>
-    /// Utility that simplifies awaiting a task with a timeout. If the given task does not
-    /// complete within <paramref name="timeout"/>, a <see cref="TimeoutException"/> is thrown.
-    /// </summary>
-    /// <param name="task">The task to be awaited</param>
-    /// <param name="timeout">How much time to allow <paramref name="task"/> to complete before throwing a <see cref="TimeoutException"/></param>
-    /// <returns>An awaitable task that represents the original task plus the timeout</returns>
-    static async Task<T> WithTimeout<T>(this Task<T> task, NpgsqlTimeout timeout)
+    static async Task WithCancellationAndTimeout(this Task task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
     {
+        var tasks = new Lazy<List<Task>>(() => new List<Task>());
+
+        Task? cancellationTask = default;
+        CancellationTokenRegistration registration = default;
+        if (cancellationToken.CanBeCanceled)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            registration = cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs);
+            cancellationTask = tcs.Task;
+            tasks.Value.Add(cancellationTask);
+        }
+
+        Task? delayTask = default;
+        CancellationTokenSource? delayCts = default;
         if (timeout.IsSet)
         {
             var timeLeft = timeout.CheckAndGetTimeLeft();
-            if (task != await Task.WhenAny(task, Task.Delay(timeLeft)))
-                throw new TimeoutException();
+            delayCts = new CancellationTokenSource();
+            delayTask = Task.Delay(timeLeft, delayCts.Token);
+            tasks.Value.Add(delayTask);
         }
 
-        return await task;
-    }
-
-    /// <summary>
-    /// Allows you to cancel awaiting for a non-cancellable task.
-    /// </summary>
-    /// <remarks>
-    /// Read https://blogs.msdn.com/b/pfxteam/archive/2012/10/05/how-do-i-cancel-non-cancelable-async-operations.aspx
-    /// and be very careful with this.
-    /// </remarks>
-    static async Task<T> WithCancellation<T>(this Task<T> task, CancellationToken cancellationToken)
-    {
-        var tcs = new TaskCompletionSource<bool>();
-        using (cancellationToken.Register(s => ((TaskCompletionSource<bool>)s!).TrySetResult(true), tcs))
-            if (task != await Task.WhenAny(task, tcs.Task))
-                // This treatment is more consistent with .NET6 Task.WaitAsync(CancellationToken) than throwing new TaskCancelledException(task). 
-                task = Task.FromCanceled<T>(cancellationToken);
-        return await task;
-    }
-
-    static Task<T> WithCancellationAndTimeout<T>(this Task<T> task, NpgsqlTimeout timeout, CancellationToken cancellationToken)
-        => task.WithCancellation(cancellationToken).WithTimeout(timeout);
-#endif
-
-    static async Task<TResult> ExecuteWithTimeout<TResult>(Func<CancellationToken, Task<TResult>> func, NpgsqlTimeout timeout,
-        CancellationToken cancellationToken)
-    {
-        CancellationTokenSource? combinedCts = null;
         try
         {
-            var combinedCancellationToken = GetCombinedCancellationToken(ref combinedCts, timeout, cancellationToken);
-            return await func(combinedCancellationToken);
-        }
-        catch (OperationCanceledException) when (combinedCts?.IsCancellationRequested == true && !cancellationToken.IsCancellationRequested)
-        {
-            throw new TimeoutException();
+            if (tasks.IsValueCreated)
+            {
+                tasks.Value.Add(task);
+                var result = await Task.WhenAny(tasks.Value);
+                if (result == cancellationTask)
+                {
+                    task = Task.FromCanceled(cancellationToken);
+                }
+                else if (result == delayTask)
+                {
+                    task = Task.FromException(new TimeoutException());
+                }
+            }
+
+            await task;
         }
         finally
         {
-            combinedCts?.Dispose();
+            delayCts?.Cancel();
+            delayCts?.Dispose();
+            registration.Dispose();
         }
     }
-
-    static CancellationToken GetCombinedCancellationToken(ref CancellationTokenSource? combinedCts, NpgsqlTimeout timeout,
-        CancellationToken cancellationToken)
-    {
-        var finalCt = cancellationToken;
-
-        if (timeout.IsSet)
-        {
-            combinedCts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
-            combinedCts.CancelAfter((int)timeout.CheckAndGetTimeLeft().TotalMilliseconds);
-            finalCt = combinedCts.Token;
-        }
-
-        return finalCt;
-    }
+#endif
 }
+
--- test/Npgsql.Tests/TaskExtensionsTest.cs
+++ test/Npgsql.Tests/TaskExtensionsTest.cs
@@ -18,44 +18,134 @@ namespace Npgsql.Tests
 
         Task GetVoidTaskAsync(int timeout, CancellationToken ct) => Task.Delay(timeout, ct);
 
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
+        [Test]
         public async Task SuccessfulResultTaskAsync() =>
             Assert.AreEqual(Value, await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetResultTaskAsync(10, ct),
                 NpgsqlTimeout.Infinite, CancellationToken.None));
 
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
+        [Test]
         public async Task SuccessfulVoidTaskAsync() =>
             await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetVoidTaskAsync(10, ct),
                 NpgsqlTimeout.Infinite, CancellationToken.None);
 
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
-        public void InfinitelyLongResultTaskTimeout() =>
-            Assert.ThrowsAsync<TimeoutException>(async () =>
-                await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetResultTaskAsync(Timeout.Infinite, ct),
-                    new NpgsqlTimeout(TimeSpan.FromMilliseconds(10)), CancellationToken.None));
-
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
-        public void InfinitelyLongVoidTaskTimeout() =>
+        [Test]
+        public void InfinitelyLongTaskTimeout() =>
             Assert.ThrowsAsync<TimeoutException>(async () =>
                 await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetVoidTaskAsync(Timeout.Infinite, ct),
                     new NpgsqlTimeout(TimeSpan.FromMilliseconds(10)), CancellationToken.None));
 
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
-        public void InfinitelyLongResultTaskCancellation()
+        [Test]
+        public void InfinitelyLongTaskCancellation()
         {
-            using (var cts = new CancellationTokenSource(10))
-                Assert.ThrowsAsync<TaskCanceledException>(async () =>
-                    await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetResultTaskAsync(Timeout.Infinite, ct),
-                        NpgsqlTimeout.Infinite, cts.Token));
+            using var cts = new CancellationTokenSource(10);
+            Assert.ThrowsAsync<TaskCanceledException>(async () =>
+                await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetVoidTaskAsync(Timeout.Infinite, ct),
+                    NpgsqlTimeout.Infinite, cts.Token));
         }
 
-        [Test, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
-        public void InfinitelyLongVoidTaskCancellation()
+        /// <summary>
+        /// The test creates a delayed execution Task that is being fake-cancelled and fails subsequently and triggers 'TaskScheduler.UnobservedTaskException event'.
+        /// </summary>
+        [Theory, IssueLink("https://github.com/npgsql/npgsql/issues/4149")]
+        [TestCase("CancelAndTimeout")]
+        [TestCase("CancelOnly")]
+        [TestCase("TimeoutOnly")]
+        public Task DelayedFaultedTaskCancellation(string testCase) => RunDelayedFaultedTaskTestAsync(async getUnobservedTaskException =>
         {
-            using (var cts = new CancellationTokenSource(10))
-                Assert.ThrowsAsync<TaskCanceledException>(async () =>
-                    await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(ct => GetVoidTaskAsync(Timeout.Infinite, ct),
-                        NpgsqlTimeout.Infinite, cts.Token));
+            var cancel = true;
+            var timeout = true;
+            switch (testCase)
+            {
+                case "TimeoutOnly":
+                    cancel = false;
+                    break;
+                case "CancelOnly":
+                    timeout = false;
+                    break;
+            }
+
+            var notifyDelayCompleted = new SemaphoreSlim(0, 1);
+
+            // Invoke the method that creates a delayed execution Task that fails subsequently.
+            await CreateTaskAndPreemptWithCancellationAsync(500, cancel, timeout, notifyDelayCompleted);
+
+            // Wait enough time for the non-cancelable task to fail and then do the GC collect to trigger the finalizer.
+            await notifyDelayCompleted.WaitAsync();
+            var timeEnd = DateTime.UtcNow.AddSeconds(3);
+            while (getUnobservedTaskException() is null && timeEnd > DateTime.UtcNow)
+            {
+                await Task.Delay(100);
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+        });
+
+        static async Task RunDelayedFaultedTaskTestAsync(Func<Func<Exception?>, Task> test)
+        {
+            Exception? unobservedTaskException = null;
+
+            // Subscribe to UnobservedTaskException event to store the Exception, if any.
+            void OnUnobservedTaskException(object? source, UnobservedTaskExceptionEventArgs args)
+            {
+                if (!args.Observed)
+                {
+                    args.SetObserved();
+                }
+                unobservedTaskException = args.Exception;
+            }
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+
+            try
+            {
+                await test(() => unobservedTaskException);
+
+                // Verify the unobserved Task exception event has not been received.
+                Assert.IsNull(unobservedTaskException, unobservedTaskException?.Message);
+            }
+            finally
+            {
+                TaskScheduler.UnobservedTaskException -= OnUnobservedTaskException;
+            }
+        }
+
+        /// <summary>
+        /// Create a delayed execution, non-Cancellable Task that fails subsequently after the Task goes out of scope.
+        /// </summary>
+        static async Task CreateTaskAndPreemptWithCancellationAsync(int delayMs, bool cancel, bool timeout, SemaphoreSlim notifyDelayCompleted)
+        {
+            var nonCancellableTask = Task.Delay(delayMs, CancellationToken.None)
+                .ContinueWith(
+                    async _ =>
+                    {
+                        try
+                        {
+                            await Task.FromException(new Exception("Unobserved Task Test Exception"));
+                        }
+                        finally
+                        {
+                            notifyDelayCompleted.Release();
+                        }
+                    })
+                .Unwrap();
+
+            var timeoutMs = delayMs / 5;
+            using var cts = cancel ? new CancellationTokenSource(timeoutMs) : null;
+            try
+            {
+                await TaskExtensions.ExecuteWithCancellationAndTimeoutAsync(
+                    _ => nonCancellableTask,
+                    timeout ? new NpgsqlTimeout(TimeSpan.FromMilliseconds(timeoutMs)) : NpgsqlTimeout.Infinite,
+                    cts?.Token ?? CancellationToken.None);
+            }
+            catch (TimeoutException)
+            {
+                // Expected due to preemptive time out.
+            }
+            catch (OperationCanceledException) when (cts?.IsCancellationRequested == true)
+            {
+                // Expected due to preemptive cancellation.
+            }
+            Assert.False(nonCancellableTask.IsCompleted);
         }
     }
 }
