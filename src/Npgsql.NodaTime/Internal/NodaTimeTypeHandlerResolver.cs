39cc38bc8965ca967fb830fc8292512a870b5710
Shay Rojansky <roji@roji.org>
2021-12-29 14:51:43 +0200

    Map specialized NodaTime range types by default (#4252)
    tstzrange[] -> Interval[] (instead of NpgsqlRange<Instant>[])
    daterange[] -> DateInterval[] (instead of NpgsqlRange<LocalDate>[])
    
    Fixes #4234

--- src/Npgsql.NodaTime/Internal/NodaTimeTypeHandlerResolver.cs
+++ src/Npgsql.NodaTime/Internal/NodaTimeTypeHandlerResolver.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using NodaTime;
 using Npgsql.Internal;
+using Npgsql.Internal.TypeHandlers;
 using Npgsql.Internal.TypeHandling;
 using Npgsql.PostgresTypes;
 using NpgsqlTypes;
@@ -25,6 +26,11 @@ public class NodaTimeTypeHandlerResolver : TypeHandlerResolver
     DateMultirangeHandler? _dateMultirangeHandler;
     TimestampTzMultirangeHandler? _timestampTzMultirangeHandler;
 
+    NpgsqlTypeHandler? _timestampTzRangeArray;
+    NpgsqlTypeHandler? _dateRangeArray;
+
+    readonly ArrayNullabilityMode _arrayNullabilityMode;
+
     internal NodaTimeTypeHandlerResolver(NpgsqlConnector connector)
     {
         _databaseInfo = connector.DatabaseInfo;
@@ -42,6 +48,8 @@ public class NodaTimeTypeHandlerResolver : TypeHandlerResolver
 
         // Note that the range handlers are absent on some pseudo-PostgreSQL databases (e.g. CockroachDB), and multirange types
         // were only introduced in PG14. So we resolve these lazily.
+
+        _arrayNullabilityMode = connector.Settings.ArrayNullabilityMode;
     }
 
     public override NpgsqlTypeHandler? ResolveByDataTypeName(string typeName)
@@ -58,6 +66,9 @@ public class NodaTimeTypeHandlerResolver : TypeHandlerResolver
             "daterange" => DateRange(),
             "tstzmultirange" => TsTzMultirange(),
             "datemultirange" => DateMultirange(),
+
+            "tstzrange[]" => TsTzRangeArray(),
+            "daterange[]" => DateRangeArray(),
 
             _ => null
         };
@@ -206,4 +217,12 @@ public class NodaTimeTypeHandlerResolver : TypeHandlerResolver
 
     NpgsqlTypeHandler DateMultirange()
         => _dateMultirangeHandler ??= new DateMultirangeHandler((PostgresMultirangeType)PgType("datemultirange"), DateRange());
+
+    NpgsqlTypeHandler TsTzRangeArray()
+        => _timestampTzRangeArray ??=
+            new ArrayHandler<Interval>((PostgresArrayType)PgType("tstzrange[]"), TsTzRange(), _arrayNullabilityMode);
+
+    NpgsqlTypeHandler DateRangeArray()
+        => _dateRangeArray ??=
+            new ArrayHandler<DateInterval>((PostgresArrayType)PgType("daterange[]"), DateRange(), _arrayNullabilityMode);
 }
