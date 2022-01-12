0fa3e9feb1ed6a1c7ee597453008a02ec0425b18
Brar Piening <brar@gmx.de>
2022-01-12 16:46:56 +0100

    Support READ_REPLICATION_SLOT cmd on physical replication connections (#4241)
    Closes #4215

--- src/Npgsql/Replication/PhysicalReplicationSlot.cs
+++ src/Npgsql/Replication/PhysicalReplicationSlot.cs
@@ -1,4 +1,6 @@
-﻿namespace Npgsql.Replication;
+﻿using NpgsqlTypes;
+
+namespace Npgsql.Replication;
 
 /// <summary>
 /// Wraps a replication slot that uses physical replication.
@@ -13,6 +15,22 @@ public class PhysicalReplicationSlot : ReplicationSlot
     /// that has been initialized for physical replication.
     /// </remarks>
     /// <param name="slotName">The name of the existing replication slot</param>
-    public PhysicalReplicationSlot(string slotName)
-        : base(slotName) { }
+    /// <param name="restartLsn">The replication slot's <c>restart_lsn</c></param>
+    /// <param name="restartTimeline">The timeline ID associated to <c>restart_lsn</c>, following the current timeline history.</param>
+    public PhysicalReplicationSlot(string slotName, NpgsqlLogSequenceNumber? restartLsn = null, ulong? restartTimeline = null)
+        : base(slotName)
+    {
+        RestartLsn = restartLsn;
+        RestartTimeline = restartTimeline;
+    }
+
+    /// <summary>
+    /// The replication slot's <c>restart_lsn</c>.
+    /// </summary>
+    public NpgsqlLogSequenceNumber? RestartLsn { get; }
+
+    /// <summary>
+    /// The timeline ID associated to <c>restart_lsn</c>, following the current timeline history.
+    /// </summary>
+    public ulong? RestartTimeline { get; }
 }
