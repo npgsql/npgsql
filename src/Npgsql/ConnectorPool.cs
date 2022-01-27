ed59682462803653bbe205d5b94bf0c8ac1cc886
Nikita Kazmin <vonzshik@gmail.com>
2022-01-27 15:53:08 +0300

    Fix clear pool stack overflow for broken connections (#4294)
    Fixes #4244
    
    (cherry picked from commit 560ead83047076d5a65e63cd0ae9889f2a362351)

--- src/Npgsql/ConnectorPool.cs
+++ src/Npgsql/ConnectorPool.cs
@@ -58,6 +58,8 @@ namespace Npgsql
         volatile bool _pruningTimerEnabled;
         int _pruningSampleIndex;
 
+        volatile int _isClearing;
+
         static readonly SingleThreadSynchronizationContext SingleThreadSynchronizationContext = new("NpgsqlRemainingAsyncSendWorker");
 
         #endregion
@@ -294,7 +296,7 @@ namespace Npgsql
             // If Clear/ClearAll has been been called since this connector was first opened,
             // throw it away. The same if it's broken (in which case CloseConnector is only
             // used to update state/perf counter).
-            if (connector.ClearCounter < _clearCounter || connector.IsBroken)
+            if (connector.ClearCounter != _clearCounter || connector.IsBroken)
             {
                 CloseConnector(connector);
                 return;
@@ -310,14 +312,24 @@ namespace Npgsql
         {
             Interlocked.Increment(ref _clearCounter);
 
-            var count = _idleCount;
-            while (count > 0 && _idleConnectorReader.TryRead(out var connector))
+            if (Interlocked.CompareExchange(ref _isClearing, 1, 0) == 1)
+                return;
+
+            try
             {
-                if (CheckIdleConnector(connector))
+                var count = _idleCount;
+                while (count > 0 && _idleConnectorReader.TryRead(out var connector))
                 {
-                    CloseConnector(connector);
-                    count--;
+                    if (CheckIdleConnector(connector))
+                    {
+                        CloseConnector(connector);
+                        count--;
+                    }
                 }
+            }
+            finally
+            {
+                _isClearing = 0;
             }
         }
 
