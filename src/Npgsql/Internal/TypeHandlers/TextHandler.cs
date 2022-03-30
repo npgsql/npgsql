c80d34df825c38d0288626c2d22099c096e8167a
Shay Rojansky <roji@roji.org>
2022-03-30 21:33:12 +0300

    Pass encoding for column GetTextReader
    Fixes #4384

--- src/Npgsql/Internal/TypeHandlers/TextHandler.cs
+++ src/Npgsql/Internal/TypeHandlers/TextHandler.cs
@@ -270,5 +270,5 @@ public partial class TextHandler : NpgsqlTypeHandler<string>, INpgsqlTypeHandler
     #endregion
 
     /// <inheritdoc />
-    public virtual TextReader GetTextReader(Stream stream) => new StreamReader(stream);
+    public virtual TextReader GetTextReader(Stream stream) => new StreamReader(stream, _encoding);
 }
