9e43fd8449315b9d2b597a1b0588d53bd53d8ee4
Nikita Kazmin <vonzshik@gmail.com>
2022-04-19 17:29:12 +0300

    Fix double connector return while loading database info (#4426)
    Fixes #4425

--- src/Npgsql/NpgsqlConnection.cs
+++ src/Npgsql/NpgsqlConnection.cs
@@ -324,15 +324,6 @@ public sealed class NpgsqlConnection : DbConnection, ICloneable, IComponent
                 Debug.Assert(connector.Connection is null,
                     $"Connection for opened connector '{Connector?.Id.ToString() ?? "???"}' is bound to another connection");
 
-                ConnectorBindingScope = ConnectorBindingScope.Connection;
-                connector.Connection = this;
-                Connector = connector;
-
-                if (enlistToTransaction is not null)
-                    EnlistTransaction(enlistToTransaction);
-
-                timeout = new NpgsqlTimeout(connectionTimeout);
-
                 // Since this connector was last used, PostgreSQL types (e.g. enums) may have been added
                 // (and ReloadTypes() called), or global mappings may have changed by the user.
                 // Bring this up to date if needed.
@@ -340,6 +331,13 @@ public sealed class NpgsqlConnection : DbConnection, ICloneable, IComponent
                 // need to update the connector type mapper (this is why this is here).
                 if (connector.TypeMapper.ChangeCounter != TypeMapping.GlobalTypeMapper.Instance.ChangeCounter)
                     await connector.LoadDatabaseInfo(false, timeout, async, cancellationToken);
+
+                ConnectorBindingScope = ConnectorBindingScope.Connection;
+                connector.Connection = this;
+                Connector = connector;
+
+                if (enlistToTransaction is not null)
+                    EnlistTransaction(enlistToTransaction);
 
                 LogMessages.OpenedConnection(Logger, Host!, Port, Database, _userFacingConnectionString, connector.Id);
                 FullState = ConnectionState.Open;
