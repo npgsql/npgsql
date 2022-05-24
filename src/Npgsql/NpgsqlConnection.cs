4629af59baef36f3febede7689243e6843898bdd
Nikita Kazmin <vonzshik@gmail.com>
2022-05-24 13:50:23 +0300

    Just in case

--- src/Npgsql/NpgsqlConnection.cs
+++ src/Npgsql/NpgsqlConnection.cs
@@ -291,6 +291,7 @@ namespace Npgsql
                     
                     ConnectorBindingScope = ConnectorBindingScope.Connection;
                     Connector = connector;
+                    connectorIsBound = true;
 
                     if (enlistToTransaction is not null)
                         EnlistTransaction(enlistToTransaction);
