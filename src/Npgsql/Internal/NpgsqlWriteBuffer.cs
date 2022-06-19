8e131b3e68098c86fe452854cc1b1afc7c5203cb
Nikita Kazmin <vonzshik@gmail.com>
2022-06-20 00:30:12 +0300

    Add support for Stream (#4517)
    Closes #4466

--- src/Npgsql/Internal/NpgsqlWriteBuffer.cs
+++ src/Npgsql/Internal/NpgsqlWriteBuffer.cs
@@ -470,6 +470,30 @@ public sealed partial class NpgsqlWriteBuffer : IDisposable
         }
     }
 
+    public async Task WriteStreamRaw(Stream stream, int count, bool async, CancellationToken cancellationToken = default)
+    {
+        while (count > 0)
+        {
+            if (WriteSpaceLeft == 0)
+                await Flush(async, cancellationToken);
+            try
+            {
+                var read = async
+                    ? await stream.ReadAsync(Buffer, WritePosition, Math.Min(WriteSpaceLeft, count), cancellationToken)
+                    : stream.Read(Buffer, WritePosition, Math.Min(WriteSpaceLeft, count));
+                if (read == 0)
+                    throw new EndOfStreamException();
+                WritePosition += read;
+                count -= read;
+            }
+            catch (Exception e)
+            {
+                throw Connector.Break(new NpgsqlException("Exception while writing to stream", e));
+            }
+        }
+        Debug.Assert(count == 0);
+    }
+
     public void WriteNullTerminatedString(string s)
     {
         Debug.Assert(s.All(c => c < 128), "Method only supports ASCII strings");
