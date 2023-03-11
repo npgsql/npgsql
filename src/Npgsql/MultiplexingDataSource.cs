ef9d124b5d78799a43f65cb03af995739fdeef59
Nikita Kazmin <vonzshik@gmail.com>
2023-03-11 20:49:53 +0300

    Test fix

--- src/Npgsql/MultiplexingDataSource.cs
+++ src/Npgsql/MultiplexingDataSource.cs
@@ -109,7 +109,7 @@ sealed class MultiplexingDataSource : PoolingDataSource
                     }
 
                     connector = await OpenNewConnector(
-                        command.Connection!,
+                        command.InternalConnection!,
                         TimeSpan.FromSeconds(Settings.Timeout),
                         async: true,
                         CancellationToken.None);
