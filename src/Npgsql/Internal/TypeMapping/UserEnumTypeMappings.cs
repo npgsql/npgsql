e431d0f03bfe11ecc326da1cd180146893f3c5a3
kronic <kronic@softland.ru>
2023-06-06 11:29:06 +0300

    Optimize UserEnumTypeMapping

--- src/Npgsql/Internal/TypeMapping/UserEnumTypeMappings.cs
+++ src/Npgsql/Internal/TypeMapping/UserEnumTypeMappings.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using Npgsql.Internal.TypeHandlers;
 using Npgsql.Internal.TypeHandling;
 using Npgsql.PostgresTypes;
@@ -20,17 +20,20 @@ sealed class UserEnumTypeMapping<TEnum> : IUserEnumTypeMapping
     public string PgTypeName { get; }
     public Type ClrType => typeof(TEnum);
     public INpgsqlNameTranslator NameTranslator { get; }
-
-    readonly Dictionary<TEnum, string> _enumToLabel = new();
-    readonly Dictionary<string, TEnum> _labelToEnum = new();
+    readonly Dictionary<string, TEnum> _labelToEnum;
+    readonly Dictionary<TEnum, string> _enumToLabel;
+    
 
     public UserEnumTypeMapping(string pgTypeName, INpgsqlNameTranslator nameTranslator)
     {
         (PgTypeName, NameTranslator) = (pgTypeName, nameTranslator);
-
-        foreach (var field in typeof(TEnum).GetFields(BindingFlags.Static | BindingFlags.Public))
+        var fields = typeof(TEnum).GetFields(BindingFlags.Static | BindingFlags.Public);
+        _enumToLabel = new(fields.Length, new EnumEqualityComparer());
+        _labelToEnum = new(fields.Length);
+        
+        foreach (var field in fields)
         {
-            var attribute = (PgNameAttribute?)field.GetCustomAttributes(typeof(PgNameAttribute), false).FirstOrDefault();
+            var attribute = (PgNameAttribute?)field.GetCustomAttribute(typeof(PgNameAttribute), false);
             var enumName = attribute is null
                 ? nameTranslator.TranslateMemberName(field.Name)
                 : attribute.PgName;
@@ -43,4 +46,64 @@ sealed class UserEnumTypeMapping<TEnum> : IUserEnumTypeMapping
 
     public NpgsqlTypeHandler CreateHandler(PostgresType postgresType, NpgsqlConnector connector)
         => new EnumHandler<TEnum>((PostgresEnumType)postgresType, _enumToLabel, _labelToEnum);
+
+    sealed class EnumEqualityComparer : IEqualityComparer<TEnum>
+    {
+        readonly Func<TEnum, int> _getHashCode;
+        readonly Func<TEnum, TEnum, bool> _equals;
+        public EnumEqualityComparer()
+        {
+            var enumUnderlyingType = typeof(TEnum).GetEnumUnderlyingType();
+
+            if (enumUnderlyingType == typeof(byte))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, byte>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, byte>(ref x) == Unsafe.As<TEnum, byte>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(sbyte))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, sbyte>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, sbyte>(ref x) == Unsafe.As<TEnum, sbyte>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(short))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, short>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, short>(ref x) == Unsafe.As<TEnum, short>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(ushort))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, ushort>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, ushort>(ref x) == Unsafe.As<TEnum, ushort>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(int))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, int>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, int>(ref x) == Unsafe.As<TEnum, int>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(uint))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, uint>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, uint>(ref x) == Unsafe.As<TEnum, uint>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(long))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, long>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, long>(ref x) == Unsafe.As<TEnum, long>(ref y);
+            }
+            else if (enumUnderlyingType == typeof(ulong))
+            {
+                _getHashCode = static e => Unsafe.As<TEnum, ulong>(ref e).GetHashCode();
+                _equals = static (x, y) => Unsafe.As<TEnum, ulong>(ref x) == Unsafe.As<TEnum, ulong>(ref y);
+            }
+            else
+            {
+                _getHashCode = static e => e.GetHashCode();
+                _equals = static (x, y) => EqualityComparer<TEnum>.Default.Equals(x, y);
+            }
+        }
+
+        public bool Equals(TEnum x, TEnum y) => _equals(x, y);
+
+        public int GetHashCode(TEnum obj) => _getHashCode(obj);
+    }
 }
