e0c69a027d517279ec8b3d60bb28abaa37791d9f
Nino Floris <mail@ninofloris.com>
2023-06-30 18:29:22 +0200

    Remove record bloat

--- src/Npgsql/PostgresTypes/Oid.cs
+++ src/Npgsql/PostgresTypes/Oid.cs
@@ -1,9 +1,19 @@
+using System;
+
 namespace Npgsql.PostgresTypes;
 
-public readonly record struct Oid(uint Value)
+public readonly struct Oid: IEquatable<Oid>
 {
+    public Oid(uint value) => Value = value;
+
     public static explicit operator uint(Oid oid) => oid.Value;
     public static implicit operator Oid(uint oid) => new(oid);
+    public uint Value { get; init; }
 
     public override string ToString() => Value.ToString();
+    public bool Equals(Oid other) => Value == other.Value;
+    public override bool Equals(object? obj) => obj is Oid other && Equals(other);
+    public override int GetHashCode() => (int)Value;
+    public static bool operator ==(Oid left, Oid right) => left.Equals(right);
+    public static bool operator !=(Oid left, Oid right) => !left.Equals(right);
 }
