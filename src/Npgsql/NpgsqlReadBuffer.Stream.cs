c2440b8cdaa9491eecc4fd275928d92877bfd286
Nikita Kazmin <vonzshik@gmail.com>
2023-08-25 18:11:21 +0300

    Fix seek for ColumnStream (#5224)
    Fixes #5223
    
    (cherry picked from commit 820cfcddf95dd7736affa53fafd4f4e04b473e04)
    
    # Conflicts:
    #	src/Npgsql/Internal/NpgsqlReadBuffer.Stream.cs
    #	test/Npgsql.Tests/ReaderTests.cs

--- src/Npgsql/NpgsqlReadBuffer.Stream.cs
+++ src/Npgsql/NpgsqlReadBuffer.Stream.cs
@@ -64,7 +64,7 @@ namespace Npgsql
                 {
                     if (value < 0)
                         throw new ArgumentOutOfRangeException(nameof(value), "Non - negative number required.");
-                    Seek(_start + value, SeekOrigin.Begin);
+                    Seek(value, SeekOrigin.Begin);
                 }
             }
 
@@ -86,24 +86,25 @@ namespace Npgsql
                     var tempPosition = unchecked(_start + (int)offset);
                     if (offset < 0 || tempPosition < _start)
                         throw new IOException(seekBeforeBegin);
-                    _buf.ReadPosition = _start;
-                    return tempPosition;
+                    _buf.ReadPosition = tempPosition;
+                    _read = (int)offset;
+                    return _read;
                 }
                 case SeekOrigin.Current:
                 {
                     var tempPosition = unchecked(_buf.ReadPosition + (int)offset);
                     if (unchecked(_buf.ReadPosition + offset) < _start || tempPosition < _start)
                         throw new IOException(seekBeforeBegin);
-                    _buf.ReadPosition = tempPosition;
-                    return tempPosition;
+                    _read += (int)offset;
+                    return _read;
                 }
                 case SeekOrigin.End:
                 {
-                    var tempPosition = unchecked(_len + (int)offset);
-                    if (unchecked(_len + offset) < _start || tempPosition < _start)
+                    var tempPosition = unchecked(_start + _len + (int)offset);
+                    if (unchecked(_start + _len + offset) < _start || tempPosition < _start)
                         throw new IOException(seekBeforeBegin);
-                    _buf.ReadPosition = tempPosition;
-                    return tempPosition;
+                    _read = _len + (int)offset;
+                    return _read;
                 }
                 default:
                     throw new ArgumentOutOfRangeException(nameof(origin), "Invalid seek origin.");
