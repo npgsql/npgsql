990969691ace0406327620db2f7a49c5a4687585
Nikita Kazmin <vonzshik@gmail.com>
2023-08-25 23:54:04 +0300

    Revert "Trying to repro no-data message error"
    This reverts commit 53e8c515ef7305335df14e6c787f8349d77e3917.

--- src/Npgsql/Internal/NpgsqlConnector.cs
+++ src/Npgsql/Internal/NpgsqlConnector.cs
@@ -2340,11 +2340,6 @@ public sealed partial class NpgsqlConnector
             ReadBuffer = _origReadBuffer;
             _origReadBuffer = null;
         }
-
-        if (ReadBuffer.ReadBytesLeft > 0)
-        {
-            throw new Exception("Non-empty buffer");
-        }
     }
 
     internal void UnprepareAll()
