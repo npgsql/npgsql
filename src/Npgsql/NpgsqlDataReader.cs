21c41cc1d70d6acce44789cbcf085e69ad2ea6e4
Nikita Kazmin <vonzshik@gmail.com>
2023-09-27 19:39:21 +0300

    Fix setting IsReadOnly with old readers's schema (#5286)
    Fixes #5284
    
    (cherry picked from commit aa32e4f479a37a71a965edb52cc4fd837895c1fb)

--- src/Npgsql/NpgsqlDataReader.cs
+++ src/Npgsql/NpgsqlDataReader.cs
@@ -2169,6 +2169,7 @@ public sealed class NpgsqlDataReader : DbDataReader, IDbColumnSchemaGenerator
             row["IsRowVersion"] = false;
             row["IsHidden"] = column.IsHidden == true;
             row["IsLong"] = column.IsLong == true;
+            row["IsReadOnly"] = column.IsReadOnly == true;
             row["DataTypeName"] = column.DataTypeName;
 
             table.Rows.Add(row);
