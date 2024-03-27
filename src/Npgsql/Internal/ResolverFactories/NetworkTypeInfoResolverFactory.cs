5f24dc61157f4d96be740b5bd627adb901184e88
Nino Floris <mail@ninofloris.com>
2024-03-27 20:15:05 +0100

    Return boxing info for derived IPAddress types instead of using reflection (#5649)

--- src/Npgsql/Internal/ResolverFactories/NetworkTypeInfoResolverFactory.cs
+++ src/Npgsql/Internal/ResolverFactories/NetworkTypeInfoResolverFactory.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.NetworkInformation;
 using Npgsql.Internal.Converters;
@@ -31,13 +30,15 @@ sealed class NetworkTypeInfoResolverFactory : PgTypeInfoResolverFactory
                 mapping => mapping with { MatchRequirement = MatchRequirement.DataTypeName });
 
             // inet
-            // This is one of the rare mappings that force us to use reflection for a lack of any alternative.
             // There are certain IPAddress values like Loopback or Any that return a *private* derived type (see https://github.com/dotnet/runtime/issues/27870).
-            // However we still need to be able to resolve an exactly typed converter for those values.
-            // We do so by wrapping our converter in a casting converter constructed over the derived type.
+            // However we still need to be able to resolve some typed converter for those values.
+            // We do so by returning a boxing info when we deal with a derived type, as a result we don't need an exact typed converter.
+            // For arrays users can't actually reference the private type so we'll only see some version of ArrayType<IPAddress>.
+            // For reads we'll only see the public type so we never surface an InvalidCastException trying to cast IPAddress to ReadOnlyIPAddress.
             // Finally we add a custom predicate to be able to match any type which values are assignable to IPAddress.
             mappings.AddType<IPAddress>(DataTypeNames.Inet,
-                CreateInfo,
+                static (options, mapping, _) => new PgTypeInfo(options, new IPAddressConverter(),
+                    new DataTypeName(mapping.DataTypeName), unboxedType: mapping.Type == typeof(IPAddress) ? null : mapping.Type),
                 mapping => mapping with
                 {
                     MatchRequirement = MatchRequirement.Single,
@@ -49,21 +50,6 @@ sealed class NetworkTypeInfoResolverFactory : PgTypeInfoResolverFactory
             // cidr
             mappings.AddStructType<NpgsqlCidr>(DataTypeNames.Cidr,
                 static (options, mapping, _) => mapping.CreateInfo(options, new NpgsqlCidrConverter()), isDefault: true);
-
-            // Code is split out to a local method as suppression attributes on lambdas aren't properly handled by the ILLink analyzer yet.
-            [UnconditionalSuppressMessage("AotAnalysis", "IL3050",
-                Justification = "MakeGenericType is safe because the target will only ever be a reference type.")]
-            static PgTypeInfo CreateInfo(PgSerializerOptions options, TypeInfoMapping resolvedMapping, bool _)
-            {
-                var derivedType = resolvedMapping.Type != typeof(IPAddress);
-                PgConverter converter = new IPAddressConverter();
-                if (derivedType)
-                    // There is not much more we can do, the deriving type IPAddress+ReadOnlyIPAddress isn't public.
-                    converter = (PgConverter)Activator.CreateInstance(typeof(CastingConverter<>).MakeGenericType(resolvedMapping.Type),
-                        converter)!;
-
-                return resolvedMapping.CreateInfo(options, converter);
-            }
 
             return mappings;
         }
