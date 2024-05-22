5879c21c8ae1a29cb3393b90741779d0d8874414
Bjørnar Skinnes <bjornar_skinnes@trimble.com>
2024-05-22 21:08:23 +0200

    update test, better clean up.

--- test/Npgsql.Tests/ConnectionTests.cs
+++ test/Npgsql.Tests/ConnectionTests.cs
@@ -738,7 +738,9 @@ public class ConnectionTests : MultiplexingTestBase
         await conn1.ExecuteNonQueryAsync($"DROP SCHEMA IF EXISTS {extensionSchema} CASCADE");
         await conn1.ExecuteNonQueryAsync($"CREATE SCHEMA IF NOT EXISTS dummy");
         await conn1.ExecuteNonQueryAsync($"CREATE SCHEMA {extensionSchema}");
-        await conn1.ExecuteNonQueryAsync($"DROP EXTENSION IF EXISTS postgis CASCADE");
+        await conn1.ExecuteNonQueryAsync($"DROP EXTENSION IF EXISTS postgis_topology");
+        await conn1.ExecuteNonQueryAsync($"DROP EXTENSION IF EXISTS postgis_tiger_geocoder");
+        await conn1.ExecuteNonQueryAsync($"DROP EXTENSION IF EXISTS postgis");
         await conn1.ExecuteNonQueryAsync($"CREATE EXTENSION postgis SCHEMA {extensionSchema}");
         await trans.CommitAsync();
         await conn1.CloseAsync();
@@ -752,6 +754,11 @@ public class ConnectionTests : MultiplexingTestBase
         using var dataSource = dataSourceBuilder.Build();
         using var conn = await dataSource.OpenConnectionAsync();
         Assert.True(dataSource.DatabaseInfo.BaseTypes.Any(x => x.Name == "geography" && x.Namespace == extensionSchema));
+
+        await conn1.ExecuteNonQueryAsync($"DROP EXTENSION IF EXISTS postgis");
+        await conn1.ExecuteNonQueryAsync($"CREATE EXTENSION postgis");
+        await conn1.ExecuteNonQueryAsync($"CREATE EXTENSION postgis_tiger_geocoder");
+        await conn1.ExecuteNonQueryAsync($"CREATE EXTENSION postgis_topology");
     }
 
     [Test]
