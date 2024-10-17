bcad46b33a076c631a8e0e72f7d9be2f5e932255
Nikita Kazmin <vonzshik@gmail.com>
2024-10-17 16:48:04 +0300

    Add IPNetwork to cidr mapping

---
+++ src/Npgsql/Internal/Converters/Networking/IPNetworkConverter.cs
@@ -0,0 +1,26 @@
+﻿#if NET8_0_OR_GREATER
+
+using System.Net;
+
+// ReSharper disable once CheckNamespace
+namespace Npgsql.Internal.Converters;
+
+sealed class IPNetworkConverter : PgBufferedConverter<IPNetwork>
+{
+    public override bool CanConvert(DataFormat format, out BufferRequirements bufferRequirements)
+        => CanConvertBufferedDefault(format, out bufferRequirements);
+
+    public override Size GetSize(SizeContext context, IPNetwork value, ref object? writeState)
+        => NpgsqlInetConverter.GetSizeImpl(context, value.BaseAddress, ref writeState);
+
+    protected override IPNetwork ReadCore(PgReader reader)
+    {
+        var (ip, netmask) = NpgsqlInetConverter.ReadImpl(reader, shouldBeCidr: true);
+        return new(ip, netmask);
+    }
+
+    protected override void WriteCore(PgWriter writer, IPNetwork value)
+        => NpgsqlInetConverter.WriteImpl(writer, (value.BaseAddress, (byte)value.PrefixLength), isCidr: true);
+}
+
+#endif
