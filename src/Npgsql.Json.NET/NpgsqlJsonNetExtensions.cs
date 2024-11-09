39f5d3606ac8f91c99e3ad61a466a2ae0ae67630
Nino Floris <mail@ninofloris.com>
2024-11-09 17:42:16 +0700

    Add plugin registration overloads returning the concrete builder type

--- src/Npgsql.Json.NET/NpgsqlJsonNetExtensions.cs
+++ src/Npgsql.Json.NET/NpgsqlJsonNetExtensions.cs
@@ -13,6 +13,7 @@ namespace Npgsql;
 /// </summary>
 public static class NpgsqlJsonNetExtensions
 {
+    // Note: defined for binary compatibility and NpgsqlConnection.GlobalTypeMapper.
     /// <summary>
     /// Sets up JSON.NET mappings for the PostgreSQL json and jsonb types.
     /// </summary>
@@ -31,6 +32,32 @@ public static class NpgsqlJsonNetExtensions
         JsonSerializerSettings? settings = null,
         Type[]? jsonbClrTypes = null,
         Type[]? jsonClrTypes = null)
+    {
+        // Reverse order
+        mapper.AddTypeInfoResolverFactory(new JsonNetPocoTypeInfoResolverFactory(jsonbClrTypes, jsonClrTypes, settings));
+        mapper.AddTypeInfoResolverFactory(new JsonNetTypeInfoResolverFactory(settings));
+        return mapper;
+    }
+
+    /// <summary>
+    /// Sets up JSON.NET mappings for the PostgreSQL json and jsonb types.
+    /// </summary>
+    /// <param name="mapper">The type mapper to set up.</param>
+    /// <param name="settings">Optional settings to customize JSON serialization.</param>
+    /// <param name="jsonbClrTypes">
+    /// A list of CLR types to map to PostgreSQL <c>jsonb</c> (no need to specify <see cref="NpgsqlDbType.Jsonb" />).
+    /// </param>
+    /// <param name="jsonClrTypes">
+    /// A list of CLR types to map to PostgreSQL <c>json</c> (no need to specify <see cref="NpgsqlDbType.Json" />).
+    /// </param>
+    [RequiresUnreferencedCode("Json serializer may perform reflection on trimmed types.")]
+    [RequiresDynamicCode("Serializing arbitrary types to json can require creating new generic types or methods, which requires creating code at runtime. This may not work when AOT compiling.")]
+    public static TMapper UseJsonNet<TMapper>(
+        this TMapper mapper,
+        JsonSerializerSettings? settings = null,
+        Type[]? jsonbClrTypes = null,
+        Type[]? jsonClrTypes = null)
+        where TMapper : INpgsqlTypeMapper
     {
         // Reverse order
         mapper.AddTypeInfoResolverFactory(new JsonNetPocoTypeInfoResolverFactory(jsonbClrTypes, jsonClrTypes, settings));
--- src/Npgsql.NetTopologySuite/NpgsqlNetTopologySuiteExtensions.cs
+++ src/Npgsql.NetTopologySuite/NpgsqlNetTopologySuiteExtensions.cs
@@ -10,6 +10,7 @@ namespace Npgsql;
 /// </summary>
 public static class NpgsqlNetTopologySuiteExtensions
 {
+    // Note: defined for binary compatibility and NpgsqlConnection.GlobalTypeMapper.
     /// <summary>
     /// Sets up NetTopologySuite mappings for the PostGIS types.
     /// </summary>
@@ -26,6 +27,28 @@ public static class NpgsqlNetTopologySuiteExtensions
         PrecisionModel? precisionModel = null,
         Ordinates handleOrdinates = Ordinates.None,
         bool geographyAsDefault = false)
+    {
+        mapper.AddTypeInfoResolverFactory(new NetTopologySuiteTypeInfoResolverFactory(coordinateSequenceFactory, precisionModel, handleOrdinates, geographyAsDefault));
+        return mapper;
+    }
+
+    /// <summary>
+    /// Sets up NetTopologySuite mappings for the PostGIS types.
+    /// </summary>
+    /// <param name="mapper">The type mapper to set up (global or connection-specific).</param>
+    /// <param name="coordinateSequenceFactory">The factory which knows how to build a particular implementation of ICoordinateSequence from an array of Coordinates.</param>
+    /// <param name="precisionModel">Specifies the grid of allowable points.</param>
+    /// <param name="handleOrdinates">Specifies the ordinates which will be handled. Not specified ordinates will be ignored.
+    /// If <see cref="F:GeoAPI.Geometries.Ordiantes.None" /> is specified, an actual value will be taken from
+    /// the <see cref="P:GeoAPI.Geometries.ICoordinateSequenceFactory.Ordinates"/> property of <paramref name="coordinateSequenceFactory"/>.</param>
+    /// <param name="geographyAsDefault">Specifies that the geography type is used for mapping by default.</param>
+    public static TMapper UseNetTopologySuite<TMapper>(
+        this TMapper mapper,
+        CoordinateSequenceFactory? coordinateSequenceFactory = null,
+        PrecisionModel? precisionModel = null,
+        Ordinates handleOrdinates = Ordinates.None,
+        bool geographyAsDefault = false)
+        where TMapper : INpgsqlTypeMapper
     {
         mapper.AddTypeInfoResolverFactory(new NetTopologySuiteTypeInfoResolverFactory(coordinateSequenceFactory, precisionModel, handleOrdinates, geographyAsDefault));
         return mapper;
