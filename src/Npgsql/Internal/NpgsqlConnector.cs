2a6fa30bddee89ee11807e4ce12f8500846dfea2
Nikita Kazmin <vonzshik@gmail.com>
2025-01-14 16:44:36 +0300

    Send close_notify TLS alert on connection shutdown

--- src/Npgsql/Internal/NpgsqlConnector.cs
+++ src/Npgsql/Internal/NpgsqlConnector.cs
@@ -2193,6 +2193,21 @@ public sealed partial class NpgsqlConnector
     /// </remarks>
     void Cleanup()
     {
+        if (_stream is SslStream sslStream)
+        {
+            try
+            {
+                // Send close_notify TLS alert to correctly close connection on postgres's side
+                sslStream.ShutdownAsync().GetAwaiter().GetResult();
+                // Theoretically we should do a 0 read here to receive server's close_notify alert
+                // But overall it doesn't look like it makes much of a difference
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
         try
         {
             _stream?.Dispose();
