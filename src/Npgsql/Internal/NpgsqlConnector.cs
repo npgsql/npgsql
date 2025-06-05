3f7fe8b24731aed67dae041e61d758a5345b808d
Nikita Kazmin <vonzshik@gmail.com>
2025-06-05 19:02:21 +0300

    Small refactoring

--- src/Npgsql/Internal/NpgsqlConnector.cs
+++ src/Npgsql/Internal/NpgsqlConnector.cs
@@ -666,28 +666,14 @@ public sealed partial class NpgsqlConnector
         var clientOptions = new NegotiateAuthenticationClientOptions { TargetName = targetName };
         var authentication = new NegotiateAuthentication(clientOptions);
 
-        var lengthBuffer = new byte[4];
-
         try
         {
-
             var data = authentication.GetOutgoingBlob(ReadOnlySpan<byte>.Empty, out var statusCode)!;
             Debug.Assert(statusCode == NegotiateAuthenticationStatusCode.ContinueNeeded);
 
-            Unsafe.WriteUnaligned(ref lengthBuffer[0], BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(data.Length) : data.Length);
+            var lengthBuffer = new byte[4];
 
-            if (async)
-            {
-                await _stream.WriteAsync(lengthBuffer, cancellationToken).ConfigureAwait(false);
-                await _stream.WriteAsync(data, cancellationToken).ConfigureAwait(false);
-                await _stream.FlushAsync(cancellationToken).ConfigureAwait(false);
-            }
-            else
-            {
-                _stream.Write(lengthBuffer);
-                _stream.Write(data);
-                _stream.Flush();
-            }
+            await WriteGssEncryptMessage(async, data, lengthBuffer).ConfigureAwait(false);
 
             while (true)
             {
@@ -720,6 +706,11 @@ public sealed partial class NpgsqlConnector
                     break;
                 }
 
+                await WriteGssEncryptMessage(async, data, lengthBuffer).ConfigureAwait(false);
+            }
+
+            async ValueTask WriteGssEncryptMessage(bool async, byte[] data, byte[] lengthBuffer)
+            {
                 Unsafe.WriteUnaligned(ref lengthBuffer[0], BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(data.Length) : data.Length);
 
                 if (async)
@@ -742,169 +733,10 @@ public sealed partial class NpgsqlConnector
             throw new NpgsqlException("Exception while performing GSS encryption", e);
         }
 
-        _stream = new GSSWrappingStream(_stream, authentication);
+        _stream = new GSSStream(_stream, authentication);
         ReadBuffer.Underlying = _stream;
         WriteBuffer.Underlying = _stream;
         return true;
-    }
-
-    sealed class GSSWrappingStream : Stream
-    {
-        const int MaxWriteMessageSizeLimit = 8 * 1024;
-        const int MaxReadMessageSizeLimit = 16 * 1024;
-
-        readonly Stream _stream;
-        readonly NegotiateAuthentication _authentication;
-
-        readonly ArrayBufferWriter<byte> _writeBuffer;
-        readonly byte[] _writeLengthBuffer;
-
-        readonly byte[] _readBuffer;
-        int _readPosition;
-        int _leftToRead;
-
-        internal GSSWrappingStream(Stream stream, NegotiateAuthentication authentication)
-        {
-            _stream = stream;
-            _authentication = authentication;
-            _writeBuffer = new ArrayBufferWriter<byte>(MaxWriteMessageSizeLimit + 2048);
-            _writeLengthBuffer = new byte[4];
-            _readBuffer = new byte[MaxReadMessageSizeLimit];
-        }
-
-        public override void Write(ReadOnlySpan<byte> buffer)
-        {
-            var start = 0;
-            while (start != buffer.Length)
-            {
-                var lengthToWrite = Math.Min(buffer.Length - start, MaxWriteMessageSizeLimit);
-                var result = _authentication.Wrap(
-                    buffer.Slice(start, lengthToWrite),
-                    _writeBuffer,
-                    _authentication.IsEncrypted,
-                    out _);
-                if (result != NegotiateAuthenticationStatusCode.Completed)
-                    throw new NpgsqlException();
-
-                var written = _writeBuffer.WrittenMemory;
-                Unsafe.WriteUnaligned(ref _writeLengthBuffer[0], BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(written.Length) : written.Length);
-
-                _stream.Write(_writeLengthBuffer);
-                _stream.Write(buffer.Slice(start, lengthToWrite));
-
-                _writeBuffer.ResetWrittenCount();
-                start += lengthToWrite;
-            }
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-            => Write(buffer.AsSpan(offset, count));
-
-        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
-        {
-            var start = 0;
-            while (start != buffer.Length)
-            {
-                var lengthToWrite = Math.Min(buffer.Length - start, MaxWriteMessageSizeLimit);
-                var result = _authentication.Wrap(
-                    buffer.Slice(start, lengthToWrite).Span,
-                    _writeBuffer,
-                    _authentication.IsEncrypted,
-                    out _);
-                if (result != NegotiateAuthenticationStatusCode.Completed)
-                    throw new NpgsqlException();
-
-                var written = _writeBuffer.WrittenMemory;
-                Unsafe.WriteUnaligned(ref _writeLengthBuffer[0], BitConverter.IsLittleEndian ? BinaryPrimitives.ReverseEndianness(written.Length) : written.Length);
-
-                await _stream.WriteAsync(_writeLengthBuffer, cancellationToken).ConfigureAwait(false);
-                await _stream.WriteAsync(_writeBuffer.WrittenMemory, cancellationToken).ConfigureAwait(false);
-
-                _writeBuffer.ResetWrittenCount();
-                start += lengthToWrite;
-            }
-        }
-
-        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-            => await WriteAsync(buffer.AsMemory(offset, count), cancellationToken).ConfigureAwait(false);
-
-        public override void Flush() => _stream.Flush();
-
-        public override Task FlushAsync(CancellationToken cancellationToken) => _stream.FlushAsync(cancellationToken);
-
-        public override int Read(Span<byte> buffer)
-        {
-            if (_leftToRead == 0)
-            {
-                _stream.ReadExactly(_readBuffer.AsSpan(0, 4));
-                var messageLength = BitConverter.IsLittleEndian
-                    ? BinaryPrimitives.ReverseEndianness(Unsafe.ReadUnaligned<int>(ref _readBuffer[0]))
-                    : Unsafe.ReadUnaligned<int>(ref _readBuffer[0]);
-                var messageBuffer = _readBuffer.AsSpan(0, messageLength);
-                _stream.ReadExactly(messageBuffer);
-                var result = _authentication.UnwrapInPlace(messageBuffer, out _readPosition, out _leftToRead, out _);
-                if (result != NegotiateAuthenticationStatusCode.Completed)
-                    throw new NpgsqlException();
-            }
-
-            var maxRead = Math.Min(_leftToRead, buffer.Length);
-            _readBuffer.AsSpan(_readPosition, maxRead).CopyTo(buffer);
-            _readPosition += maxRead;
-            _leftToRead -= maxRead;
-            return maxRead;
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-            => Read(buffer.AsSpan(offset, count));
-
-        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
-        {
-            if (_leftToRead == 0)
-            {
-                await _stream.ReadExactlyAsync(_readBuffer.AsMemory(0, 4), cancellationToken).ConfigureAwait(false);
-                var messageLength = BitConverter.IsLittleEndian
-                    ? BinaryPrimitives.ReverseEndianness(Unsafe.ReadUnaligned<int>(ref _readBuffer[0]))
-                    : Unsafe.ReadUnaligned<int>(ref _readBuffer[0]);
-                var messageBuffer = _readBuffer.AsMemory(0, messageLength);
-                await _stream.ReadExactlyAsync(messageBuffer, cancellationToken).ConfigureAwait(false);
-                var result = _authentication.UnwrapInPlace(messageBuffer.Span, out _readPosition, out _leftToRead, out _);
-                if (result != NegotiateAuthenticationStatusCode.Completed)
-                    throw new NpgsqlException();
-            }
-
-            var maxRead = Math.Min(_leftToRead, buffer.Length);
-            _readBuffer.AsMemory(_readPosition, maxRead).CopyTo(buffer);
-            _readPosition += maxRead;
-            _leftToRead -= maxRead;
-            return maxRead;
-        }
-
-        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-            => await ReadAsync(buffer.AsMemory(offset, count), cancellationToken).ConfigureAwait(false);
-
-        public override void Close() => _stream.Close();
-
-        protected override void Dispose(bool disposing)
-        {
-            _authentication.Dispose();
-            _stream.Dispose();
-        }
-
-        public override ValueTask DisposeAsync() => _stream.DisposeAsync();
-
-        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
-        public override void SetLength(long value) => throw new NotSupportedException();
-
-        public override bool CanRead => true;
-        public override bool CanWrite => true;
-        public override bool CanSeek => false;
-        public override long Length => throw new NotSupportedException();
-
-        public override long Position
-        {
-            get => throw new NotSupportedException();
-            set => throw new NotSupportedException();
-        }
     }
 
     internal async ValueTask<DatabaseState> QueryDatabaseState(
