11bfdc61736a0a5485d5995aa2c880a6e240d93f
Nikita Kazmin <vonzshik@gmail.com>
2025-12-08 19:10:29 +0300

    Throw OperationCancelledException with correct CancellationToken from Dns.GetHostAddressesAsync

--- src/Npgsql/Internal/NpgsqlConnector.cs
+++ src/Npgsql/Internal/NpgsqlConnector.cs
@@ -1349,9 +1349,9 @@ public sealed partial class NpgsqlConnector
                 {
                     ipAddresses = await Dns.GetHostAddressesAsync(Host,  combinedToken).ConfigureAwait(false);
                 }
-                catch (OperationCanceledException oce) when (
-                    oce.CancellationToken == combinedToken && !cancellationToken.IsCancellationRequested)
+                catch (OperationCanceledException)
                 {
+                    cancellationToken.ThrowIfCancellationRequested();
                     throw new TimeoutException();
                 }
             }
